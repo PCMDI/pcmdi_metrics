ec86aff27736aa97534227eff0cdf05895c0f4f2
Jeff Painter <painter1@oceanonly.llnl.gov>
2013-05-24 10:36:22 -0700

    minor bug fixes, minor indentation changes, lots of printing supported by new __repr__ methods, sort method in basic_filetable, added a couple more variables to the list of interesting standard_names.

--- src/python/io/filetable.py
+++ src/python/io/filetable.py
@@ -1,13 +1,13 @@
-#!/usr/local/uvcdat/bin/python
+#!/usr/local/uvcdat/1.3.1/bin/python
 
 # Set up and index a table whose entries will be something like
 #   file_id,  variable_id,  time_range,  lat_range,  lon_range,  level_range
 # subject to change!
 
-import sys, cdms2
+import sys, os, cdms2, pprint
 
 class drange:
-   def __init__( low, high, units ):
+   def __init__( self, low, high, units ):
        self.lo = low
        self.hi = high
        self.units = units
@@ -20,6 +20,8 @@ class drange:
          return True
       else:
          return self.hi>range2.lo and self.lo<range2.hi
+   def __repr__(self):
+      return "drange %s to %s %s"%(self.lo,self.hi,self.units)
        
 class row:
     """This class identifies a file and contains the information essential to tell
@@ -27,22 +29,26 @@ class row:
     There will be no more than that - if you want more information you can open the file.
     If the file has several variables, there will be several rows for the file."""
     # One can think of lots of cases where this is too simple, but it's a start.
-    def __init__( fileid, variableid, timerange, latrange, lonrange, levelrange=None ):
+    def __init__( self, fileid, variableid, timerange, latrange, lonrange, levelrange=None ):
         self.fileid = fileid
         self.variableid = variableid
         self.timerange = timerange
         self.latrange = latrange
         self.lonrange = lonrange
         self.levelrange = levelrange
+    def __repr__(self):
+       return "\n(row: ..%s %s domain:\n   %s %s %s)"%(
+          os.path.basename(self.fileid), self.variableid,\
+             self.timerange.__repr__(), self.latrange.__repr__(), self.lonrange.__repr__() )
 
 
 def get_datafile_support( dfile ):
     """dfile is an open datafile.  If the file type is recognized,
     then this will return an object with methods needed to support that file type."""
-    if hasattr(dfile,'Conventions') and dfile.Conventions[0:1]=='CF':
-        return CF_support( dfile )
+    if hasattr(dfile,'Conventions') and dfile.Conventions[0:2]=='CF':
+       return CF_support( dfile )
     else:
-        return No_support()
+       return No_support()
 
 class basic_filetable:
     """Conceptually a file table is just a list of rows; but we need to attach some methods,
@@ -64,6 +70,12 @@ class basic_filetable:
         print "filelist=",filelist,type(filelist)
         for filep in filelist.files:
             self.addfile( filep )
+    def __repr__(self):
+       return self._table.__repr__()
+    def sort(self):
+       """in-place sort keyed on the file paths"""
+       self._table.sort(key=(lambda row: row.fileid))
+       return self
     def addfile( self, filep ):
         """Extract essential header information from a file filep,
         and put the results in the table.
@@ -72,6 +84,7 @@ class basic_filetable:
         dfile = cdms2.open( fileid )
         filesupp = get_datafile_support( dfile )
         vars = filesupp.interesting_variables()
+        print "vars=",vars
         if len(vars)>0:
             timerange = filesupp.get_timerange()
             latrange = filesupp.get_latrange()
@@ -82,11 +95,11 @@ class basic_filetable:
                 newrow = row( fileid, variableid, timerange, latrange, lonrange,
                               levelrange )
                 self._table.append( newrow )
-                if self.fileindex.haskey(fileid):
+                if fileid in self._fileindex.keys():
                     self._fileindex[fileid].append(newrow)
                 else:
                     self._fileindex[fileid] = [newrow]
-                if self._varindex.haskey(variableid):
+                if variableid in self._varindex.keys():
                     self._varindex[variableid].append(newrow)
                 else:
                     self._varindex[variableid] = [newrow]
@@ -111,53 +124,57 @@ class basic_support:
     """Children of this class contain methods which support specific file types,
     and are used to build the file table.  Maybe later we'll put here methods
     to support other functionality."""
-    def get_timerange(): return None
-    def get_latrange(): return None
-    def get_lonrange(): return None
-    def get_levelrange(): return None
-    def interesting_variables(): return []
+    def get_timerange(self): return None
+    def get_latrange(self): return None
+    def get_lonrange(self): return None
+    def get_levelrange(self): return None
+    def interesting_variables(self): return []
 
 class No_support(basic_support):
     """Any unsupported file type gets this one."""
 
 class CF_support(basic_support):
     """NetCDF file conforming to the CF Conventions."""
-    def __init__(dfile):
+    def __init__(self,dfile):
         """dfile is an open file"""
         # There are many possible interesting variables!
         # As we add plots to the system, we'll need to expand this list:
         self._all_interesting_standard_names = [
-            'surface_air_pressure', 'surface_temperature' ]
-        _dfile = dfile
-    def interesting_variables():
+            'cloud_area_fraction', 'precipitation_flux', 'surface_air_pressure', 'surface_temperature' ]
+        self._dfile = dfile
+    def interesting_variables(self):
         iv = []
-        for var in _dfile.variables.keys():
-            if getattr( _dfile[var], 'standard_name', None ) in\
-                    self.all_interesting_standard_names:
-                iv.append(var)
+        print "will check variables",self._dfile.variables.keys()
+        for var in self._dfile.variables.keys():
+           standard_name = getattr( self._dfile[var], 'standard_name', None )
+           if standard_name!=None:
+              print "  ",var," has standard name",standard_name
+           if standard_name in\
+                  self._all_interesting_standard_names:
+              iv.append(var)
         return iv
-    def get_timerange():
-        time_bnds_name = _dfile.axes['time'].bounds
-        lo = f[time_bnds_name][0][0]
-        hi = f[time_bnds_name][-1][1]
-        units = dfile.axes['time'].units
+    def get_timerange(self):
+        time_bnds_name = self._dfile.axes['time'].bounds
+        lo = self._dfile[time_bnds_name][0][0]
+        hi = self._dfile[time_bnds_name][-1][1]
+        units = self._dfile.axes['time'].units
         return drange( lo, hi, units )
-    def get_latrange():
-        lat_bnds_name = _dfile.axes['lat'].bounds
-        lo = f[lat_bnds_name][0][0]
-        hi = f[lat_bnds_name][-1][1]
-        units = dfile.axes['lat'].units
+    def get_latrange(self):
+        lat_bnds_name = self._dfile.axes['lat'].bounds
+        lo = self._dfile[lat_bnds_name][0][0]
+        hi = self._dfile[lat_bnds_name][-1][1]
+        units = self._dfile.axes['lat'].units
         return drange( lo, hi, units )
-    def get_lonrange():
-        lon_bnds_name = _dfile.axes['lon'].bounds
-        lo = f[lon_bnds_name][0][0]
-        hi = f[lon_bnds_name][-1][1]
-        units = dfile.axes['lon'].units
+    def get_lonrange(self):
+        lon_bnds_name = self._dfile.axes['lon'].bounds
+        lo = self._dfile[lon_bnds_name][0][0]
+        hi = self._dfile[lon_bnds_name][-1][1]
+        units = self._dfile.axes['lon'].units
         return drange( lo, hi, units )
-    def get_levelrange():
+    def get_levelrange(self):
         levelaxis = None
-        for axis_name in dfile.axes.keys():
-            axis = dfile[axis_name]
+        for axis_name in self._dfile.axes.keys():
+            axis = self._dfile[axis_name]
             if hasattr( axis, 'positive' ):
                 # The CF Conventions specifies this as a way to detect a vertical axis.
                 levelaxis = axis
@@ -170,12 +187,12 @@ class CF_support(basic_support):
         return drange( lo, hi, units )
 
 if __name__ == '__main__':
-    if len( sys.argv ) > 1:
-        from findfiles import *
-        datafiles = treeof_datafiles( sys.argv[1] )
-        print "datafiles=", datafiles
-        filetable = basic_filetable( datafiles )
-        print "filetable=", filetable
-    else:
-        print "usage: findfiles.py root"
+   if len( sys.argv ) > 1:
+      from findfiles import *
+      datafiles = treeof_datafiles( sys.argv[1] )
+      print "datafiles=", datafiles
+      filetable = basic_filetable( datafiles )
+      print "filetable=", filetable.sort()
+   else:
+      print "usage: filetable.py root"
 
