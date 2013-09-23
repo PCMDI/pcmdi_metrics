d0bd2c1f0096412fd957a3b09c9f35f49d6f8faa
Peter Gleckler <gleckler1@llnl.gov>
2013-09-23 15:26:22 -0700

    Testing..

--- src/python/wgne/io.py
+++ src/python/wgne/io.py
@@ -15,12 +15,22 @@ obs_dic = {'rlut':{'default':'CERES','alternate':'ERBE'},
            'ta':{'default':'ERAINT','ref3':'JRA25','alternate':'rnl_ncep'},
            'zg':{'default':'ERAINT','ref3':'JRA25','alternate':'rnl_ncep'},
            'tos':{'default':'HadISST'},
-           'zos':{'default':'CNES_AVISO'},
+           'zos':{'default':'CNES_AVISO_L4'},
            'sos':{'default':'WOA09'},
             }
 
+### ADDED BY PJG
+obs_period = {'ERAINT':{'period':'000001-000012'},
+              'CERES':{'period': '000001-000012'},
+              'GPCP':{'period': '000001-000012'},
+              'HadISST':{'period':'198001-200512'},
+              'WOA09':  {'period':'177201-200812'},
+              'CNES_AVISO_L4':{'period':'199201-200512'}
+          }
+
 class OBS(metrics.io.base.Base):
     def __init__(self,root,var,reference="default",period="198001-200512"):
+        period = obs_period[obs_dic[var][reference]]['period']  ### ADDED BY PJG
         template = "%s/%s/ac/%s_%s_%%(period)_ac.%%(ext)" % (var,obs_dic[var][reference],var,obs_dic[var][reference])
         metrics.io.base.Base.__init__(self,root,template)
         if var in ['tos','sos','zos']:
