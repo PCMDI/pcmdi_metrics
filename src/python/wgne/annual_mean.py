d689ff32a002b62b9070a7db1b0e7bcac71088f2
Peter Gleckler <gleckler1@llnl.gov>
2013-09-24 13:20:55 -0700

    Added DJF seasonal metrics

--- src/python/wgne/annual_mean.py
+++ src/python/wgne/annual_mean.py
@@ -1,6 +1,6 @@
 import cdutil,cdms2
 
 def compute(dm,do):
-    """ Computes bias"""
+    """ Computes ANNUAL MEAN"""
     cdms2.setAutoBounds('on') # Do we really want this? Wouldn't it better to let it fails
     return cdutil.averager(dm,axis='t'),cdutil.averager(do,axis='t')
