91ee5daff353f54aa765e6734b0c92b91464850b
Charles Doutriaux <doutriaux1@llnl.gov>
2014-10-30 17:21:41 -0700

    uncommented out first test case

--- test/test_suite.py
+++ test/test_suite.py
@@ -5,7 +5,7 @@ import test_portrait
 
 suite= unittest.TestSuite()
 
-#suite.addTest(test_from_param.TestFromParam("test/pcmdi/basic_test_parameters_file.py"))
+suite.addTest(test_from_param.TestFromParam("test/pcmdi/basic_test_parameters_file.py"))
 suite.addTest(test_portrait.TestGraphics("test_portrait"))
 
 unittest.TextTestRunner(verbosity=2).run(suite)
