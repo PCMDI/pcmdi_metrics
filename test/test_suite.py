7324502adca0cc7996cc7c371de9d5338f6b3f9d
Charles Doutriaux <doutriaux1@llnl.gov>
2014-10-30 17:05:31 -0700

    Adding test_protrait to test suite

--- test/test_suite.py
+++ test/test_suite.py
@@ -1,8 +1,12 @@
 import unittest,test_from_param
+import sys
+sys.path.append("test/graphics")
+import test_portrait
 
 suite= unittest.TestSuite()
 
 suite.addTest(test_from_param.TestFromParam("test/pcmdi/basic_test_parameters_file.py"))
+suite.addTest(test_portrait.TestGraphics("test_portrait"))
 
 unittest.TextTestRunner(verbosity=2).run(suite)
 
