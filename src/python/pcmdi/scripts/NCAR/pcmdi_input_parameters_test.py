bd8349e62134564e452fd0604ecdd41a181cf274
Paul J. Durack <durack1@llnl.gov>
2014-11-25 09:58:25 -0800

    Updated parameter file

--- src/python/pcmdi/scripts/NCAR/pcmdi_input_parameters_test.py
+++ src/python/pcmdi/scripts/NCAR/pcmdi_input_parameters_test.py
@@ -1,63 +1,53 @@
-import genutil
-import getpass
-
-buildDate = '140922' ; # Must be set to allow correct metrics install to be picked up
+import os
 
 ################################################################################
-#  OPTIONS ARE SET BY USER IN THIS FILE AS INDICATED BELOW BY: 
-#
+#  OPTIONS ARE SET BY USER IN THIS FILE AS INDICATED BELOW: 
 ################################################################################
 
-## RUN IDENTIFICATION
-# DEFINES A SUBDIRECTORY TO METRICS OUTPUT RESULTS SO MULTIPLE CASES CAN BE COMPARED
-case_id = 'sampletest'
-# LIST OF MODEL VERSIONS TO BE TESTED - WHICH ARE EXPECTED TO BE PART OF CLIMATOLOGY FILENAME
-model_versions = ['GFDL-CM4','GFDL-ESM2G',]
+## METRICS RUN IDENTIFICATION
+caseId = 'sampletest_141104' ; # Defines a subdirectory to output metrics results so multiple runs can be compared
 
-### VARIABLES AND OBSERVATIONS TO USE
-vars = ['pr','tos']
-#vars = ['pr','tas','rlut','rsut','hus_850']
-#vars = ['ta_850','ta_200','ua_850','ua_200','va_850','va_200','zg_500','rlut','rsut','rlutcs','rsutcs','tas']
-#vars = ['pr','psl','rlut','rlutcs','rsut','rsutcs','ta_200','ta_850','tas','tauu','tauv','ua_200','ua_850','va_200','va_850','vas','zg_500']
+## LIST OF MODEL VERSIONS TO BE TESTED - WHICH ARE EXPECTED TO BE PART OF CLIMATOLOGY FILENAME
+modelVersions  = ['NCAR-CAM5_1deg','NCAR-CAM5_0p25deg'] ; # Model identifier
+modelPeriod    = '01-12' ; # Model climatological period (if relevant)
+realization     = 'r1i1p1' ; # Model run identifier (if relevant)
 
-# Observations to use at the moment "default" or "alternate"
-ref = 'default'  #'all'
-ext = '.nc' ; # '.xml'
+## VARIABLES AND OBSERVATIONS TO USE
+# Variable acronyms are described in the CMIP5 standard output document - http://cmip-pcmdi.llnl.gov/cmip5/docs/standard_output.pdf
+vars = ['pr','prw','tas'] ; # NCAR atmos subsuite test
+#vars = ['pr','psl','rlut','rlutcs','rsut','rsutcs','ta_200','ta_850','tas','ua_200','ua_850','va_200','va_850','zg_500'] ; # GFDL atmos test suite
+#vars = ['clt','hfss','pr','prw','psl','rlut','rlutcs','rsdt','rsut','rsutcs','tas','tauu','tauv','ts','uas','vas'] ; # 2d atmos variables
+#vars = ['hur','hus','huss','ta','ua','va','zg'] ; # 3d atmos variables
+#vars = ['hus_850','ta_850','ta_200','ua_850','ua_200','va_850','va_200','zg_500'] ; # 3d atmos variables - example heights
+#vars = ['sos','tos','zos'] ; # 2d ocean variables
+#vars = ['rlwcrf','rswcrf'] ; # Non-standard CMIP5 variables (available from obs output)
 
-# INTERPOLATION OPTIONS
-targetGrid        = '2.5x2.5' # OPTIONS: '2.5x2.5' or an actual cdms2 grid object
-regrid_tool       = 'esmf' #'regrid2' # OPTIONS: 'regrid2','esmf'
-regrid_method     = 'linear'  # OPTIONS: 'linear','conservative', only if tool is esmf
-regrid_tool_ocn   = 'esmf'    # OPTIONS: "regrid2","esmf"
-regrid_method_ocn = 'linear'  # OPTIONS: 'linear','conservative', only if tool is esmf
+# Observations to use 'default', 'alternate' or specific enumerated climatology e.g. 'ref3'
+ref = ['default'] ; #,'all','alternate','ref3'
 
-# SIMULATION PARAMETERS
-model_period = '01-12'
-realization = 'r1i1p1'
+## INTERPOLATION OPTIONS
+targetGrid          = '2.5x2.5' # Options: '2.5x2.5' or an actual cdms2 grid object
+regrid_tool         = 'regrid2' # Options: 'regrid2','esmf'
+regrid_method       = 'linear'  # Options: 'linear','conservative', only if tool is esmf
+regrid_tool_ocn     = 'esmf'    # Options: 'regrid2','esmf' - Note regrid2 will fail with non lat/lon grids
+regrid_method_ocn   = 'linear'  # Options: 'linear','conservative', only if tool is esmf
+save_mod_clims      = True      # Options: True or False (Save interpolated model climatologies used in metrics calculations)
 
-# SAVE INTERPOLATED MODEL CLIMATOLOGIES ?
-save_mod_clims = True # True or False
+## DATA LOCATION: MODELS, OBS AND METRICS OUTPUT - AND TEMPLATES FOR MODEL OUTPUT CLIMATOLOGY FILES
+baseDir                         = '/glade/u/home/durack1/141104_metrics/'
+# Template example: tas_GFDL-ESM2G_Amon_historical_r1i1p1_198001-199912-clim.nc
+filename_template               = "%(variable)_%(modelVersion)_%(table)_historical_%(realization)_%(modelPeriod)-clim.nc"
+# ROOT PATH FOR MODELS CLIMATOLOGIES
+mod_data_path                   = os.path.join(baseDir,'test_new')
+# ROOT PATH FOR OBSERVATIONS
+obs_data_path                   = '/glade/u/home/durack1/obs'
+# DIRECTORY WHERE TO PUT RESULTS - will create case_id subdirectory
+metrics_output_path             = os.path.join(baseDir,caseId)
+# DIRECTORY WHERE TO PUT INTERPOLATED MODELS' CLIMATOLOGIES - will create case_id subdirectory
+model_clims_interpolated_output = metrics_output_path
+# FILENAME FOR INTERPOLATED CLIMATOLOGIES OUTPUT
+filename_output_template        = "%(variable)%(level)_%(modelVersion)_%(table)_%(realization)_%(modelPeriod)_interpolated_%(regridMethod)_%(targetGridName)-clim.nc"
 
-## DATA LOCATION: MODELS, OBS AND METRICS OUTPUT
-
-## Templates for climatology files
-## TEMPLATE EXAMPLE: cmip5.GFDL-ESM2G.historical.r1i1p1.mo.atm.Amon.rlut.ver-1.1980-1999.AC.nc
-filename_template = "%(variable)_%(model_version)_%(table)_historical_%(realization)_%(model_period)-clim.nc"
-
-## dictionary for custom %(keyword) designed by user
+## DICTIONARY FOR CUSTOM %(keyword) IMPLEMENTED BY USER FOR CUSTOM METRICS
 # Driver will match each key to its value defined by a variable name OR all if variable name is not present, OR "" if "all" is not defined
-#custom_keys = { "key1": {"all":"key1_value_for_all_var", "tas" : "key1_value_for_tas"},
-#    }
-
-## ROOT PATH FOR MODELS CLIMATOLOGIES
-#mod_data_path = ''.join(['/home/p1d/',buildDate,'_metrics/test/'])
-mod_data_path = ''.join(['/home/',getpass.getuser(),'/',buildDate,'_metrics/test/'])
-## ROOT PATH FOR OBSERVATIONS
-#obs_data_path = '/home/p1d/obs/'
-obs_data_path = ''.join(['/home/',getpass.getuser(),'/obs/'])
-## DIRECTORY WHERE TO PUT RESULTS
-metrics_output_path = './metrics_output_path'
-## DIRECTORY WHERE TO PUT INTERPOLATED MODELS' CLIMATOLOGIES
-model_clims_interpolated_output = './metrics_output_path/Interpolation_Output'
-## FILENAME FOR INTERPOLATED CLIMATOLOGIES OUTPUT
-filename_output_template = "%(model_version).historical.r1i1p1.mo.%(table_realm).%(variable)%(level).ver-1.%(period).interpolated.%(regridMethod).%(targetGridName).AC%(ext)"
+custom_keys = { "key1": {"all":"key1_value_for_all_var", "tas" : "key1_value_for_tas"}, }
