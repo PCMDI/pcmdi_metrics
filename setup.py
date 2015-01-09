5ecda54d69e32dfc6333abf5d1ab43a1d4a47338
Charles Doutriaux <doutriaux1@llnl.gov>
2015-01-08 18:13:05 -0800

    Seem to have cleaned up setup.py back to pre @durack1 madness

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ demo_ACME_files         = glob.glob("demo/ACME/*.py")
 demo_GFDL_files         = glob.glob("demo/GFDL/*.py")
 demo_NCAR_files         = glob.glob("demo/NCAR/*.py")
 param_files             = glob.glob("doc/parameter_files/*.py")
-test_data_files         = glob.glob("test/pcmdi/data/*.nc")
-test_py_files           = glob.glob("test/pcmdi/*.py")
 
 setup (name         = 'pcmdi_metrics',
        version      = descr,
@@ -56,16 +54,6 @@ setup (name         = 'pcmdi_metrics',
                        ('share/CMIP_metrics_results/CMIP5/piControl',cmip5_piControl_json),
                        ('share/graphics/vcs',portrait_files),
                        ('share/pcmdi',('doc/obs_info_dictionary.json',)),
-                       ('test',('test/test_from_param.py','test/test_suite.py',)),
-                       ('test/pcmdi',test_py_files),
-                       ('test/pcmdi/data',test_data_files),
-                       ('test/pcmdi/gensftlfTest',('test/pcmdi/gensftlfTest/tas_2.5x2.5_esmf_linear_metrics.json',)),
-                       ('test/pcmdi/installationTest',('test/pcmdi/installationTest/tas_2.5x2.5_regrid2_linear_metrics.json','test/pcmdi/installationTest/tos_2.5x2.5_esmf_linear_metrics.json',)),
-                       ('test/pcmdi/nosftlfTest',('test/pcmdi/nosftlfTest/tas_2.5x2.5_esmf_linear_metrics.json',)),
-                       ('test/pcmdi/obs/atm/mo/tas/ERAINT/ac',('test/pcmdi/obs/atm/mo/tas/ERAINT/ac/tas_pcmdi-metrics_Amon_ERAINT_198901-200911-clim.nc',)),
-                       ('test/pcmdi/obs/fx/sftlf/ERAINT',('test/pcmdi/obs/fx/sftlf/ERAINT/sftlf_pcmdi-metrics_fx_ECMWF-ERAInterim_197901-201407.nc',)),
-                       ('test/pcmdi/obs/ocn/mo/tos/UKMETOFFICE-HadISST-v1-1/ac',('test/pcmdi/obs/ocn/mo/tos/UKMETOFFICE-HadISST-v1-1/ac/tos_pcmdi-metrics_Omon_UKMETOFFICE-HadISST-v1-1_198002-200501-clim.nc',)),
-                       ('test/pcmdi/unitsTest',('test/pcmdi/unitsTest/tas_2.5x2.5_esmf_linear_metrics.json',)),
                       ]
        #include_dirs = [numpy.lib.utils.get_include()],
        #ext_modules = [
