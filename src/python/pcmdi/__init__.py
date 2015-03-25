3010f04ec959a6e297b2646127bde3836c263951
Marie-Alice.Foujols@ipsl.jussieu.fr <igcmg@ciclad-ng.private.ipsl.fr>
2015-03-25 16:44:53 +0100

    Removed Ext_Info stuff

--- src/python/pcmdi/__init__.py
+++ src/python/pcmdi/__init__.py
@@ -1,6 +1,5 @@
 import io
 from mean_climate_metrics_calculations import compute_metrics
-from mean_climate_metrics_calculations_ExtInfo import compute_metrics_ExtInfo
 
 import rms_xyt,cor_xyt,bias,rms_xy,annual_mean, cor_xy, meanabs_xy, seasonal_mean
 
