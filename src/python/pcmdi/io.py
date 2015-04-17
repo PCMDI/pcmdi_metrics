c4f3c2ec39e29370a18eb035ffecebf881a032a5
Charles Doutriaux <doutriaux1@llnl.gov>
2015-04-17 11:57:03 -0700

    fix #256 for real

--- src/python/pcmdi/io.py
+++ src/python/pcmdi/io.py
@@ -4,6 +4,7 @@ class OBS(pcmdi_metrics.io.base.Base):
       
         template = "%(realm)/%(frequency)/%(variable)/%(reference)/%(ac)/%(filename)"
         pcmdi_metrics.io.base.Base.__init__(self,root,template)
+        print "REF:",reference
         obs_name = obs_dic[var][reference]
         ## usually send "default", "alternate", etc
         # but some case (sftlf) we send the actual name
