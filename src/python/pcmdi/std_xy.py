6c2d7479ffd22458c52f2ccb89d73f5b3c553d19
Peter Gleckler <gleckler1@llnl.gov>
2015-10-30 15:11:15 -0700

    Added std functions

---
+++ src/python/pcmdi/std_xy.py
@@ -0,0 +1,14 @@
+import genutil
+
+
+def compute(d):
+    """ Computes std"""
+    if dm is None and do is None:  # just want the doc
+        return {
+            "Name": "Spatial Standard Deviation",
+            "Abstract": "Compute Spatial Standard Deviation",
+            "URI": "http://uvcdat.llnl.gov/documentation/" +
+            "utilities/utilities-2.html",
+            "Contact": "Peter Gleckler <gleckler1@llnl.gov>",
+        }
+    return float(genutil.statistics.std(d, axis='xy', weights='weighted'))
