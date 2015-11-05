fd4ba8ea238f1cadef000ad3b4172c5b114d6e84
Paul J. Durack <durack1@llnl.gov>
2015-11-04 16:53:14 -0800

    Added Python build from source option

---
+++ upgradePython.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -ex
+wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+tar -xzvf Python-2.7.10.tgz
+cd Python-2.7.10 && ./configure --prefix=/usr && make && sudo make install
+
+# Tips: http://docs.travis-ci.com/user/installing-dependencies/#Installing-Projects-from-Source
