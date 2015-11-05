5575f09d72124ea2db4fa70b1e883d963d2b84b0
Paul J. Durack <durack1@llnl.gov>
2015-11-05 09:25:27 -0800

    Added setuptools build to check non-build Python compatibility

--- upgradePython.sh
+++ upgradePython.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 set -ex
 
-# Python
-curl -O https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-tar -xzvf Python-2.7.10.tgz
-cd Python-2.7.10 && ./configure --prefix=/usr && make && sudo make install
-
 # Expat
 curl -O http://tcpdiag.dl.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz
 tar -xzvf expat-2.1.0.tar.gz
 chmod -R 755 expat-2.1.0
 cd expat-2.1.0 && ./configure --prefix=/usr && make && sudo make install
+
+# Python
+curl -O https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+tar -xzvf Python-2.7.10.tgz
+cd Python-2.7.10 && ./configure --prefix=/usr && make && sudo make install
+
+# Setuptools 1.4
+curl -O http://uv-cdat.llnl.gov/cdat/resources/setuptools-1.4.tar.gz
+tar -xzvf setuptools-1.4.tar.gz
+cd setuptools-1.4 && ./configure --prefix=/usr && make && sudo make install
+
+# Setuptools 18.5
+curl -O http://uv-cdat.llnl.gov/cdat/resources/setuptools-18.5.tar.gz
+tar -xzvf setuptools-18.5.tar.gz
+cd setuptools-18.5 && ./configure --prefix=/usr && make && sudo make install
 
 # Tips: http://docs.travis-ci.com/user/installing-dependencies/#Installing-Projects-from-Source
