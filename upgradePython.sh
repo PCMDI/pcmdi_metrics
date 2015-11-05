68f044b58425beaed61b48ee2a27228749fbb069
Paul J. Durack <durack1@llnl.gov>
2015-11-05 07:43:18 -0800

    Add more python/expat diagnostic prints

--- upgradePython.sh
+++ upgradePython.sh
@@ -2,9 +2,9 @@
 set -ex
 
 # Python
-#curl -O https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-#tar -xzvf Python-2.7.10.tgz
-#cd Python-2.7.10 && ./configure --prefix=/usr && make && sudo make install
+curl -O https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+tar -xzvf Python-2.7.10.tgz
+cd Python-2.7.10 && ./configure --prefix=/usr && make && sudo make install
 
 # Expat
 curl -O http://tcpdiag.dl.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz
