32134ca21ad2d379711b9a971fd6f1e74ccd4517
Paul J. Durack <durack1@llnl.gov>
2015-11-04 17:04:12 -0800

    Corrected OS X download to use curl not wget

--- upgradePython.sh
+++ upgradePython.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+curl -O https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
 tar -xzvf Python-2.7.10.tgz
 cd Python-2.7.10 && ./configure --prefix=/usr && make && sudo make install
 
