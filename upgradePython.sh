3f2631893e3c8c5100398ec88c70ef4d4ec46a84
Paul J. Durack <durack1@llnl.gov>
2015-11-06 15:48:47 -0800

    Try python2710 build with --with-system-expat build statement

--- upgradePython.sh
+++ upgradePython.sh
@@ -10,14 +10,14 @@
 #sudo chmod -R 777 /Users/travis/build/PCMDI
 
 # Python
-#curl -O https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-#tar -xzvf Python-2.7.10.tgz
-#cd Python-2.7.10 && ./configure --prefix=/usr && make && sudo make install
+curl -O https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+tar -xzvf Python-2.7.10.tgz
+cd Python-2.7.10 && ./configure --prefix=/usr --with-system-expat && make && sudo make install
 
 # Setuptools 1.4
-#curl -O http://uv-cdat.llnl.gov/cdat/resources/setuptools-1.4.tar.gz
-#tar -xzvf setuptools-1.4.tar.gz
-#cd setuptools-1.4 && python setup.py build && sudo python setup.py install
+curl -O http://uv-cdat.llnl.gov/cdat/resources/setuptools-1.4.tar.gz
+tar -xzvf setuptools-1.4.tar.gz
+cd setuptools-1.4 && python setup.py build && sudo python setup.py install
 
 # Setuptools 18.5
 #curl -O http://uv-cdat.llnl.gov/cdat/resources/setuptools-18.5.tar.gz
