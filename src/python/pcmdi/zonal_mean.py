c787d4c39aca2a075e01587c60fde68cb2ccb205
Peter Gleckler <gleckler1@llnl.gov>
2016-02-17 13:13:01 -0800

    adding zonal_mean function

---
+++ src/python/pcmdi/zonal_mean.py
@@ -0,0 +1,19 @@
+import cdutil
+import cdms2
+
+
+def compute(dm, do):
+    """ Computes ZONAL MEAN"""
+    if dm is None and do is None:  # just want the doc
+        return {
+            "Name": "Zonal Mean",
+            "Abstract": "Compute Zonal Mean",
+            "URI":
+                    "http://uvcdat.llnl.gov/documentation/" +
+                    "utilities/utilities-2.html",
+            "Contact": "Peter Gleckler <gleckler1@llnl.gov>",
+            "Comments": ""
+        }
+    # Do we really want this? Wouldn't it better to let it fails
+    cdms2.setAutoBounds('on')
+    return cdutil.averager(dm, axis='x'), cdutil.averager(do, axis='x')
