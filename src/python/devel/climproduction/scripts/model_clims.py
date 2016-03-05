b381992919e1155b367bbbba280c34046066798e
Peter Gleckler <gleckler1@llnl.gov>
2016-03-04 19:29:17 -0800

    begin cleaning code

--- src/python/devel/climproduction/scripts/model_clims.py
+++ src/python/devel/climproduction/scripts/model_clims.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
 #######################################################################################
-### This program uses pcmdi_compute_climatologies.py for mass prodduction of CMORized model climatololgies from data available from PCMDIs CMIP database. 
-
+# THIS PROGRAM USES pcmdi_compute_climatologies.py FOR MASS PRODUCTION OF CMORized MODEL CLIMATOLOGIES FROM DATA AVAILABEL FROM PCMDIS CMIP DATABASE
 
 #######################################################################################
 
-
+import pcmdi_metrics
 import cdms2 as cdms
 import os, string
 import cdtime, cdutil
@@ -17,31 +16,39 @@ import time
 from subprocess import call,Popen,PIPE
 
 
+#ClimCodePath = os.path.join( pcmdi_metrics.__path__[0], "..", "..", "..", "..", "share", "pcmdi", "scripts", "pcmdi_compute_climatologies.py")
+ClimCodePath = 'pcmdi_compute_climatologies.py'
+ClimCodePath = ClimCodePath + ' -f'
+
+
+#ClimCodePath = '/export/gleckler1/git/pcmdi_metrics/src/python/pcmdi/scripts/pcmdi_compute_climatologies.py -f'
+
+
+#### PARAMETERS SET BY USER #######################################################################################################
+#EXPERIMENT
 exp = 'cmip5-historical'
 exp = 'cmip5-amip'
 
-pathout_basedir = '/work/gleckler1/processed_data/'
+# OUTPUT TARGET ROOT DIRECTORY
+#pathout_basedir = '/work/gleckler1/processed_data/'
+outpath_base = '/work/metricspackage/mod_clims/'
+
+# VARIABLES TO COMPUTE
+#vars = ['rlut','rsdt','pr','ts','ta']
+#vars = ['rlut','rsdt','rsut','rsutcs','rlutcs','rsds','rsus','rlus','rlds','rsuscs','rsdscs','rldscs','pr','prw','psl','ts','tas','tauu','tauv','uas','vas','huss', 'ta','ua','va','zg','hus']
+vars = ['rsdt']
+
+#### END PARAMETERS SET BY USER #######################################################################################
 
 if exp == 'cmip5-historical':
   pathin_template = '/work/cmip5/historical/REALM/mo/'
   pathin = '/work/cmip5/historical/atm/mo/'
-  pathout = pathout_basedir + 'cmip5clims_CMOR-historical' 
 
 if exp == 'cmip5-amip':
   pathin_template = '/work/cmip5/amip/REALM/mo/'
   pathin = '/work/cmip5/amip/atm/mo/'
-  pathout = pathout_basedir + 'cmip5clims_CMOR-amip'
 
 
-vars = ['rlut','rsdt','pr','ts','ta']
-
-vars = ['rlut','rsdt','rsut','rsutcs','rlutcs','rsds','rsus','rlus','rlds','rsuscs','rsdscs','rldscs','pr','prw','psl','ts','tas','tauu','tauv','uas','vas','huss', 'ta','ua','va','zg','hus']
-
-vars = ['rsdt']
-
-ClimCodePath = '/export/gleckler1/git/pcmdi_metrics/src/python/pcmdi/scripts/pcmdi_compute_climatologies.py -f'
-
-outpath_base = '/work/metricspackage/mod_clims/'
 
 try:
  os.mkdir(outpath_base + exp)
@@ -65,15 +72,17 @@ for var in vars:
   for run in modruns:
      mod = string.split(run,'.')[1]    
  
-     cmd1 = run[:-1] + ' -v ' + var + ' -X ' + '"{"positive":"up"}"  -s 1979-01 -e 1989-12 -T pcmdi_metrics' 
-
-     cmd1 = run[:-1] + ' -v ' + var + ' -X ' + "'{" + '"positive"' + ':' + '"up"' + "}'"  + ' -s 1979-01 -e 1989-12 -T ../lib/CMOR_Amon_pmp_02052016 '  + '-O ' + outpath  
-     cmd1 = run[:-1] + ' -v ' + var + positive + ' -s 1979-01 -e 1989-12 -T pcmdi_metrics '  + '-O ' + outpath  
-
+#    cmd1 = run[:-1] + ' -v ' + var + ' -X ' + '"{"positive":"up"}"  -s 1979-01 -e 1989-12 -T pcmdi_metrics' 
+#    cmd1 = run[:-1] + ' -v ' + var + ' -X ' + "'{" + '"positive"' + ':' + '"up"' + "}'"  + ' -s 1979-01 -e 1989-12 -T ../lib/CMOR_Amon_pmp_02052016 '  + '-O ' + outpath  
+#    cmd1 = run[:-1] + ' -v ' + var + positive + ' -s 1979-01 -e 1989-12 -T pcmdi_metrics '  + '-O ' + outpath  
+     cmd1 = run[:-1] + ' -v ' + var + positive + ' -s 1979-01 -e 1989-12 -T CMOR_Amon_pmp_02052016 '  + '-O ' + outpath
 
      print run
 
      cmd = ClimCodePath + ' ' + cmd1 
+     cmd = 'pcmdi_compute_climatologies.py -f ' + cmd1
+
+
 
      print cmd
 
