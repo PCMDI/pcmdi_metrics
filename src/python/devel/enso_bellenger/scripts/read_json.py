3ecf1a93fbc0c098206ecbbd9fb68e8d48422463
Ji-Woo Lee <lee1043@crunchy.llnl.gov>
2016-04-29 11:29:08 -0700

    Add century scale variability plots for monthly std. dev.

--- src/python/devel/enso_bellenger/scripts/read_json.py
+++ src/python/devel/enso_bellenger/scripts/read_json.py
@@ -1,14 +1,39 @@
 import json
+import string
 
 execfile('./plot_metrics.py')
 execfile('./plot_metrics2.py')
 
-var = 'ts'
-#var = 'pr'
-
 test = True
 
-with open('test_ENSO_cmip5_piControl_'+var+'.json') as json_data:
+way1 = True
+way1 = False
+
+if way1:
+  mip = 'cmip5'
+  exp = 'piControl'
+  run = 'r1i1p1'
+  fq = 'mo'
+  realm = 'atm'
+  var = 'ts'
+  #var = 'pr'
+
+  json_filename = 'ENSO_' + mip + '_' + exp + '_' + run + '_' + fq + '_' +realm + '_' + var
+
+else:
+  json_file = 'ENSO_cmip5_piControl_r1i1p1_mo_atm_ts.json'
+  json_filename = string.split(json_file,'.')[0]
+
+  mip = string.split(json_filename,'_')[1]
+  exp = string.split(json_filename,'_')[2]
+  run = string.split(json_filename,'_')[3]
+  fq = string.split(json_filename,'_')[4]
+  realm = string.split(json_filename,'_')[5]
+  var = string.split(json_filename,'_')[6]
+
+  print mip, exp, run, fq, realm, var
+
+with open(json_filename+'.json') as json_data:
     d = json.load(json_data)
     #if test == True:
     #  print(d)
@@ -21,17 +46,23 @@ if test:
 else:
   regs = ['Nino34', 'Nino3', 'Nino4', 'Nino12','TSA','TNA','IO']
 
+mods = d.keys()
+mods.sort()
+
+reg_time = []
+for mod in mods:
+  reg_time.append(d[mod]['reg_time'])
+
 for reg in regs:
-  mods = d.keys()
-  mods.sort()
+
   tmp = []
   for mod in mods:
-    tmp.append(d[mod][reg]['std'])
+    tmp.append(d[mod][reg]['std']['entire'])
   stdv[reg] = tmp[:]
   plot_metrics(reg,mods)
 
   del tmp[:]
   for mod in mods:
-    tmp.append(float(d[mod][reg]['std_NDJ'])/float(d[mod][reg]['std_MAM']))
+    tmp.append(float(d[mod][reg]['std_NDJ']['entire'])/float(d[mod][reg]['std_MAM']['entire']))
   stdv[reg] = tmp[:]
-  plot_metrics2(reg,mods)
+  #plot_metrics2(reg,mods)
