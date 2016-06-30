874ccbe7d418ccc6df7f8f1e3570749586023925
Peter Gleckler <gleckler1@llnl.gov>
2016-06-30 13:57:49 -0700

    script updated with input and output provided via argparse

--- demo/external-setups/NCAR/time_model_xml_generation.py
+++ demo/external-setups/NCAR/time_model_xml_generation.py
@@ -1,8 +1,8 @@
 #### USED TO GENERATE XML FILES FOR NCAR CLIMATOLGIES
-# NCAR PRODUCES CLIMATOLOGIES FOR EACH CALENDAR MONTH AS SEPERATE FILES
-# XMLS PRODUCED BY THIS CODES COMBINE THESE SO THAT THE MONTHLY CLIMATOLOGICAL ANNUAL CYCLE CAN BE READ BY A SINGLE (XML) FILE  
 
-## THIS CODE NEEDS TO BE IMPROVED USING argparse SO THAT OPTIONS CAN BE SENT VIA COMMAND LINE.  CURRENTLY HARDWIRED
+# NCAR OUTPUT PRODUCES CLIMATOLOGIES FOR EACH CALENDAR MONTH AS SEPERATE FILES
+# XMLS PRODUCED BY THIS CODES COMBINE THESE SO THAT THE MONTHLY CLIMATOLOGICAL ANNUAL CYCLE CAN BE READ BY A SINGLE (XML) FILE  
+# THE VARIABLES "filenamea" AND "filenameb" BELOW ARE NOT LIKELY TO WORK FOR THE GENERAL CASE.  THE LOGIC TO TRAP months "01", "02"... and "10", "11" and "12" MAY NEED TO BE MODIFIED DEPENDING ON THE FILES AVAILABLE. 
 
 # LAST UPDATE 6/29/16 PJG
 
@@ -12,6 +12,8 @@ import cdms2 as cdms
 import os, string
 import time
 import sys
+import argparse
+
 # Set cdms preferences - no compression, no shuffling, no complaining
 cdms.setNetcdfDeflateFlag(1)
 cdms.setNetcdfDeflateLevelFlag(9) ; # 1-9, min to max - Comes at heavy IO (read/write time cost)
@@ -20,42 +22,48 @@ cdms.setCompressionWarnings(0) ; # Turn off nag messages
 # Set bounds automagically
 #cdm.setAutoBounds(1) ; # Use with caution
 
+parser = argparse.ArgumentParser(description='Given a list of directories with simulation clims, use cdscan to produce xmls for annual cycle climatologies ')
+parser.add_argument("-b","--basedir",help="root directory below which subdirectories of individual simulations are expected")  #,default="*.nc",nargs="*")
+parser.add_argument("-o","--outdir",help="output directory",default=None)
 
-pathin = '/work/gleckler1/processed_data/ncar_clims/'
-#pathin = '/work/gleckler1/processed_data/ncar_clims/46L_cam5301_B03F2_taper2_D05_FAMIP/'
+args =parser.parse_args(sys.argv[1:])
 
-lst  = os.listdir(pathin)
+pathin = args.basedir # Get string from first index of list - only one directory expected 
 
-runs = []
+xmldir = args.outdir
+try:
+       os.mkdir(xmldir)
+except:
+       pass
 
-for l in lst:
-   if l not in ['ncar-older','older-older', 'ncar-crap']: runs.append(l)
+print 'pathin is...', pathin
+print 'pathout is...', xmldir 
 
-runs = ['46L_cam5301_B03F2_taper2_D05_FAMIP']
+# It is assummed that each subdirectory (run) with the path xmldir includes climatological files  
+runs  = os.listdir(pathin)
+
 print runs 
 
 for run in runs:
 
-     filename = run   #string.split(test_dic[exp][test],'/')[0]
-     filenamea = filename + '_0*_climo.nc' 
-     filenameb = filename + '_1*_climo.nc'
+     filename = run   
 
-#    cmd = 'cdscan -x text.xml '  +  pathin + '/' + test_dic[exp][test] + '/' + filename + '_[0-9]*_climo.nc' 
-
-     xmldir = 'xmls/'
-
+# In this case the clim files are in the run directory
      try:  
+      filenamea = filename + '.001_0*_climo.nc' 
+      filenameb = filename + '.001_1*_climo.nc'
       cmd = 'cdscan -x ' + xmldir + filename + '.xml ' + pathin + filename + '/' + filenamea  + ' ' + pathin + filename + '/' + filenameb  
       print cmd
-#    w = sys.stdin.readline()    
       os.popen(cmd).readlines()
      except:
       pass
 
+# In this case the clim files are in the run/climo directory
      try: 
+      filenamea = filename + '_0*_climo.nc' 
+      filenameb = filename + '_1*_climo.nc'
+
       cmd = 'cdscan -x ' + xmldir + filename + '.xml ' + pathin + filename + '/climo/' + filenamea  + ' ' + pathin + filename + '/climo/' + filenameb
-      print cmd
-#    w = sys.stdin.readline()    
       os.popen(cmd).readlines()
      except:
       pass
