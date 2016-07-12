b32edcb54e04b5201a063099467664943667f3e0
Peter Gleckler <gleckler1@llnl.gov>
2016-07-12 10:08:36 -0700

    updated setup file

--- demo/parameter_files/pmp_input_parameters_demo1.py
+++ demo/parameter_files/pmp_input_parameters_demo1.py
@@ -1,8 +1,11 @@
 ################################################################################
-#  SAMPLE INPUT PARAMETER FILE FOR THE PCMDI METRICS PACKAGE (PMP V1.1)
+#  THIS IS A DEMO INPUT PARAMETER FILE FOR THE PCMDI METRICS PACKAGE (PMP V1.1)
 #  THIS IS A VERY SIMPLE EXAMPLE, ONLY COMPUTING STATISTICS FOR ONE MODEL VERSION AND ONE VARIABLE 
 #  SAMPLE MODEL AND OBSERVATIONAL DATA TO RUN THIS CODE ARE AVAILABLE FROM: 
-# 
+#  http://oceanonly.llnl.gov/gleckler1/pmp-demo-data/pmpv1.1_demodata.tar
+#  Download this demo data and untar in the directory you plan to run the tests.  
+#  For more info, see https://github.com/PCMDI/pcmdi_metrics/wiki/Using-the-package
+
 # NOTES FOR PYTHON NEWBIES: ON ANY GIVEN LINE, ANTHING TO THE RIGHT OF A "#" IS CONSIDERED A COMMENT IN PYTHON  
 # IN THIS SIMPLE EXAMPLE WE DEFINE CHARACTER STRINGS, BUT ALSO ONE OF THE MOST BASIC AND POWERFUL PYTHON OBJECTS KNOWN AS A LIST.  
 # PYTHON LISTS ARE DEFINED WITH SQUARE BRACKETS [] ... FOR MORE INFO SEE: https://docs.python.org/2/tutorial/datastructures.html
@@ -14,6 +17,8 @@ model_versions = ['ACCESS1-0']   # THIS IS A MANDETORY ENTRY FOR DOCUMENTING RES
 ###############################################################################
 ## DATA LOCATION: MODELS, OBS AND METRICS OUTPUT
 ## ROOT PATH FOR MODELS CLIMATOLOGIES
+## THE MODEL AND OBSERVATIONAL DATA USED FOR THIS TEST CAN BE DOWNLOADED FROM: :wq
+
 #mod_data_path = '/work/metricspackage/mod_clims/cmip5-amip'
 mod_data_path = './cmip5clims_metrics_package-amip/'
 filename_template = "pr_ACCESS1-0_Amon_amip_r1i1p1_198001-200512-clim.nc"
