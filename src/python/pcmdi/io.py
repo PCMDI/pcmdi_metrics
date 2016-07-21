9c5fd6d5641022ff6c3da4af76cedfc73dad55fa
Charles Doutriaux <doutriaux1@llnl.gov>
2016-07-21 14:12:20 -0700

    first commit, nothing tested

--- src/python/pcmdi/io.py
+++ src/python/pcmdi/io.py
@@ -1,5 +1,5 @@
 import pcmdi_metrics
-
+import json
 
 class OBS(pcmdi_metrics.io.base.Base):
 
@@ -31,3 +31,82 @@ class OBS(pcmdi_metrics.io.base.Base):
         self.filename = ref["filename"]
         self.reference = obs_name
         self.variable = var
+
+class JSONs(object):
+    def __init__(self,files):
+        self.data = {}
+        self.json_version = "2.0"
+        for fnm in files:
+            f=open(fnm)
+            tmp_dict=json.load(f)
+            json_version=tmp_dict.get("json_version",None)
+            if json_version is None:
+                # Json format not stored, trying to guess
+                R = tmp_dict["RESULTS"]
+                out = R[R.keys()[0]]  # get first available model
+                out=out[out.keys()[0]]  # get first set of obs
+                k = out.keys()
+                k.pop("source")
+                out = out[k[0]] # first realization
+                # Ok at this point we need to see if it is json std 1 or 2
+                # version 1 had NHEX in every region
+                # version 2 does not
+                if "bias_xy_djf_NHEX" in out.keys():
+                    json_version = "1.0"
+                else:
+                    json_version = "2.0"
+            if json_version == "1.0":  # ok old way we need to convert to 2.0
+                for model in R.keys():  # loop through models
+                    m = R[model]
+                    refs = m.keys()
+                    refs.pop("SimulationDescription")
+                    for ref in m.keys():
+                        aref = m[ref]
+                        reals = aref.keys()
+                        src = reals.pop("source")
+                        for real in reals:
+                            areal = aref[real]
+                            areal2={"source":src}
+                            for region in areal.keys():
+                                reg = areal[region]
+                                if region == "global":
+                                    region2 = ""
+                                else:
+                                    region2=region+"_"
+                                areal2[region2+"global"]={}
+                                areal2[region2+"NHEX"]={}
+                                areal2[region2+"SHEX"]={}
+                                areal2[region2+"TROPICS"]{}
+                                key_stats = reg.keys()
+                                for k in key_stats:
+                                    if k[:7]=="custom_":
+                                        areal2[region][k]=reg[k]
+                                    else:
+                                        sp = k.split("_")
+                                        new_key = "_".join(sp[-1])
+                                        domain = sp[-1]
+                                        if domain == "GLB":
+                                            domain = "global"
+                                        areal2[region2+domain][new_key]=reg[k]
+                            # Now we can replace the realization with the correctly formatted one
+                            aref[areal] = areal2
+                        # restore ref into model
+                        m[ref]=aref
+                    # restore model into results
+                    R[m]=m
+            # Now update our stored results
+            # First we need to figure out the variable read in
+            var = tmp_dict.get("Variable",None)
+            if var is None:  # Not stored in json, need to get from file name
+                fnm = os.path.basename(fnm)
+                varnm = fnm.split("_")[0]
+            else:
+                varnm = var["id"]
+                if hasattr(var,"level"):
+                    varnm+="-"+int(var["level"]/100.)
+            if data.has_key(varnm):
+                self.data[varnm].update(R)
+            else:
+                self.data[varnm]=R
+    def getDimensions(self):
+        self.bla = pass
