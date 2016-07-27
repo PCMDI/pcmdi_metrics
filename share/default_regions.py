707ef4a9da686bf2a2eec18cfb49fb86d277ce7e
Ji-Woo Lee <lee1043@crunchy.llnl.gov>
2016-07-27 10:47:15 -0700

    Add domains for modes of variability

--- share/default_regions.py
+++ share/default_regions.py
@@ -13,6 +13,12 @@ regions_specs = {
         'ocean_SHEX' : {'value':0, 'domain':cdutil.region.domain(latitude=(-90.,-30,'ccb'))},
         'ocean_TROPICS': {'value':0, 'domain':cdutil.region.domain(latitude=(30.,30,'ccb'))},
         "ocean" : {'value':0, },
+        # Below is for modes of variability
+        "nam" : {'domian':cdutil.region.domain(latitude=(20.,90,'ccb'),longitude=(-180,180,'ccb'))},
+        "nao" : {'domian':cdutil.region.domain(latitude=(20.,80,'ccb'),longitude=(-90,40,'ccb'))},
+        "sam" : {'domian':cdutil.region.domain(latitude=(-20.,-90,'ccb'),longitude=(0,360,'ccb'))},
+        "pna" : {'domian':cdutil.region.domain(latitude=(20.,85,'ccb'),longitude=(120,240,'ccb'))},
+        "pdo" : {'domian':cdutil.region.domain(latitude=(20.,70,'ccb'),longitude=(110,260,'ccb'))},
         }
 
 default_regions = ['global','NHEX','SHEX','TROPICS']
