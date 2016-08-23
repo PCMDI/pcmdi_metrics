c5e07159c865f4ac9e75d741354778c5154fe776
Ji-Woo Lee <lee1043@crunchy.llnl.gov>
2016-08-23 16:39:23 -0700

    Add multiple realization

--- src/python/devel/variability_mode_nao/lib/get_pcmdi_data.py
+++ src/python/devel/variability_mode_nao/lib/get_pcmdi_data.py
@@ -7,6 +7,16 @@ def get_latest_pcmdi_mip_data_path(mip,exp,mod,fq,realm,var,run):
    latest = modelFileListTrimmed[0][:-1]
    return(latest)
 
+def get_latest_pcmdi_mip_data_path_as_list(mip,exp,mod,fq,realm,var,run):
+   #from durolib import trimModelList
+   if realm == 'atm': Realm = 'Amon'
+   if mip == 'cmip5': pin = '/work/' + mip + '/' + exp + '/' + realm + '/' + fq + '/' + var + '/' + mip + '.' + mod + '.' + exp + '.' + run + '.' + fq + '.' + realm + '.' + Realm + '.' + var + '*.xml'
+   lst0 = os.popen('ls ' + pin).readlines()
+   modelFileListTrimmed = trimModelList(lst0)
+   #latest = modelFileListTrimmed[0][:-1]
+   latest = map(str.rstrip, modelFileListTrimmed)
+   return(latest)
+
 def get_latest_pcmdi_mip_lf_data_path(mip,mod,var):
    #from durolib import trimModelList
    if mip == 'cmip5': pin = '/work/' + mip + '/fx/fx/' + var + '/' + mip + '.' + mod + '.*.xml'
@@ -17,7 +27,7 @@ def get_latest_pcmdi_mip_lf_data_path(mip,mod,var):
 
 def get_all_mip_mods(mip,exp,fq,realm,var):
    if realm == 'atm': Realm = 'Amon'
-   if mip == 'cmip5': pin = '/work/' + mip + '/' + exp + '/' + realm + '/' + fq + '/' + var + '/' + mip + '.*.' + exp + '.' + run + '.' + fq + '.' + realm + '.' + Realm + '.' + var + '*.xml'
+   if mip == 'cmip5': pin = '/work/' + mip + '/' + exp + '/' + realm + '/' + fq + '/' + var + '/' + mip + '.*.' + exp + '.*.' + fq + '.' + realm + '.' + Realm + '.' + var + '*.xml'
    lst = os.popen('ls ' + pin).readlines()
    mods = []
    for l in lst:
