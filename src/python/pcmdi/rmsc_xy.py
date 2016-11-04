70ee9b9c0838c6aadd2d7e56b574cd3dda52a7ae
Peter Gleckler <gleckler1@llnl.gov>
2016-11-04 10:10:50 -0700

    added centered rmsc

---
+++ src/python/pcmdi/rmsc_xy.py
@@ -0,0 +1,14 @@
+import genutil
+
+
+def compute(dm, do):
+    """ Computes centered rms"""
+    if dm is None and do is None:  # just want the doc
+        return {
+            "Name": "Spatial Root Mean Square",
+            "Abstract": "Compute Centered Spatial Root Mean Square",
+            "URI": "http://uvcdat.llnl.gov/documentation/" +
+            "utilities/utilities-2.html",
+            "Contact": "pcmdi-metrics@llnl.gov",
+        }
+    return float(genutil.statistics.rms(dm, do, axis='xy', centered = 1, weights='weighted'))
