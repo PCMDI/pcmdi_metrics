f096a5c027cf9eca6cc48da3335fa7e4bfa96166
Peter Gleckler <gleckler1@llnl.gov>
2017-01-31 09:59:27 -0800

    moved skill score calcs to fcn

--- src/python/devel/monsoon_wang/scripts/mpi_compute.py
+++ src/python/devel/monsoon_wang/scripts/mpi_compute.py
@@ -6,6 +6,7 @@ import cdutil
 import regrid2
 import json
 from pcmdi_metrics.pcmdi.pmp_parser import *
+from pcmdi_metrics.monsoon_wang import * 
 import pcmdi_metrics
 import collections
 
@@ -116,24 +117,7 @@ obsgrid = dobs_orig.getGrid()
 
 ########################################
 
-#  SEASONAL RANGE - USING ANNUAL CYCLE CLIMATOLGIES 0=Jan, 11=Dec
-def mpd(d):
- mjjas = (d[4] + d[5] + d[6] + d[7] + d[8])/5.
- ndjfm = (d[10] + d[11] + d[0] + d[1] + d[2])/5.
- ann = MV2.average(d,axis=0)
-
- annrange = MV2.subtract(mjjas,ndjfm)
- annrange0 = annrange*1.
-
- lats0 = annrange.getAxis(0)
-
- for l in range(len(lats0)):
-   if lats0[l] <0:
-    annrange[l,:] = -1*annrange[l,:]
-
- mpi = MV2.divide(annrange,ann)
-
- return annrange, mpi 
+# FCN TO COMPUTE GLOBAL ANNUAL RANGE AND MONSOON PRECIP INDEX 
 
 annrange_obs, mpi_obs = mpd(dobs_orig)
 
@@ -223,24 +207,8 @@ for mod in gmods:
 
    mt = MV2.where(MV2.greater(annrange_mod_dom,thr),1.,0.)
    ot = MV2.where(MV2.greater(annrange_obs_dom,thr),1.,0.)
- 
-   both = MV2.add(mt,ot) # HIT WILL MEAN 2, OTHERWISE 0 or 1
-   hitmap = MV2.where(MV2.equal(both,2.),1.,0.) 
-   hit = float(MV2.sum(hitmap))
 
-   mt1 = MV2.where(MV2.greater(annrange_mod_dom,thr),10.,0.)
-   both1 = MV2.add(mt1,ot) # 10 for MOD above THRESHOLD, OTHERWISE 0 
-   missmap = MV2.where(MV2.equal(both1,1.),1.,0.)
-   missed = float(MV2.sum(missmap))
-
-   ot1 = MV2.where(MV2.greater(annrange_obs_dom,thr),10.,0.)
-   both2 = MV2.add(mt,ot1) # 10 for OBS above THRESHOLD OTHERWISE 0 
-   falarmmap = MV2.where(MV2.equal(both2,1.),1.,0.)
-   falarm = float(MV2.sum(falarmmap))
-
-   print mod,' hit missed falarm ', hit,' ', missed,' ', falarm
-
-   score = hit/(hit+missed+falarm)
+   hit, missed,falarm,score,hitmap,missmap,falarmmap = mpi_skill_scores(mt,ot,thr)
 
    mpi_stats_dic[mod][dom] = {}
    mpi_stats_dic[mod][dom]['cor']=  format(cor,sig_digits)  
