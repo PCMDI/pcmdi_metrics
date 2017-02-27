02871d68b8a0f28e3b49dfff98c8eacda1b8aece
zshaheen <zshaheen93@gmail.com>
2017-02-27 11:14:23 -0800

    first try

--- conda-recipes/pcmdi_metrics/conda_upload.sh
+++ conda-recipes/pcmdi_metrics/conda_upload.sh
@@ -6,5 +6,14 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
+
+git clone git://github.com/UV-CDAT/conda-recipes
+cd conda-recipes
+
+python ./prep_for_build.py -v $VERSION
 conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
+
+python ./prep_for_build.py -v $VERSION -f nox
+conda build .
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-nox-$VERSION-py27_0.tar.bz2 --force
