0192fcadf4286a0236bddd4c105cebc084e32c11
zshaheen <zshaheen93@gmail.com>
2017-02-27 16:55:55 -0800

    please work 3

--- conda-recipes/pcmdi_metrics/conda_upload.sh
+++ conda-recipes/pcmdi_metrics/conda_upload.sh
@@ -12,7 +12,8 @@ git clone git://github.com/UV-CDAT/conda-recipes temp
 mkdir -p temp/conda-recipes/pcmdi_metrics
 cp conda-recipes/pcmdi_metrics/build.sh conda-recipes/pcmdi_metrics/meta.yaml.in temp/conda-recipes/pcmdi_metrics
 cd temp/conda-recipes
-
+echo ls temp/conda-recipes
+ls
 python ./prep_for_build.py -v $VERSION
 conda build pcmdi_metrics
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
