475124ecd870514eacf8007d78912dd91774504b
zshaheen <zshaheen93@gmail.com>
2017-02-28 09:42:08 -0800

    This will work, I promise.

--- conda-recipes/pcmdi_metrics/conda_upload.sh
+++ conda-recipes/pcmdi_metrics/conda_upload.sh
@@ -13,12 +13,11 @@ git clone git://github.com/UV-CDAT/conda-recipes temp/conda-recipes
 mkdir temp/conda-recipes/pcmdi_metrics
 cp conda-recipes/pcmdi_metrics/build.sh conda-recipes/pcmdi_metrics/meta.yaml.in temp/conda-recipes/pcmdi_metrics
 cd temp/conda-recipes
-echo ls temp/conda-recipes:
-ls
+
 python ./prep_for_build.py -v $VERSION
 conda build pcmdi_metrics
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
 
 python ./prep_for_build.py -v $VERSION -f nox
 conda build pcmdi_metrics
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-nox-$VERSION-py27_0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-nox-$VERSION-0.tar.bz2 --force
