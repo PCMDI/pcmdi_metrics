50c3f4e5087b74768d754791c5e1da0ea56fc45d
Charles Doutriaux <doutriaux1@llnl.gov>
2017-06-08 11:33:00 -0700

    nicely cleaned up test

--- src/python/devel/monsoon_wang/tests/test_monsoon_wang.py
+++ src/python/devel/monsoon_wang/tests/test_monsoon_wang.py
@@ -7,33 +7,64 @@ import numpy
 
 
 class MonsoonTest(unittest.TestCase):
-    def assertAllClose(self, a, b):
-        self.assertTrue(numpy.ma.allclose(a, b))
+    def checkAllClose(self, a, b):
+        if numpy.ma.allclose(a.filled(), b.filled()):
+            return True
+        else:
+            axes =a.getAxisList()
+            c = numpy.isclose(a.filled(),b.filled())
+            w = numpy.argwhere(c==0)
+            for d in w:
+                print "Error for:",
+                for i,indx in enumerate(d):
+                    print "%s, " % axes[i][indx],
+                print "(",
+                for indx in d:
+                    print "%i," % indx,
+                print "). Test value %.3f vs expected value: %.3f" % (a[tuple(d)],b[tuple(d)])
+            return False
+        return True
 
     def testMonsoonWang(self):
+
         cmd = 'mpi_compute.py --mp test/monsoon/pr_MODS_Amon_historical_r1i1p1_198001-200512-clim.nc --op test/monsoon/pr_GPCP_000001-000012_ac.nc --mns "[\'NorESM1-ME\',\'MRI-CGCM3\']" --outpd test_monsoon --outpj test_monsoon'
         p = subprocess.Popen(shlex.split(cmd))
-
         o, e = p.communicate()
 
         test_file = "test_monsoon/out.json"
         correct_file = "test/monsoon/mpi.json"
 
+        self.compareJsons(test_file,correct_file)
+        os.remove(test_file)
+
+
+    def compareJsons(self, test_file, correct_file):
+
+        print "Comparing:",test_file, correct_file
         T = pcmdi_metrics.io.base.JSONs([test_file], oneVariablePerFile=False)
         test = T()
 
-        C = pcmdi_metrics.io.base.JSONs([correct_file], oneVariablePerFile=False)
-        correct = C()
+        V = pcmdi_metrics.io.base.JSONs([correct_file], oneVariablePerFile=False)
+        valid = V()
 
-        self.assertEqual(test.shape, correct.shape)
+        self.assertEqual(test.shape, valid.shape)
 
-        self.assertAllClose(correct.filled(), test.filled())
 
         tax = test.getAxisList()
-        cax = correct.getAxisList()
+        cax = valid.getAxisList()
 
+        correct = True
         for i in range(len(tax)):
-            self.assertEqual(tax[i].id, cax[i].id)
+            if not tax[i].id == cax[i].id:
+                print "Axes index %i have different names, test is '%s' vs expected: '%s'" % (i,tax[i].id, cax[i].id)
+                correct = False
             for j in range(len(tax[i])):
-                self.assertEqual(tax[i][j], cax[i][j])
-        os.remove(test_file)
+                if not tax[i][j] == cax[i][j]:
+                    print "Axes %s, differ at index %i, test value: %s vs expectedi value: %s" % (tax[i].id,j,tax[i][j], cax[i][j])
+                    correct = False
+
+        if not self.checkAllClose(test, valid):
+            correct = False
+
+        if not correct:
+            raise Exception("jsons file %s differ from correct one: %s" % (test_file, correct_file))
