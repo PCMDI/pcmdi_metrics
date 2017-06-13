d5c4d5eec734a3d374c0a6273a522d0aa9c196e9
Charles Doutriaux <doutriaux1@llnl.gov>
2017-06-13 13:22:36 -0700

    tweaks

--- conda-recipes/pcmdi_metrics/conda_upload.sh
+++ conda-recipes/pcmdi_metrics/conda_upload.sh
@@ -13,17 +13,16 @@ else
     OS=osx-64
 fi
 
-mkdir ~/conda-bld
+mkdir conda-bld
 conda config --set anaconda_upload no
-export CONDA_BLD_PATH=${HOME}/conda-bld
+export CONDA_BLD_PATH=`pwd`
 export VERSION=`date +%Y.%m.%d`
 echo "Cloning recipes"
+cd conda-bld
 git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
 # uvcdat creates issues for build -c uvcdat confises package and channel
-rm -rf uvcdat
-pwd
-ln -s ../../conda-recipes/pcmdi_metrics pcmdi_metrics
+ln -s ../../conda-recipes/pcmdi_metrics .
 echo "Starting prep for build"
 python ./prep_for_build.py
 echo "starting conda build"
