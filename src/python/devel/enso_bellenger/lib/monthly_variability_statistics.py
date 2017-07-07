96a150f5f6bf83e88f02fd95b4338acbff9cc7cb
Ji-Woo Lee <lee1043@crunchy.llnl.gov>
2017-07-07 09:45:43 -0700

    Let seasonal mean use full seasons

--- src/python/devel/enso_bellenger/lib/monthly_variability_statistics.py
+++ src/python/devel/enso_bellenger/lib/monthly_variability_statistics.py
@@ -10,10 +10,10 @@ def interannual_variability_seasonal_std_mean_removed(d,season_string):
    d_area_avg = cdutil.averager(d,axis='xy')
    pre_defined_seasons = ['DJF', 'MAM', 'JJA', 'SON', 'YEAR']
    if season_string in pre_defined_seasons:
-     d_area_avg_anom=getattr(cdutil,season_string).departures(d_area_avg)
+     d_area_avg_anom=getattr(cdutil,season_string).departures(d_area_avg,criteriaarg=[1.0,None])
    else:
      CustomSeason = cdutil.times.Seasons(season_string)
-     d_area_avg_anom = CustomSeason.departures(d_area_avg)
+     d_area_avg_anom = CustomSeason.departures(d_area_avg,criteriaarg=[1.0,None])
    d_area_avg_anom_sd = genutil.statistics.std(d_area_avg_anom)
    return(float(d_area_avg_anom_sd))
 
