64c95a29d076d59f37864e21098160ab62f8b23c
Charles Doutriaux <doutriaux1@llnl.gov>
2017-07-27 07:35:27 -0700

    trying to set log just for module

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -1,6 +1,7 @@
 #  put here the impor calls to expose whatever we want to the user
 import logging
 LOG_LEVEL = logging.INFO
+logging.getLogger("pcmdi_metrics").setLevel(logging.WARNING)
 import io  # noqa
 import pcmdi  # noqa
 from version import __version__, __git_sha1__, __git_tag_describe__  # noqa
