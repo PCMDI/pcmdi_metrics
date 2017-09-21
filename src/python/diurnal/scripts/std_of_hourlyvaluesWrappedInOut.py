8bcb8165ca06175b0ae47b9d1e70ce883ab9536c
Charles Doutriaux <doutriaux1@llnl.gov>
2017-09-21 10:31:56 -0700

    4th script src/python/diurnal/scripts/std_of_hourlyvaluesWrappedInOut.py

--- src/python/diurnal/scripts/std_of_hourlyvaluesWrappedInOut.py
+++ src/python/diurnal/scripts/std_of_hourlyvaluesWrappedInOut.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
-# Globally average the variance of daily mean precipitation values from a given month spanning a given set of years,
-# then take the square root. This is the correct order of operations to get the fraction of total variance, as shown
-# by Covey and Gehne 2016 (https://e-reports-ext.llnl.gov/pdf/823104.pdf).
-
-# For Januarys, taking the square root before globally averaging would be close to the DJF results mapped in the
-# middle-right panel of Figure 2 in Trenberth, Zhang & Gehne (2107) "Intermittency in Precipitation," J. Hydromet.
-# This is done in ./plot_std_of_dailymeans.py.)
+# Globally average the day-to-day variance of CMIP5/AMIP composite-diurnal-cycle precipitation--"Var(Pi)" in Trenberth,
+# Zhang & Gehne 2107: "Intermittency in Precipitation," J. Hydrometeorology--from a given month spanning years 1999-2005.
+# Square the input standard deviations before averaging over the hours of the day and then area, and then finally take
+# the square root. This is the correct order of operations to get the fraction of total variance, as shown by Covey and
+# Gehne 2016 (https://e-reports-ext.llnl.gov/pdf/823104.pdf).
 
 # This has the PMP Parser "wrapped" around it, so it's executed with both input and output parameters specified in the
 # Unix command line. For example:
 
-# ---> python std_of_dailymeansWrappedInOut.py -i out -o jsons -m 7
+# ---> python std_of_hourlyvaluesWrappedInOut.py -m7 
 
-#       Charles Doutriaux September 2017
-#	Curt Covey (from ./old_std_of_dailymeansWrappedInOut.py)		            July 2017
+#	Curt Covey (from ./old_std_of_hourlyvaluesWrappedInOut.py)		        July 2017
+
+
+
+
 
 import cdms2, cdutil, os
 import numpy.ma
@@ -29,7 +30,7 @@ from pcmdi_metrics.diurnal.common import monthname_d, P, populateStringConstruct
 P.add_argument("-j", "--outnamejson",
                       type = str,
                       dest = 'outnamejson',
-                      default = 'std_of_dailymeans_%(month).json',
+                      default = 'std_of_hourlymeans_%(month).json',
                       help = "Output name for jsons")
 
 P.add_argument("--lat1",type=float,default=-49.875,help="First latitude")
@@ -38,7 +39,7 @@ P.add_argument("--lon1",type=float,default=0.125,help="First longitude")
 P.add_argument("--lon2",type=float,default=359.875,help="Last longitude")
 
 P.add_argument("-t","--filename_template",
-       default = "pr_%(model)_%(month)_%(firstyear)-%(lastyear)_std_of_dailymeans.nc")
+       default = "pr_%(model)_%(month)_%(firstyear)-%(lastyear)_diurnal_std.nc")
 P.add_argument("--model",default="*")
 
 args = P.parse_args(sys.argv[1:])
@@ -61,7 +62,7 @@ lonrange =  (args.lon1,args.lon2)
 # latrange = (-15.0,  -5.0)
 # lonrange = (285.0, 295.0)
 
-datanameID = 'dailySD' # Short ID name of output data
+datanameID = 'diurnalstd' # Short ID name of output data
 
 print 'Preparing to write output to JSON file ...'           
 if not os.path.exists(args.output_directory):
@@ -97,9 +98,10 @@ for fnameRoot in files:
     print '  Shape =', x.shape
     print 'Finding RMS area-average ...'
     x = x*x
+    x = cdms2.MV2.average(x, axis=0)
     x = cdutil.averager(x, axis = 'xy')
     x = numpy.ma.sqrt(x)
-    print 'For %8s in %s, average variance of daily values = (%5.2f %s)^2' % (model, monthname, x, units)
+    print 'For %8s in %s, average variance of hourly values = (%5.2f %s)^2' % (model, monthname, x, units)
     stats_dic[model] = float(x) # Converts singleton transient variable to plain floating-point number
     f.close()
 
