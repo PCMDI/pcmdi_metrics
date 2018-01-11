8e84d74c8f98c7f05fad3dac413bd9793743ce66
lee1043 <lee1043@llnl.gov>
2018-01-10 17:26:47 -0800

    clean up

--- src/python/devel/variability_mode/lib/write_nc_output.py
+++ src/python/devel/variability_mode/lib/write_nc_output.py
@@ -1,6 +1,5 @@
 def write_nc_output(output_file_name,
                     eofMap, pc, frac, slopeMap, interceptMap):
-
   import cdms2 as cdms
   fout = cdms.open(output_file_name+'.nc','w')
   fout.write(eofMap,id='eof')
