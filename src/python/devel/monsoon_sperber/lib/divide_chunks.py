63a37e85db9c4720f931817879f738063f2ba360
lee1043 <lee1043@llnl.gov>
2018-07-17 21:05:10 -0700

    remove redundant debug print

--- src/python/devel/monsoon_sperber/lib/divide_chunks.py
+++ src/python/devel/monsoon_sperber/lib/divide_chunks.py
@@ -8,7 +8,8 @@ def divide_chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i+n]
 
-
+""" Above code advanced considering leap year
+"""
 def divide_chunks_advanced(l, n, debug=False):
     # Double check first date should be Jan 1 (except for SH monsoon)
     month = l.getTime().asComponentTime()[0].month
@@ -19,15 +20,11 @@ def divide_chunks_advanced(l, n, debug=False):
 
     # Check number of days in given year
     nday = len(l)
-    #p = 0
 
     if nday in [365, 360]:
         # looping till length l
         for i in range(0, len(l), n):
             yield l[i:i+n]
-            #if debug: 
-            #    print(i, i+n-1, p, l[i:i+n])
-            #    p += 1
     elif nday == 366:
         # until leap year day detected
         for i in range(0, len(l), n):
@@ -44,22 +41,12 @@ def divide_chunks_advanced(l, n, debug=False):
             if leap_detect:
                 yield l[i:i+n+1]
                 tmp = i+n+1
-                #if debug:
-                #    #print('debug: tmp: ', tmp)
-                #    print(i, i+n, p, l[i:i+n+1], '*')
-                #    p += 1
                 break
             else:
                 yield l[i:i+n]
-                #if debug: 
-                #    print(i, i+n-1, p, l[i:i+n])
-                #    p += 1
         # after leap year day passed
         if leap_detect:
             for i in range(tmp, len(l), n):
                 yield l[i:i+n]
-                #if debug: 
-                #    print(i, i+n-1, p, l[i:i+n])
-                #    p += 1
     else:
         sys.exit('error: number of days in year is '+str(nday))
