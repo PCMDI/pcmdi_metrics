a4d5030364bcf1a6e24dcddeefff42932f17460f
lee1043 <lee1043@llnl.gov>
2018-07-17 22:00:49 -0700

    update model list as available

--- src/python/devel/monsoon_sperber/scripts/myParam.py
+++ src/python/devel/monsoon_sperber/scripts/myParam.py
@@ -26,17 +26,16 @@ oeyear = 1999
 modpath = '/work/cmip5-test/new/historical/atmos/day/pr/cmip5.%(model).%(exp).%(realization).day.atmos.day.%(variable).*.xml'
 modpath_lf = '/work/lee1043/ESGF/xmls/cmip5/fx/fx/sftlf/cmip5.%(model).fx.r0i0p0.fx.sftlf.xml'
 
-modnames = ['ACCESS1-0', 'ACCESS1-3', 'bcc-csm1-1', 'bcc-csm1-M', 'BNU-ESM',
-            'CanCM4', 'CanESM2', 'CCSM4', 'CESM1-BGC', 'CESM1-CAM5', 'CESM1-FASTCHEM',
-            'CMCC-CESM', 'CMCC-CM', 'CMCC-CMS', 'CNRM-CM5', 'CSIRO-Mk3-6-0',
-            'EC-EARTH', 'FGOALS-g2', 'FGOALS-s2',
-            'GFDL-CM3', 'GFDL-ESM2G', 'GFDL-ESM2M',
-            'GISS-E2-H', 'GISS-E2-R',
-            'HadGEM2-CC', 'HadGEM2-ES',
-            'inmcm4', 'IPSL-CM5A-LR', 'IPSL-CM5A-MR', 'IPSL-CM5B-LR',
-            'MIROC4h', 'MIROC5', 'MIROC-ESM-CHEM', 'MIROC-ESM',
-            'MPI-ESM-MR', 'MPI-ESM-P', 
-            'MRI-CGCM3', 'MRI-ESM1', 'NorESM1-M']
+modnames = ['ACCESS1-0', 'ACCESS1-3', 
+            'bcc-csm1-1', 'bcc-csm1-1-m', 'BNU-ESM', 
+            'CanCM4', 'CanESM2', 'CCSM4', 
+            'CESM1-BGC', 'CESM1-CAM5', 'CESM1-FASTCHEM', 
+            'CMCC-CESM', 'CMCC-CM', 'CMCC-CMS', 'CNRM-CM5', 'CSIRO-Mk3-6-0', 
+            'EC-EARTH', 'FGOALS-g2', 'GFDL-CM3', 'GFDL-ESM2G', 'GFDL-ESM2M', 
+            'GISS-E2-H', 'GISS-E2-R', 'HadGEM2-AO', 'HadGEM2-CC', 'HadGEM2-ES', 
+            'inmcm4', 'IPSL-CM5A-LR', 'IPSL-CM5A-MR', 'IPSL-CM5B-LR', 
+            'MIROC-ESM', 'MIROC-ESM-CHEM', 'MIROC4h', 'MIROC5', 
+            'MPI-ESM-MR', 'MPI-ESM-P', 'MRI-CGCM3', 'MRI-ESM1', 'NorESM1-M'] 
 
 modnames = ['ACCESS1-0']
 
