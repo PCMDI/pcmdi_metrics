a636a10a3bb5aa18587669d2884a997e8b8a95d1
Charles Doutriaux <doutriaux1@llnl.gov>
2018-07-20 13:08:06 -0700

    had not updated param file

--- examples/parameter_files/cmip_clims_param.py
+++ examples/parameter_files/cmip_clims_param.py
@@ -1,28 +1,20 @@
 import os
 import glob
 
-vars = ['pr','rlut', 'rsut','rsutcs','rlutcs','tas','prw','tauu','tauv','uas','vas','psl','hus','ta','ua','va', 'zg']
-#vars = ['rlut']
-var = ['rlut']
 realm = 'atm'
 MIP = 'cmip5'
 exp = 'historical'
 
-modpath = os.path.join('/work', 'cmip5', exp, realm, 'mo_new')
+modpath = os.path.join('/work', 'cmip5-test', exp, realm, 'mo')
 
+filename_template = "%(variable)/cmip5.%(model).%(experiment).r1i1p1.mo.atm.Amon.%(variable).ver-%(version).latestX.xml"
 
-filename_template = MIP + '.*' + '.' + exp + '.r1i1p1.mo.' + realm + '.Amon.' + '*' + '.ver-*.latestX.xml'
-
-glb = os.path.join(modpath,"rlut",filename_template)
-files = glob.glob(glb)
-
-file = files[:5]
-#var= vars[:3]
-climout = 'cmip5clims_newsystem_' + exp + '/'  
+var= ['rlut', 'pr']
+climout = './example_results'
 
 results_dir = climout
-
+model = ["BNU-ESM", "CNRM-CM5-2", "CNRM-CM5"]
 start = '1981'
 end = '2005-12-31'
-
-granularize = ["file", "var"]
+num_workers = 3
+granularize = ["var", "model"]
