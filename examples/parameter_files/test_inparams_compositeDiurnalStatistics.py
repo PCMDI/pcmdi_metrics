d7b4dad124754643f468fd94dadbe2ed5807a0ed
Peter Gleckler <gleckler1@crunchy.llnl.gov>
2018-09-24 11:25:48 -0700

    worked on with CD last week

--- examples/parameter_files/test_inparams_compositeDiurnalStatistics.py
+++ examples/parameter_files/test_inparams_compositeDiurnalStatistics.py
@@ -5,10 +5,10 @@
 # EXECUTION
 # > compositeDiurnalStatisticsWrapped.py -p test_inparams_compositeDiurnalStatistics.py
 
-mp = '/work/durack1/cmip5/historical/atm/3hr/pr/'
-template = "cmip5.%(model).historical.r1i1p1.3hr.atm.3hr.pr.ver-*.latestX.xml"
+modpath = '/work/durack1/cmip5/historical/atm/3hr/pr/'
+filename_template = "cmip5.%(model).historical.r1i1p1.3hr.atm.3hr.pr.%(version).latestX.xml"
 num_workers = 3
 model = 'GFDL-ESM2M'   #'*'   #'IPSL-CM5A-LR'
-FIRSTYEAR = '1999'
-LASTYEAR = '2000'
+firstyear = 1989
+lastyear = 1999 
 results_dir = './test_diurnal_results'
