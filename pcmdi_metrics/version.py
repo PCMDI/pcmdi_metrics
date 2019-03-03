944883ea16eba35c0c65d48c482bc4f30d27a650
Peter Gleckler <gleckler1@crunchy.llnl.gov>
2019-03-02 16:19:30 -0800

    version change

--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v1.2'
-__git_tag_describe__ = 'v1.2-43-g8c03362'
-__git_sha1__ = '8c03362cc62e0cb41036e891847dbdd2d1f9b151'
+__git_tag_describe__ = 'v1.2-40-ge4e2258'
+__git_sha1__ = 'e4e2258d063e0da11442c7dae5ec6ce688387f51'
