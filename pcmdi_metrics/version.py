0cfbeb3b1536bd0a78de54adda215ae85313f204
Charles Doutriaux <doutriaux1@llnl.gov>
2019-05-09 09:40:39 -0700

    Adding tests to fix #602

--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v1.2'
-__git_tag_describe__ = 'v1.2-57-g930f3ae'
-__git_sha1__ = '930f3aec5867b96e376ff3e29bb383008432398e'
+__git_tag_describe__ = 'v1.2-45-g6fef135'
+__git_sha1__ = '6fef1358acba0e4c5617143fbf2fe25ad4e0f406'
