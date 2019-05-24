d35d0da1ae874243637a9ba3f48886b0297260ff
Ji-Woo Lee <lee1043@grim.llnl.gov>
2019-05-24 16:31:24 -0700

    add inline description

--- pcmdi_metrics/monsoon_sperber/calc_metrics.py
+++ pcmdi_metrics/monsoon_sperber/calc_metrics.py
@@ -20,17 +20,23 @@ def sperber_metrics(d, region, debug=False):
     """ d: input, 1d array of cumulative pentad time series """
     # Convert accumulation to fractional accumulation; normalize by sum
     d_sum = d[-1]
+    # Normalize
     frac_accum = MV2.divide(d, d_sum)
+    # Stat 1: Onset
     onset_index = next(i for i, v in enumerate(frac_accum) if v >= 0.2)
+    # Stat 2: Decay
     if region == 'GoG':
         decay_threshold = 0.6
     else:
         decay_threshold = 0.8
     decay_index = next(i for i, v in enumerate(
         frac_accum) if v >= decay_threshold)
+    # Stat 3: Slope
     slope = (frac_accum[decay_index] - frac_accum[onset_index]) \
         / float(decay_index - onset_index)
+    # Stat 4: Duration
     duration = decay_index - onset_index + 1
+    # Calc done, return result as dic
     return {'frac_accum': frac_accum,
             'onset_index': onset_index,
             'decay_index': decay_index,
