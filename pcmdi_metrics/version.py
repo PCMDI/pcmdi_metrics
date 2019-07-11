04ad61a8bdf52af8af61698c64a9dd553d05ed45
Charles Doutriaux <doutriaux1@llnl.gov>
2019-07-11 07:26:14 -0700

    need a try/except because ndim shouldn't be reset

--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v1.2'
-__git_tag_describe__ = 'v1.2-58-gec93e57'
-__git_sha1__ = 'ec93e57248be0ad4b6c81d860e30714717f82c6b'
+__git_tag_describe__ = 'v1.2-59-g85056c0'
+__git_sha1__ = '85056c0355c3b396cd759aa7432482506698b891'
