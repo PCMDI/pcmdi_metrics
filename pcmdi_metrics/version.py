0ac0e391be7d3b55a185096a2ded8dbbfe65a146
Charles Doutriaux <doutriaux1@llnl.gov>
2019-07-24 05:52:03 -0700

    move yaml to end of file (#612)

--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v1.2'
-__git_tag_describe__ = 'v1.2-61-g10aeda6'
-__git_sha1__ = '10aeda62665ba4bc671da10a0ff8facc708487f5'
+__git_tag_describe__ = 'v1.2-65-g794ebe7'
+__git_sha1__ = '794ebe7b363f440b52f2d51fe46a33f18286fc33'
