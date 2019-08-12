f4d57ffacd15a6a2d35fd04f374b1ae858f3c30d
Charles Doutriaux <doutriaux1@llnl.gov>
2019-08-12 14:01:40 -0700

    finished follwoing diagram in demo

--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v1.2.1'
-__git_tag_describe__ = 'v1.2.1-5-ga246524'
-__git_sha1__ = 'a246524dc4002738e796c421db5d6170b0e23b1b'
+__git_tag_describe__ = 'v1.2.1-6-gaa44029'
+__git_sha1__ = 'aa4402987a9e2043b9a46b6e624e650de959faa6'
