53f779c7ea9ba6847302891a994995bc52894e3f
lee1043 <lee1043@llnl.gov>
2019-11-14 15:39:25 -0800

    flake8 and minor

--- pcmdi_metrics/variability_mode/scripts/post_process_merge_jsons.py
+++ pcmdi_metrics/variability_mode/scripts/post_process_merge_jsons.py
@@ -11,8 +11,10 @@ mode = 'NAM'
 eof = 'EOF1'
 mip = 'cmip5'
 exp = 'historical'
-case_id = 'v20191112'
+case_id = 'v20191114'
 obs = 'NOAA-CIRES_20CR'
+syear = 1900
+eyear = 2005
 # -------------------------------
 
 pmprdir = '/work/lee1043/imsi/result_test'
@@ -25,6 +27,7 @@ json_file_dir = os.path.join(
 json_file_template = 'var_mode_%(mode)_%(eof)_stat_%(mip)_%(exp)_mo_atm_%(model)_%(run)_%(syear)-%(eyear).json'
 json_file_template = StringConstructor(json_file_template)
 
+# Search for individual JSONs
 json_files = sorted(glob.glob(
     os.path.join(
         json_file_dir,
@@ -32,6 +35,7 @@ json_files = sorted(glob.glob(
 
 print('json_files:', json_files)
 
+# Load individual JSON and merge to one big dictionary
 for j, json_file in enumerate(json_files):
     print(j, json_file)
     f = open(json_file)
@@ -42,8 +46,9 @@ for j, json_file in enumerate(json_files):
         dict_merge(dict_final, dict_tmp)
     f.close()
 
+# Dump final dictionary to JSON
 final_json_filename = json_file_template(
-    mode=mode, eof=eof, mip=mip, exp=exp, model='all', run='all', syear='1900', eyear='2005')
+    mode=mode, eof=eof, mip=mip, exp=exp, model='allModels', run='allRuns', syear=str(syear), eyear=str(eyear))
 final_json_file = os.path.join(json_file_dir, final_json_filename)
 
 with open(final_json_file, 'w') as fp:
