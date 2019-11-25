c18de13f300adb606f3e4ded0701b2f5b66cfd43
lee1043 <lee1043@llnl.gov>
2019-11-25 13:16:05 -0800

    minor

--- pcmdi_metrics/variability_mode/scripts/run_all_modes_mips.sh
+++ pcmdi_metrics/variability_mode/scripts/run_all_modes_mips.sh
@@ -6,12 +6,12 @@ set -ax
 # - Grim: pmp_nightly_20190522
 
 ver=`date +"%Y%m%d"`
-mips='cmip5 cmip6'
+mips='cmip3 cmip5 cmip6'
 modes='NAM NAO PNA SAM PDO NPO NPGO'
 
 for mip in $mips; do
     for mode in $modes; do
-        python variability_modes_driver.py -p ../doc/myParam_${mode}_${mip}.py >& log.${mip}.${mode}.all.v${ver}.txt &
+        python variability_modes_driver.py -p ../doc/myParam_${mode}_${mip}.py --modnames all >& log.${mip}.${mode}.all.v${ver}.txt &
         #python variability_modes_driver.py -p ../doc/param_special_cases/myParam_${mode}_${mip}_obs2.py >& log.${mip}.${mode}.all.v${ver}.obs2.txt &
         disown
     done
