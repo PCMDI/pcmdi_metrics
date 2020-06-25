4988fad4afb877bf9b6dae13d683487fc08fc2a6
lee1043 <lee1043@llnl.gov>
2020-06-25 14:25:32 -0700

    update

--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v1.2.1'
-__git_tag_describe__ = 'v1.2.1-214-g0bbec22'
-__git_sha1__ = '0bbec223ac7252d14dd0c70e2c25937a22a35444'
+__git_tag_describe__ = 'v1.2.1-251-gff6e85b'
+__git_sha1__ = 'ff6e85b18d948d3802083fd521bd3bd1b0a9ce3c'
