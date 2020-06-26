4115f8d014d4f8a82dffe0ec5dcf836c4526c7e6
lee1043 <lee1043@llnl.gov>
2020-06-26 10:19:30 -0700

    merge jsons after parallel execution

--- pcmdi_metrics/mjo/lib/__init__.py
+++ pcmdi_metrics/mjo/lib/__init__.py
@@ -3,3 +3,4 @@ from .debug_chk_plot import debug_chk_plot  # noqa
 from .lib_mjo import interp2commonGrid, subSliceSegment, Remove_dailySeasonalCycle, get_daily_ano_segment, space_time_spectrum, taper, decorate_2d_array_axes, output_power_spectra, write_netcdf_output, calculate_ewr, unit_conversion, mjo_metrics_to_json  # noqa
 from .mjo_metric_calc import mjo_metric_ewr_calculation  # noqa
 from .plot_wavenumber_frequency_power import plot_power  # noqa
+from .dict_merge import dict_merge  # noqa
