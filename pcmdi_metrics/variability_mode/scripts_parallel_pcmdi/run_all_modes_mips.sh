74dae40066583e5894da2482f6c5650f8111eb1d
lee1043 <lee1043@llnl.gov>
2020-11-04 08:47:40 -0800

    update

--- pcmdi_metrics/variability_mode/scripts_parallel_pcmdi/run_all_modes_mips.sh
+++ pcmdi_metrics/variability_mode/scripts_parallel_pcmdi/run_all_modes_mips.sh
@@ -12,8 +12,8 @@ modes='NAM NAO PNA SAM PDO NPO NPGO'
 
 for mip in $mips; do
     for mode in $modes; do
-        python ../scriptsvariability_modes_driver.py -p ../doc/myParam_${mode}_${mip}.py --modnames all >& log.${mip}.${mode}.all.v${ver}.txt &
-        #python ../scriptsvariability_modes_driver.py -p ../doc/param_special_cases/myParam_${mode}_${mip}_obs2.py >& log.${mip}.${mode}.all.v${ver}.obs2.txt &
+        python ../drivervariability_modes_driver.py -p ../doc/myParam_${mode}_${mip}.py --modnames all >& log.${mip}.${mode}.all.v${ver}.txt &
+        #python ../drivervariability_modes_driver.py -p ../doc/param_special_cases/myParam_${mode}_${mip}_obs2.py >& log.${mip}.${mode}.all.v${ver}.obs2.txt &
         disown
     done
 done
