9222126779cfaea2d403ddb5c7e694e57a829782
Peter Gleckler <gleckler1@gates.llnl.gov>
2020-12-21 17:27:43 -0800

    still doing tests

--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v1.2.1'
-__git_tag_describe__ = 'v1.2.1-394-gcf14b23'
-__git_sha1__ = 'cf14b234f90ef548601ecb6431cca704f80c901f'
+__git_tag_describe__ = 'v1.2.1-402-ge0d97dd'
+__git_sha1__ = 'e0d97dd9d5e596adea0c09b158172592d5cf71b6'
