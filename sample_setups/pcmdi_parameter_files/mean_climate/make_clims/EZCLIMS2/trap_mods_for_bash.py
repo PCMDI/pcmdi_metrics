cf0e615b87ab82930483b3ee00f95a777de947ba
Peter Gleckler <gleckler1@gates.llnl.gov>
2021-01-05 11:23:10 -0800

    cleaning up

--- sample_setups/pcmdi_parameter_files/mean_climate/make_clims/EZCLIMS2/trap_mods_for_bash.py
+++ sample_setups/pcmdi_parameter_files/mean_climate/make_clims/EZCLIMS2/trap_mods_for_bash.py
@@ -2,26 +2,43 @@ import glob, os
 import datetime
 
 var = 'tas'
-verin = 'v20201226' 
-pin = '/p/user_pub/pmp/pmp_results/pmp_v1.1.2/additional_xmls/latest/' + verin + '/cmip6/historical/atmos/mon/' + var + '/'
+exp = 'historical'
+mip = 'cmip6'
+verin = 'v20201226'
+start = '1980-01'
+end = '2005-12'
+
+#################
+ 
+pin = '/p/user_pub/pmp/pmp_results/pmp_v1.1.2/additional_xmls/latest/' + verin + '/' + mip + '/' + exp + '/atmos/mon/' + var + '/'
 
 verout = datetime.datetime.now().strftime('v%Y%m%d')
 
 lst = glob.glob(pin + '*r1i1p1f1*.xml')
 
-pathout = '/p/user_pub/pmp/pmp_results/pmp_v1.1.2/diagnostic_results/CMIP_CLIMS/cmip6/historical/' + verout + '/' + var + '/' 
+pathout_base = '/p/user_pub/pmp/pmp_results/pmp_v1.1.2/diagnostic_results/CMIP_CLIMS/' + mip + '/' + exp + '/'   
 
 try:
- os.mkdir('/p/user_pub/pmp/pmp_results/pmp_v1.1.2/diagnostic_results/CMIP_CLIMS/cmip6/historical/' + verout)
+ os.mkdir(pathout_base + verout)
 except:
  pass
 try:
- os.mkdir('/p/user_pub/pmp/pmp_results/pmp_v1.1.2/diagnostic_results/CMIP_CLIMS/cmip6/historical/' + verout + '/' + var)
+ os.mkdir(pathout_base + verout + '/' + var)
 except:
  pass
 
-cmd0 = "python ./clim_calc_driver.py -p clim_calc_cmip_inparam.py --start 1981-01 --end 2005-12 --infile " 
+#pathout = '/p/user_pub/pmp/pmp_results/pmp_v1.1.2/diagnostic_results/CMIP_CLIMS/' + mip + '/' + exp + '/' + verout + '/' + var + '/'
+pathout = pathout_base + verout + '/' + var
 
+cmd0 = "python ./clim_calc_driver.py -p clim_calc_cmip_inparam.py --start " + start + " --end " + end + " --infile " 
+
+lst1 = []
 for l in lst:
-  print('nohup ' + cmd0 + l + " --outpath " + pathout + ' --var ' + var + ' &')
+  cmd = 'nohup ' + cmd0 + l + " --outpath " + pathout + ' --var ' + var + ' > log.' + mip + '.' + exp + '.' + var + '.' + verin + '.txt' + ' & \n'
+  print(cmd)
+  lst1.append(cmd)
+
+fout = open(var + '.' + mip + '.' + exp + '.' + verin + '.' + verout + '.bash',"w+")
+fout.writelines(lst1)
+fout.close()
   
--- sample_setups/pcmdi_parameter_files/mean_climate/make_clims/MIP_EXP_all.bash
+++
@@ -1,1 +0,0 @@
-pcmdi_compute_climatologies.py -p cmip_clims_param.py -model_id GFDL-CM4 
