d10231bdbaf018e51d5e0092de9fe15015d35a53
Peter Gleckler <gleckler1@gates.llnl.gov>
2021-01-08 15:45:31 -0800

    working on flake8

--- pcmdi_metrics/pcmdi/scripts/pcmdi_compute_climatologies.py
+++ pcmdi_metrics/pcmdi/scripts/pcmdi_compute_climatologies.py
@@ -1,15 +1,6 @@
-import glob, os,sys
-import cdms2, MV2
-import cdutil
-import argparse
+import cdms2 
 import datetime
-import cdms2
-import cdtime
 import pcmdi_metrics
-
-
-#from clim_module import clim_calc
-from pcmdi_metrics.pcmdi.pmp_parser import PMPParser
 
 ver = datetime.datetime.now().strftime('v%Y%m%d')
 
@@ -18,131 +9,142 @@ cdms2.setNetcdfDeflateFlag(0)
 cdms2.setNetcdfDeflateLevelFlag(0)
 
 ########
-def clim_calc(var, infile,outfile,outdir,outfilename,start,end):
-   import cdms2
-   import cdutil
-   import cdtime
-   import datetime
-   import os
 
-   ver = datetime.datetime.now().strftime('v%Y%m%d')
 
-   l = infile
-   tmp = l.split('/')
-   infilename = tmp[len(tmp)-1]
-   print('infilename is ', infilename)
+def clim_calc(var, infile, outfile, outdir, outfilename, start, end):
+	import cdms2
+    import cdutil
+    import cdtime
+    import datetime
+    import os
 
-   f = cdms2.open(l)
-   atts = f.listglobal()
-   outfd = outfile
+    ver = datetime.datetime.now().strftime('v%Y%m%d')
 
-### CONTROL OF OUTPUT DIRECTORY AND FILE
+    lf = infile
+    tmp = lf.split('/')
+    infilename = tmp[len(tmp)-1]
+    print('infilename is ', infilename)
 
-## outdir AND outfilename PROVIDED BY USER
-   if outdir is not None and outfilename is not None: outfd = outdir + outfilename
+    f = cdms2.open(lf)
+    atts = f.listglobal()
+    outfd = outfile
 
-## outdir PROVIDED BY USER, BUT filename IS TAKEN FROM infilename WITH CLIM MODIFICATIONS SUFFIX ADDED BELOW
-   if outdir is not None and outfilename is None: outfd = outdir + '/' + infilename
+# CONTROL OF OUTPUT DIRECTORY AND FILE
 
-   if outdir is None and outfilename is None:    # WORKING!!!
-        outfd = outfile
+# outdir AND outfilename PROVIDED BY USER
+    if outdir is not None and outfilename is not None: 
+    	outfd = outdir + outfilename
 
-   print('outfd is ', outfd)
-   print('outdir is ', outdir)
+# outdir PROVIDED BY USER, BUT filename IS TAKEN FROM infilename WITH CLIM MODIFICATIONS SUFFIX ADDED BELOW
+    if outdir is not None and outfilename is None: 
+    	outfd = outdir + '/' + infilename
 
-   seperate_clims = 'y'
+    if outdir is None and outfilename is None:    # WORKING!!!
+    	outfd = outfile
 
-### DEFAULT CLIM - BASED ON ENTIRE TIME SERIES
-   if start == end == None:
-    d = f(var)
-    t = d.getTime()
-    c = t.asComponentTime()
-    start_yr_str = str(c[0].year)
-    start_mo_str = str(c[0].month)
-    end_yr_str = str(c[len(c)-1].year)
-    end_mo_str = str(c[len(c)-1].month)
-    start_yr_int = int(c[0].year)
-    start_mo_int = int(c[0].month)
-    end_yr_int = int(c[len(c)-1].year)
-    end_mo_int = int(c[len(c)-1].month)
-### USER DEFINED PERIOD
-   else:
-    start_mo = int(start.split('-')[1])
-    start_yr = int(start.split('-')[0])
-    end_mo = int(end.split('-')[1])
-    end_yr = int(end.split('-')[0])
-    start_yr_str = str(start_yr)
-    start_mo_str = str(start_mo)
-    end_yr_str = str(end_yr)
-    end_mo_str = str(end_mo)
-    d = f(var,time=(cdtime.comptime(start_yr,start_mo),cdtime.comptime(end_yr,end_mo)))
-
-   print("start_yr_str is ", start_yr_str)
-
-   if start_mo_str not in ['11','12']: start_mo_str = '0' + start_mo_str
-   if end_mo_str not in ['11','12']: end_mo_str = '0' + end_mo_str
-
-########
-
-   d_ac =   cdutil.ANNUALCYCLE.climatology(d).astype('Float32')
-   d_djf =  cdutil.DJF.climatology(d)(squeeze=1).astype('Float32')
-   d_jja =  cdutil.JJA.climatology(d)(squeeze=1).astype('Float32')
-   d_son =  cdutil.SON.climatology(d)(squeeze=1).astype('Float32')
-   d_mam =  cdutil.MAM.climatology(d)(squeeze=1).astype('Float32')
-
-   for v in [d_ac,d_djf,d_jja,d_son,d_mam]:
-     v.id = var
-
-   for s in ['AC','DJF','MAM','JJA','SON']:
-
-    addf = '.' + start_yr_str + start_mo_str + '-' + end_yr_str + end_mo_str + '.' + s + '.' + ver + '.nc'
-#   if seperate_clims == 'y': 
     print('outfd is ', outfd)
-    out = outfd
-    out = out.replace('.xml',addf)
-    out = out.replace('.nc',addf)
-    print('out is ', out)
+    print('outdir is ', outdir)
 
-    if seperate_clims == 'n':  out = outfd.replace('climo.nc',s+'.nc')
-    if s == 'AC': do = d_ac
-    if s == 'DJF': do = d_djf
-    if s == 'MAM': do = d_mam
-    if s == 'JJA': do = d_jja
-    if s == 'SON': do = d_son
-    do.id = var
+    seperate_clims = 'y'
+	
+# DEFAULT CLIM - BASED ON ENTIRE TIME SERIES
+    if start == end is None:
+    	d = f(var)
+    	t = d.getTime()
+    	c = t.asComponentTime()
+    	start_yr_str = str(c[0].year)
+    	start_mo_str = str(c[0].month)
+    	end_yr_str = str(c[len(c)-1].year)
+    	end_mo_str = str(c[len(c)-1].month)
+
+# USER DEFINED PERIOD
+    else:
+     	start_mo = int(start.split('-')[1])
+     	start_yr = int(start.split('-')[0])
+     	end_mo = int(end.split('-')[1])
+     	end_yr = int(end.split('-')[0])
+     	start_yr_str = str(start_yr)
+     	start_mo_str = str(start_mo)
+     	end_yr_str = str(end_yr)
+     	end_mo_str = str(end_mo)
+
+    d = f(var, time=(cdtime.comptime(start_yr, start_mo), cdtime.comptime(end_yr, end_mo)))
+
+    print("start_yr_str is ", start_yr_str)
+
+    if start_mo_str not in ['11', '12']: 
+    	start_mo_str = '0' + start_mo_str
+    if end_mo_str not in ['11', '12']: 
+      	end_mo_str = '0' + end_mo_str
+
+    d_ac = cdutil.ANNUALCYCLE.climatology(d).astype('Float32')
+    d_djf = cdutil.DJF.climatology(d)(squeeze=1).astype('Float32')
+    d_jja = cdutil.JJA.climatology(d)(squeeze=1).astype('Float32')
+    d_son = cdutil.SON.climatology(d)(squeeze=1).astype('Float32')
+    d_mam = cdutil.MAM.climatology(d)(squeeze=1).astype('Float32')
+
+    for v in [d_ac, d_djf, d_jja, d_son, d_mam]:
+     	v.id = var
+
+    for s in ['AC', 'DJF', 'MAM', 'JJA', 'SON']:
+
+		addf = '.' + start_yr_str + start_mo_str + '-' + end_yr_str + end_mo_str + '.' + s + '.' + ver + '.nc'
+
+#   if seperate_clims == 'y':
+ 
+     	print('outfd is ', outfd)
+     	out = outfd
+     	out = out.replace('.xml', addf)
+     	out = out.replace('.nc', addf)
+     	print('out is ', out)
+
+     if seperate_clims == 'n':  
+     	out = outfd.replace('climo.nc', s+'.nc')
+     if s == 'AC': 
+     	do = d_ac
+     if s == 'DJF': 
+     	do = d_djf
+     if s == 'MAM': 
+     	do = d_mam
+     if s == 'JJA': 
+     	do = d_jja
+     if s == 'SON': 
+     	do = d_son
+     do.id = var
 
 ### MKDIRS AS NEEDED
-    lst = outfd.split('/')
-    s = '/'
-    for l in range(len(lst)):
-     d = s.join(lst[0:l])
-     try:
-      os.mkdir(d)
-     except:
-      pass
+     lst = outfd.split('/')
+     s = '/'
+     for ll in range(len(lst)):
+     	d = s.join(lst[0:ll])
+      	try:
+       		os.mkdir(d)
+      	except:
+       	pass
 
-    g = cdms2.open(out,'w+')
-    g.write(do)
-
-    for att in atts:
-     setattr(g,att,f.getglobal(att))
-    g.close()
-    print(do.shape,' ', d_ac.shape,' ',out)
-   f.close()
+     g = cdms2.open(out, 'w+')
+     g.write(do)
+ 
+     for att in atts:
+     	setattr(g,att,f.getglobal(att))
+     g.close()
+     	print(do.shape, ' ', d_ac.shape, ' ', out)
+    f.close()
 
 #######################################################################
 
 P = pcmdi_metrics.driver.pmp_parser.PMPMetricsParser()
 
+		
 P.add_argument(
         '--var',
         dest='var',
-        help='Defines var',
+        help='Defines var', 
         required=False)
 P.add_argument(
         '--infile',
         dest='infile',
-        help='Defines infile', 
+        help='Defines infile',  
         required=False)
 P.add_argument(
         '--outfile',
@@ -169,7 +171,7 @@ P.add_argument(
         dest='end',
         help='Defines end year and month',
         required=False)
-
+			
 args = P.get_parameter()
 
 infile = args.infile
@@ -180,9 +182,9 @@ var = args.var
 start = args.start
 end = args.end
 
-print('start and end are ', start,' ',end)
+print('start and end are ', start, ' ', end)
 print('var is ', var)
 
-clim_calc(var,infile,outfile,outpath,outfilename,start,end)
+clim_calc(var, infile, outfile, outpath, outfilename, start, end)
 #'''
 #'''
--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v1.2.1'
-__git_tag_describe__ = 'v1.2.1-394-gcf14b23'
-__git_sha1__ = 'cf14b234f90ef548601ecb6431cca704f80c901f'
+__git_tag_describe__ = 'v1.2.1-426-g2ab21f9'
+__git_sha1__ = '2ab21f9dcf623bd4f092ce624fc7770f76fd03e6'
