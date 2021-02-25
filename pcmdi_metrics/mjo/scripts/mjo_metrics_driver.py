dcf0942b9ac13c6947eb32809bb5c991b95de391
Ana Ordonez <ordonez4@llnl.gov>
2021-02-25 14:42:53 -0800

    test with example obs

--- pcmdi_metrics/mjo/scripts/mjo_metrics_driver.py
+++ pcmdi_metrics/mjo/scripts/mjo_metrics_driver.py
@@ -305,4 +305,4 @@ for model in models:
 # --- Model loop end
 
 if not debug:
-    sys.exit('done')
+    sys.exit(0)
--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v1.2.1'
-__git_tag_describe__ = 'v1.2.1-419-g2f42727'
-__git_sha1__ = '2f427277b28f92316391d2ac49ce926942e299f9'
+__git_tag_describe__ = 'v1.2.1-468-g3619002'
+__git_sha1__ = '3619002c7b02e8e0bef150ed210c9610a7f3d49d'
