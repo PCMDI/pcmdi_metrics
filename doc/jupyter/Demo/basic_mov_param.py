cc75de19009930b17ae68412614c4ff82745b919
Jiwoo Lee <lee1043@llnl.gov>
2021-03-02 09:57:15 -0800

    clean up

--- doc/jupyter/Demo/basic_mov_param.py
+++ doc/jupyter/Demo/basic_mov_param.py
@@ -17,8 +17,8 @@ ConvEOF = True  # Calculate conventioanl EOF for model
 CBF = True  # Calculate Common Basis Function (CBF) for model
 
 # MODEL SETTINGS
-modpath = '$INPUT_DIR$/...'
-modnames = ['ACCESS1-0', 'CanCM4']
+modpath = 'demo_data/CMIP5_demo_data/psl_Amon_%(model)_historical_r1i1p1_185001-200512.nc'
+modnames = ['ACCESS1-0']
 varModel = 'psl'
 ModUnitsAdjust = (True, 'divide', 100.0)  # Pa to hPa
 msyear = 1900
@@ -26,8 +26,8 @@ meyear = 2005
 eofn_mod = 1
 
 # OBSERVATIONS SETTINGS
-reference_data_path = '$INPUT_DIR$/PCMDIobs2_clims'
-reference_data_name = ''
+reference_data_path = 'demo_data/PCMDIobs2/atmos/mon/psl/20CR/gn/v20200707/psl_mon_20CR_BE_gn_v20200707_187101-201212.nc'
+reference_data_name = 'NOAA-20CR'
 varOBS = 'psl'
 ObsUnitsAdjust = (True, 'divide', 100.0)  # Pa to hPa; or (False, 0, 0)
 osyear = 1900
@@ -36,7 +36,7 @@ eofn_obs = 1
 
 # DIRECTORY WHERE TO PUT RESULTS
 results_dir = os.path.join(
-    '$OUTPUT_DIR$',
+    'demo_output',
     "%(case_id)")
 
 # OUTPUT OPTIONS    
