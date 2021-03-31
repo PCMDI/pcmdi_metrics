7bd4908e135b64c69ecb6cf8d6bdaa98323be1d6
Ana Ordonez <ordonez4@llnl.gov>
2021-03-31 09:19:52 -0700

    more work

--- cmec/pmp_param_generator.py
+++ cmec/pmp_param_generator.py
@@ -52,10 +52,42 @@ if pmp_config == "mean_climate":
     settings["test_data_path"] = model_dir
     settings["reference_data_path"] = obs_dir
     settings["metrics_output_path"] = wk_dir
+    # also hard code interpolated field output path to wk_dir
 
 # Universal setting for all metrics
 settings["cmec"] = True
 settings["results_dir"] = wk_dir
+
+if "test_data_set" not in settings:
+    # See what model folders exist in model directory
+    model_list = os.listdir(model_dir)
+    test_data_set = []
+    for model in model_list:
+        fpath = os.path.join(model_dir,model)
+        if os.path.isdir(fpath):
+            test_data_set.append(model)
+    # Derive filename template
+    if "filename_template" not in settings:
+        var_name = os.path.join(fpath,os.path.listdir(fpath)[0])
+        test_name = os.listdir(var_name)[0]
+    if "%(model)" not in filename_template:
+        test_name.replace(model,"%(model)")
+        filename_template = "%(model)/"+test_name
+
+# TODO: also check aliases
+if "vars" not in settings:
+    # build variable list from directories in model directory
+    varlist = os.listdir(model_dir)
+    var = []
+    for item in varlist:
+        if item != "fx" and os.path.isdir(os.path.join(model_dir,item)):
+            var.append(item)
+    settings["vars"] = var
+    if "filename_template" not in settings:
+        for varname in var:
+            if varname in filename_template:
+                filename_template.replace(varname,"%(variable)")
+
 
 for item in settings:
     val = settings[item]
