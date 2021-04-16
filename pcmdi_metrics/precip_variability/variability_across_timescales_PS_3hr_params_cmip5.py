9754ea54b44eff458f8031ccc3fd00ad68d48785
msahn <46369397+msahn@users.noreply.github.com>
2021-04-16 15:53:10 -0700

    Add files via upload

---
+++ pcmdi_metrics/precip_variability/variability_across_timescales_PS_3hr_params_cmip5.py
@@ -0,0 +1,12 @@
+mip = 'cmip5'
+exp = 'historical'
+mod = 'ACCESS1-0.r1i1p1'
+var = 'pr'
+frq = '3hr'
+ver = 'v20210123'
+modpath = '/p/user_pub/pmp/pmp_results/pmp_v1.1.2/additional_xmls/latest/'+ver+'/'+mip+'/'+exp+'/atmos/'+frq+'/'+var+'/'
+outdir = '/work/ahn6/pr/variability_across_timescales/power_spectrum/'+ver+'/data/'+mip+'/'
+prd = [1985, 2004] # analysis period
+fac = 86400 # factor to make unit of [mm/day]
+nperseg = 10*365*8 # length of segment in power spectra (~10yrs)
+
