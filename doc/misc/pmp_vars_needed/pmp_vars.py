54c39c0b8d28c005e1a4251bb494ec2a92dbbfa9
Jiwoo Lee <lee1043@llnl.gov>
2021-04-28 18:15:19 -0700

    clean up

--- doc/misc/pmp_vars_needed/pmp_vars.py
+++ doc/misc/pmp_vars_needed/pmp_vars.py
@@ -9,7 +9,7 @@ def tree():
 
 pmp_vars = tree()
 
-pmp_vars['variability_mode']['mon'] = ['ts','psl'] 
+pmp_vars['variability_mode']['mon'] = sorted(['ts','psl'])
 pmp_vars['variability_mode']['fx'] = ['sftlf']
 
 pmp_vars['monsoon_wang']['mon'] = ['pr']
@@ -24,17 +24,16 @@ pmp_vars['mjo']['fx'] = ['sftlf']
 pmp_vars['diurnal']['3hr'] = ['pr']
 pmp_vars['diurnal']['fx'] = ['sftlf']
 
-pmp_vars['enso']['mon'] = ['pr', 'psl', 'zos', 'ts', 'tauu','tauv',
-                           'hfls', 'hfss', 'rlds', 'rlus', 'rsds', 'rsus']
-pmp_vars['enso']['fx'] = ['sftlf', 'areacella']
+pmp_vars['enso']['mon'] = sorted(['pr', 'psl', 'zos', 'ts', 'tauu','tauv',
+                                  'hfls', 'hfss', 'rlds', 'rlus', 'rsds', 'rsus'])
+pmp_vars['enso']['fx'] = sorted(['sftlf', 'areacella'])
 
-pmp_vars['mean_climate']['mon'] = ['ta', 'ua', 'va', 'zg', 'hur', 'hus',
-                                   'pr', 'prw', 'psl', 'sfcWind', 'tas',
-                                   'huss', 'hurs', 'tauu', 'tauv', 'rlut',
-                                   'rsdt', 'rsut', 'rsutcs', 'rlutcs', 'rltcre',
-                                   'rstcre', 'rsds', 'rlds', 'rlus', 'rsus']
+pmp_vars['mean_climate']['mon'] = sorted(['ta', 'ua', 'va', 'zg', 'hur', 'hus',
+                                          'pr', 'prw', 'psl', 'sfcWind', 'tas',
+                                          'huss', 'hurs', 'tauu', 'tauv', 'rlut',
+                                          'rsdt', 'rsut', 'rsutcs', 'rlutcs', 'rltcre',
+                                          'rstcre', 'rsds', 'rlds', 'rlus', 'rsus'])
 pmp_vars['mean_climate']['fx'] = ['sftlf']
 
 out_file = open("pmp_vars_needed.json", "w")
-
 json.dump(pmp_vars, out_file, indent=4, separators=(',', ':'), sort_keys=True)
