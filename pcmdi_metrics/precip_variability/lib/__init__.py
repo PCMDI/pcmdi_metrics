86779f25a8c70228e3cf365c0ce3f5c4075506fb
Min-Seop Ahn <ahn6@llnl.gov>
2021-05-03 19:02:44 -0700

    format modification

--- pcmdi_metrics/precip_variability/lib/__init__.py
+++ pcmdi_metrics/precip_variability/lib/__init__.py
@@ -1,10 +1,2 @@
 from .argparse_functions import AddParserArgument  # noqa
-from .lib_variability_across_timescales import (
-    Regrid2deg,
-    ClimAnom,
-    Powerspectrum,
-    lag1_autocorrelation,
-    rednoise,
-    RedNoiseSignificanceLevel,
-    StandardDeviation,
-)  # noqa
+from .lib_variability_across_timescales import (Regrid2deg, ClimAnom, Powerspectrum, lag1_autocorrelation, rednoise, RedNoiseSignificanceLevel, StandardDeviation)  # noqa
