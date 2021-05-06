90db1b364bb37a6c469b219a1cb742e62233ff36
Peter Gleckler <gleckler1@gates.llnl.gov>
2021-05-06 10:16:56 -0700

    ran local tests and removed

--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v1.2.1'
-__git_tag_describe__ = 'v1.2.1-612-g6456beb'
-__git_sha1__ = '6456beb877f95286aaf2609eb5c07d064cba83f9'
+__git_tag_describe__ = 'v1.2.1-641-g1bf43b7'
+__git_sha1__ = '1bf43b757ca245c41f7c18668875e5b0e31559da'
