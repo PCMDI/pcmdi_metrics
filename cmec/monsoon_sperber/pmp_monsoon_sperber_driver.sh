3ac35208e3dd69c4a35d65c80f2e59ea47e9ad17
Ana Ordonez <ordonez4@llnl.gov>
2021-06-24 10:13:29 -0700

    add cmec support

--- cmec/monsoon_sperber/pmp_monsoon_sperber_driver.sh
+++ cmec/monsoon_sperber/pmp_monsoon_sperber_driver.sh
@@ -7,11 +7,12 @@ cd $CMEC_WK_DIR
 
 tmp_param=$CMEC_WK_DIR/monsoon_sperber_param.py
 
-python $CMEC_CODE_DIR/scripts/pmp_param_generator.py \
+python $CMEC_CODE_DIR/../scripts/pmp_param_generator.py \
 $CMEC_CONFIG_DIR/cmec.json $tmp_param "monsoon_sperber"
 
 if [[ $? = 0 ]]; then
     driver_monsoon_sperber.py -p $tmp_param
+    python $CMEC_CODE_DIR/monsoon_sperber_output.py
 else
     echo "Failure in PMP/monsoon_sperber parameter file generation"
 fi
