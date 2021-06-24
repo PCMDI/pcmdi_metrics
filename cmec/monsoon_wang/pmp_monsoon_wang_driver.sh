359136a2431c575627c87d29aaf22bb53edf8c22
Ana Ordonez <ordonez4@llnl.gov>
2021-06-24 10:13:41 -0700

    output.json

--- cmec/monsoon_wang/pmp_monsoon_wang_driver.sh
+++ cmec/monsoon_wang/pmp_monsoon_wang_driver.sh
@@ -7,11 +7,14 @@ cd $CMEC_WK_DIR
 
 tmp_param=$CMEC_WK_DIR/monsoon_wang_param.py
 
-python $CMEC_CODE_DIR/scripts/pmp_param_generator.py \
+python $CMEC_CODE_DIR/../scripts/pmp_param_generator.py \
 $CMEC_CONFIG_DIR/cmec.json $tmp_param "monsoon_wang"
 
 if [[ $? = 0 ]]; then
     mpindex_compute.py -p $tmp_param
+
+    # write output.json
+    python $CMEC_CODE_DIR/monsoon_wang_output.py
 else
     echo "Failure in PMP/monsoon_wang parameter file generation"
 fi
