3331d9e03769e17c74aadaad066a671ed3fbeeae
Min-Seop Ahn <ahn6@llnl.gov>
2021-08-04 18:13:20 -0700

    init unevenness

--- pcmdi_metrics/precip_distribution/unevenness/README.md
+++
@@ -1,2 +0,0 @@
-This branch is preparing the "unevenness" software developed by Angie Pendergrass (https://github.com/apendergrass/unevenprecip) for use in the PMP.  The primary change involves generalizing how the code is operated, using the standard python argparse module and using cdms instead of xarray.
-
---
+++ pcmdi_metrics/precip_distribution/unevenness/scripts_pcmdi/run_obs.bash
@@ -0,0 +1,7 @@
+nohup python -u ../dist_unevenness_driver.py -p ../param/dist_unevenness_params_TRMM.py  > ./log/log_TRMM_90x45 &
+#nohup python -u ../dist_unevenness_driver.py -p ../param/dist_unevenness_params_TRMM.py  > ./log/log_TRMM_180x90 &
+#nohup python -u ../dist_unevenness_driver.py -p ../param/dist_unevenness_params_TRMM.py  > ./log/log_TRMM_360x180 &
+#nohup python -u ../dist_unevenness_driver.py -p ../param/dist_unevenness_params_TRMM.py  > ./log/log_TRMM_720x360 &
+
+#nohup python -u ../dist_unevenness_driver.py -p ../param/dist_unevenness_params_CMORPH.py  > ./log/log_CMORPH_180x90 &
+
