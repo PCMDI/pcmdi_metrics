f7a830126757392f481325c387bfcc136f4e05fb
Peter Gleckler <gleckler1@gates.llnl.gov>
2021-08-06 12:33:40 -0700

    jsons added

--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v2.0'
-__git_tag_describe__ = 'v2.0-67-g8c4d666'
-__git_sha1__ = '8c4d6669c468e6d4321094067c68f30c34feed10'
+__git_tag_describe__ = 'v2.0-80-g6e93a65'
+__git_sha1__ = '6e93a6513f2f981765320757a1ec9e3beb3257e8'
