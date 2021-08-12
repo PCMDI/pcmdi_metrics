be4f6824fb2f6b0fb3cffd281a30e8b62d0539e9
Min-Seop Ahn <ahn6@llnl.gov>
2021-08-11 22:21:50 -0700

    mod run_obs

--- pcmdi_metrics/precip_distribution/frequency_amount_peak/scripts_pcmdi/run_obs.bash
+++ pcmdi_metrics/precip_distribution/frequency_amount_peak/scripts_pcmdi/run_obs.bash
@@ -1,5 +1,5 @@
-nohup python -u ../dist_freq_amount_peak_width_driver.py -p ../param/dist_freq_amount_peak_width_params_TRMM.py  > ./log/log_TRMM_90x45 &
-#nohup python -u ../dist_freq_amount_peak_width_driver.py -p ../param/dist_freq_amount_peak_width_params_TRMM.py  > ./log/log_TRMM &
+#nohup python -u ../dist_freq_amount_peak_width_driver.py -p ../param/dist_freq_amount_peak_width_params_TRMM.py  > ./log/log_TRMM_90x45 &
+nohup python -u ../dist_freq_amount_peak_width_driver.py -p ../param/dist_freq_amount_peak_width_params_TRMM.py  > ./log/log_TRMM &
 #nohup python -u ../dist_freq_amount_peak_width_driver.py -p ../param/dist_freq_amount_peak_width_params_TRMM.py  > ./log/log_TRMM_360x180 &
 #nohup python -u ../dist_freq_amount_peak_width_driver.py -p ../param/dist_freq_amount_peak_width_params_TRMM.py  > ./log/log_TRMM_720x360 &
 
