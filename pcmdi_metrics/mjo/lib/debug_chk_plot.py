dabb68f014adfecca422e693eaa626bb9e16bbcc
Jiwoo Lee <lee1043@llnl.gov>
2021-11-06 09:30:10 -0700

    vcs removed from mjo debug check plot

--- pcmdi_metrics/mjo/lib/debug_chk_plot.py
+++ pcmdi_metrics/mjo/lib/debug_chk_plot.py
@@ -1,16 +1,14 @@
 import os
 
 import cartopy.crs as ccrs
-from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 import matplotlib.pyplot as plt
+from cartopy.mpl.ticker import LatitudeFormatter, LongitudeFormatter
 
 
-def debug_chk_plot(
-        d_seg_x_ano, Power, OEE, segment_year,
-        daSeaCyc, segment_ano_year):
+def debug_chk_plot(d_seg_x_ano, Power, OEE, segment_year, daSeaCyc, segment_ano_year):
 
-    if not os.path.exists('debug'):
-        os.makedirs('debug')
+    if not os.path.exists("debug"):
+        os.makedirs("debug")
 
     """
     x = vcs.init()
@@ -24,35 +22,33 @@ def debug_chk_plot(
     x.clear()
     x.plot(OEE)
     x.png('debug/OEE.png')
-
-    x.clear()
-    x.plot(segment_year)
-    x.png('debug/segment.png')
     """
 
-    print('type(daSeaCyc)', type(daSeaCyc))
-    print('daSeaCyc.shape:', daSeaCyc.shape)
-    print(daSeaCyc.getAxis(0))
-    print(daSeaCyc.getAxis(1))
-    print(daSeaCyc.getAxis(2))
-    plot_map(daSeaCyc[0], 'debug/daSeaCyc.png')
+    print("type(segment_year)", type(segment_year))
+    print("segment_year.shape:", segment_year.shape)
+    print(segment_year.getAxis(0))
+    print(segment_year.getAxis(1))
+    print(segment_year.getAxis(2))
+    plot_map(segment_year[0], "debug/segment.png")
 
-    print('type(segment_ano_year)', type(segment_ano_year))
-    print('segment_ano_year.shape:', segment_ano_year.shape)
+    print("type(daSeaCyc)", type(daSeaCyc))
+    print("daSeaCyc.shape:", daSeaCyc.shape)
+    plot_map(daSeaCyc[0], "debug/daSeaCyc.png")
+
+    print("type(segment_ano_year)", type(segment_ano_year))
+    print("segment_ano_year.shape:", segment_ano_year.shape)
     print(segment_ano_year.getAxis(0))
     print(segment_ano_year.getAxis(1))
     print(segment_ano_year.getAxis(2))
-    plot_map(segment_ano_year[0], 'debug/segment_ano.png')
+    plot_map(segment_ano_year[0], "debug/segment_ano.png")
 
 
 def plot_map(data, filename):
-    fig = plt.figure()
+    fig = plt.figure(figsize=(10, 6))
     lons = data.getLongitude()
     lats = data.getLatitude()
     ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=180))
-    ax.contourf(lons, lats, data,
-                transform=ccrs.PlateCarree(),
-                cmap='viridis')
+    im = ax.contourf(lons, lats, data, transform=ccrs.PlateCarree(), cmap="viridis")
     ax.coastlines()
     ax.set_global()
     ax.set_xticks([0, 60, 120, 180, 240, 300, 360], crs=ccrs.PlateCarree())
@@ -61,4 +57,6 @@ def plot_map(data, filename):
     lat_formatter = LatitudeFormatter()
     ax.xaxis.set_major_formatter(lon_formatter)
     ax.yaxis.set_major_formatter(lat_formatter)
+    plt.colorbar(im)
+    ax.set_aspect('auto', adjustable=None)
     fig.savefig(filename)
