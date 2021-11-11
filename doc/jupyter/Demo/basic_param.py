6dfcadc361ea7908774a0f4ef5c22c85dad1be8c
Jiwoo Lee <lee1043@llnl.gov>
2021-11-10 17:18:26 -0800

    clean up

--- doc/jupyter/Demo/basic_param.py
+++ doc/jupyter/Demo/basic_param.py
@@ -8,29 +8,29 @@ import os
 # RUN IDENTIFICATION
 # DEFINES A SUBDIRECTORY TO METRICS OUTPUT RESULTS SO MULTIPLE CASES CAN
 # BE COMPARED
-case_id = "basicTest"
+case_id = 'basicTest'
 
 # LIST OF MODEL VERSIONS TO BE TESTED - WHICH ARE EXPECTED TO BE PART OF
 # CLIMATOLOGY FILENAME
-test_data_set = ["ACCESS1-0", "CanCM4"]
+test_data_set = ['ACCESS1-0', 'CanCM4']
 
 
 # VARIABLES TO USE
-vars = ["rlut"]
+vars = ['rlut']
 
 
 # Observations to use at the moment "default" or "alternate"
-reference_data_set = ["all"]
-# ext = '.nc'
+reference_data_set = ['all']
+#ext = '.nc'
 
 # INTERPOLATION OPTIONS
-target_grid = "2.5x2.5"  # OPTIONS: '2.5x2.5' or an actual cdms2 grid object
-regrid_tool = "regrid2"  # 'regrid2' # OPTIONS: 'regrid2','esmf'
+target_grid = '2.5x2.5'  # OPTIONS: '2.5x2.5' or an actual cdms2 grid object
+regrid_tool = 'regrid2'  # 'regrid2' # OPTIONS: 'regrid2','esmf'
 # OPTIONS: 'linear','conservative', only if tool is esmf
-regrid_method = "linear"
-regrid_tool_ocn = "esmf"  # OPTIONS: "regrid2","esmf"
+regrid_method = 'linear'
+regrid_tool_ocn = 'esmf'    # OPTIONS: "regrid2","esmf"
 # OPTIONS: 'linear','conservative', only if tool is esmf
-regrid_method_ocn = "linear"
+regrid_method_ocn = 'linear'
 
 # Templates for climatology files
 # %(param) will subsitute param with values in this file
@@ -38,16 +38,18 @@ filename_template = "cmip5.historical.%(model_version).r1i1p1.mon.%(variable).19
 
 # filename template for landsea masks ('sftlf')
 sftlf_filename_template = "sftlf_%(model_version).nc"
-generate_sftlf = True  # if land surface type mask cannot be found, generate one
+generate_sftlf = True # if land surface type mask cannot be found, generate one
 
 # Region
 regions = {"rlut": ["Global"]}
 
 # ROOT PATH FOR MODELS CLIMATOLOGIES
-test_data_path = "demo_data/CMIP5_demo_clims/"
+test_data_path = 'demo_data/CMIP5_demo_clims/'
 # ROOT PATH FOR OBSERVATIONS
 # Note that atm/mo/%(variable)/ac will be added to this
-reference_data_path = "demo_data/PCMDIobs2_clims"
+reference_data_path = 'demo_data/obs4MIPs_PCMDI_clims'
 
 # DIRECTORY WHERE TO PUT RESULTS
-metrics_output_path = os.path.join("demo_output", "%(case_id)")
+metrics_output_path = os.path.join(
+    'demo_output',
+    "%(case_id)")
