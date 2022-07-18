890f34dc7117eadbe9a7bfa952cf3602034eb68d
Min-Seop Ahn <ahn6@llnl.gov>
2022-07-18 14:16:35 -0700

    remove old files

--- pcmdi_metrics/precip_distribution_old/frequency_amount_peak/lib/__init__.py
+++
@@ -1,2 +0,0 @@
-from .argparse_functions import AddParserArgument  # noqa
-from .lib_dist_freq_amount_peak_width import (Regrid, getDailyCalendarMonth, CalcBinStructure, MakeDists, CalcRainMetrics, AvgDomain)  # noqa
--- pcmdi_metrics/precip_distribution_old/frequency_amount_peak/scripts_pcmdi/run_parallel.wait.bash
+++
@@ -1,6 +0,0 @@
-#nohup ./run_cmip5.bash  > ./log/log_parallel.wait_cmip5 &
-#nohup ./run_cmip6.bash  > ./log/log_parallel.wait_cmip6 &
-
-#nohup python -u parallel_driver_cmip5.py  > ./log/log_parallel.wait_cmip5 &
-#wait
-nohup python -u parallel_driver_cmip6.py  > ./log/log_parallel.wait_cmip6 &
