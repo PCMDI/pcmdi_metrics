85c6e961aaad39bcb8623f0584c91d77d9ba8a41
lee1043 <lee1043@llnl.gov>
2022-09-21 18:31:11 -0700

    testing ...

--- pcmdi_metrics/cloud_feedback/param/my_param.py
+++ pcmdi_metrics/cloud_feedback/param/my_param.py
@@ -8,7 +8,8 @@ input_files_json = './param/input_files.json'
 # Flag to compute ECS
 # True: compute ECS using abrupt-4xCO2 run
 # False: do not compute, instead rely on ECS value present in the json file (if it exists)
-get_ecs = True
+#get_ecs = True
+get_ecs = False
 
 # Output directory path (directory will be generated if it does not exist yet.)
 xml_path = "./xmls/"
