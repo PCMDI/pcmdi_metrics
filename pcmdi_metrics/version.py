dc67c92d9cfa204c33b38e455ef9ca9fe20628c6
lee1043 <lee1043@llnl.gov>
2022-10-26 22:44:20 -0700

    clean up

--- pcmdi_metrics/version.py
+++ pcmdi_metrics/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v2.3.1'
-__git_tag_describe__ = 'v2.3.1-37-gff043f6'
-__git_sha1__ = 'ff043f64fdaf0347bd276d11926735ff3b3aaa3e'
+__git_tag_describe__ = 'v2.3.1-38-gca9d624'
+__git_sha1__ = 'ca9d6247de6ea8abd8a95b268b30850c8aa0ef6d'
