9c39fc42ad0f8f46b59f350204eb13e1f942bc18
lee1043 <lee1043@llnl.gov>
2023-02-01 21:14:29 -0800

    add flexibility for using longitude range to subset, either (-180,180) or (0, 360)

--- pcmdi_metrics/io/default_regions_define.py
+++ pcmdi_metrics/io/default_regions_define.py
@@ -1,3 +1,6 @@
+import xcdat as xc
+
+
 def load_regions_specs():
 
     regions_specs = {
@@ -61,7 +64,7 @@ def load_regions_specs():
     return regions_specs
 
 
-def region_subset(d, regions_specs, region=None):
+def region_subset(ds, regions_specs, region=None):
     """
     d: xarray.Dataset
     regions_specs: dict
@@ -75,17 +78,35 @@ def region_subset(d, regions_specs, region=None):
             if 'latitude' in list(regions_specs[region]['domain'].keys()):
                 lat0 = regions_specs[region]['domain']['latitude'][0]
                 lat1 = regions_specs[region]['domain']['latitude'][1]
-                if 'latitude' in (d.coords.dims):
-                    d = d.sel(latitude=slice(lat0, lat1))
-                elif 'lat' in (d.coords.dims):
-                    d = d.sel(lat=slice(lat0, lat1))
+                # proceed subset
+                if 'latitude' in (ds.coords.dims):
+                    ds = ds.sel(latitude=slice(lat0, lat1))
+                elif 'lat' in (ds.coords.dims):
+                    ds = ds.sel(lat=slice(lat0, lat1))
 
             if 'longitude' in list(regions_specs[region]['domain'].keys()):
                 lon0 = regions_specs[region]['domain']['longitude'][0]
                 lon1 = regions_specs[region]['domain']['longitude'][1]
-                if 'longitude' in (d.coords.dims):
-                    d = d.sel(longitude=slice(lon0, lon1))
-                elif 'lon' in (d.coords.dims):
-                    d = d.sel(lon=slice(lon0, lon1))
+                
+                # check original dataset longitude range
+                if 'longitude' in (ds.coords.dims):
+                    lon_min = ds.longitude.min()
+                    lon_max = ds.longitude.max()
+                elif 'lon' in (ds.coords.dims):
+                    lon_min = ds.lon.min()
+                    lon_max = ds.lon.max()
 
-    return d
+                # longitude range swap if needed
+                if min(lon0, lon1) < 0:  # when subset region lon is defined in (-180, 180) range
+                    if min(lon_min, lon_max) < 0:  # if original data lon range is (-180, 180) no treatment needed
+                        pass
+                    else:  # if original data lon range is (0, 360), convert swap lon
+                        ds = xc.swap_lon_axis(ds, to=(-180, 180)) 
+
+                # proceed subset
+                if 'longitude' in (ds.coords.dims):
+                    ds = ds.sel(longitude=slice(lon0, lon1))
+                elif 'lon' in (ds.coords.dims):
+                    ds = ds.sel(lon=slice(lon0, lon1))
+
+    return ds
