d082892ab2f165ad5f31a19fe78e585fea6d68a8
Tom Vo <tomvothecoder@gmail.com>
2023-02-03 10:39:07 -0800

    Replace versioning scheme with standard convention -This fixes the issue with `setuptools>=6.6.0` enforcing PEP440

--- setup.py
+++ setup.py
@@ -13,20 +13,7 @@ if "--enable-devel" in sys.argv:
 else:
     install_dev = False
 
-Version = "2.0"
-p = subprocess.Popen(
-    ("git", "describe", "--tags"),
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-)
-try:
-    descr = p.stdout.readlines()[0].strip().decode("utf-8")
-    Version = "-".join(descr.split("-")[:-2])
-    if Version == "":
-        Version = descr
-except Exception:
-    descr = Version
+release_version = "2.5.1"
 
 p = subprocess.Popen(
     ("git", "log", "-n1", "--pretty=short"),
@@ -38,9 +25,10 @@ try:
     commit = p.stdout.readlines()[0].split()[1].decode("utf-8")
 except Exception:
     commit = ""
+
 f = open("pcmdi_metrics/version.py", "w")
-print("__version__ = '%s'" % Version, file=f)
-print("__git_tag_describe__ = '%s'" % descr, file=f)
+print("__version__ = '%s'" % release_version, file=f)
+print("__git_tag_describe__ = '%s'" % release_version, file=f)
 print("__git_sha1__ = '%s'" % commit, file=f)
 f.close()
 
@@ -143,7 +131,7 @@ if install_dev:
 
 setup(
     name="pcmdi_metrics",
-    version=descr,
+    version=release_version,
     author="PCMDI",
     description="model metrics tools",
     url="http://github.com/PCMDI/pcmdi_metrics",
