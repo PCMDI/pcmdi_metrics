4ac5f9f047869bc3eb1e58d0935f022f1aee9367
Jiwoo Lee <lee1043@llnl.gov>
2023-04-06 17:19:06 -0700

    update version to future next release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if "--enable-devel" in sys.argv:
 else:
     install_dev = False
 
-release_version = "3.0.2"
+release_version = "3.0.3"
 
 p = subprocess.Popen(
     ("git", "describe", "--tags"),
