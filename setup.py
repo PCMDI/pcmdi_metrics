d1ccd3619841a3b90f1881e17844dbf5fb9c8409
Jiwoo Lee <lee1043@llnl.gov>
2023-04-06 17:16:14 -0700

    block version.py generation

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ p = subprocess.Popen(
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE,
 )
+
+"""
 try:
     descr = p.stdout.readlines()[0].strip().decode("utf-8")
     Version = "-".join(descr.split("-")[:-2])
@@ -44,6 +46,7 @@ print("__version__ = '%s'" % Version, file=f)
 print("__git_tag_describe__ = '%s'" % descr, file=f)
 print("__git_sha1__ = '%s'" % commit, file=f)
 f.close()
+"""
 
 # Generate and install default arguments
 p = subprocess.Popen(["python", "setup_default_args.py"], cwd="share")
