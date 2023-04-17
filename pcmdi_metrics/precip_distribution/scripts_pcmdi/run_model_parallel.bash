f5f82c609b73655ab7b622a9353e7044f961eaac
lee1043 <lee1043@llnl.gov>
2023-04-17 13:21:49 -0700

    let processes independent to terminal

---
+++ pcmdi_metrics/precip_distribution/scripts_pcmdi/run_model_parallel.bash
@@ -0,0 +1,3 @@
+mkdir -p ./log
+nohup python -u parallel_driver_cmip.py  -p ../param/precip_distribution_params_cmip5.py &> ./log/log_parallel.wait_cmip5 & disown
+nohup python -u parallel_driver_cmip.py -p ../param/precip_distribution_params_cmip6.py &> ./log/log_parallel.wait_cmip6 & disown
