522790d8a073eec4834376078ca4681cb0518dd5
Ana Ordonez <anaordonez@comcast.net>
2023-05-25 13:53:04 -0700

    add regions

--- pcmdi_metrics/extremes/pmp_exremes_driver.py
+++ pcmdi_metrics/extremes/pmp_exremes_driver.py
@@ -20,43 +20,56 @@ from lib import (
 from pcmdi_metrics.io.base import Base
 
 def mask_region(data,name,coords=None,shp_path=None,column=None):
-   
-    lon = data["lon"]
-    lat = data["lat"]
+    # Return data masked from coordinate list or shapefile.
+    # Masks a single region
+    print("Creating region mask.")
+
+    lon = data["lon"].data
+    lat = data["lat"].data
 
     # Option 1: Region is defined by coord pairs
     if coords is not None:
-        print("Masking region",coords)
-        regions = regionmask.Regions([coords])
-        mask = regions.mask(lon, lat)
+        try:
+            names=[name]
+            regions = regionmask.Regions([np.array(coords)],names=names)
+            mask = regions.mask(lon, lat)
+            val=0
+        except Exception as e:
+            print("Error in creating mask from provided coordinates:")
+            print("  ",e)
+            return
 
     # Option 2: region is defined by shapefile
     elif shp_path is not None:
-        print("Loading region from file",shp_path)
-        regions_file = gpd.read_file(shp_path)
-        if column is not None:
-            regions = regionmask.from_geopandas(regions_file,names=column)
-        else:
-            print("Column name not provided.")
-            regions = regionmask.from_geopandas(regions_file)
-        mask = regions.mask(lon, lat)
+        try:
+            regions_file = gpd.read_file(shp_path)
+            if column is not None:
+                regions = regionmask.from_geopandas(regions_file,names=column)
+            else:
+                print("Column name not provided.")
+                regions = regionmask.from_geopandas(regions_file)
+            mask = regions.mask(lon, lat)
+            # Can't match mask by name, rather index of name
+            val = list(regions_file[col]).index(name)
+        except Exception as e:
+            print("Error in creating mask from shapefile:")
+            print("  ",e)
+            return
 
     else:
         print("Region coordinates or shapefile must be provided.")
         print("Defaulting to global analysis.")
         return
-
+    
     try:
-        masked_data = data.where(mask == name)
-    except Error as e:
+        masked_data = data.where(mask == val)
+    except Exception as e:
         print("Error: Masking failed. Defaulting to global analysis.")
         print(e)
-        return
+        return data
 
     return  masked_data
     
-
-
 def write_to_nc(filepath,ds):
     try:
         ds.to_netcdf(filepath,mode="w")
@@ -89,6 +102,74 @@ def write_to_json(outdir,json_filename,json_dict):
     )
     return
 
+def check_region_params(shp_path,coords,region_name,col,default):
+    use_region_mask = False
+    
+    if shp_path is not None:
+        use_region_mask = True
+        if not os.path.exists(shp_path):
+            print("Error: Shapefile path does not exist.")
+            print("Must provide valid shapefile path.")
+            sys.exit()
+        if region_name is None:
+            print("Error: Region name parameter --region_name must be provided with shapefile.")
+            sys.exit()
+        if col is None:
+            print("Error: Column name parameter --column must be provided with shapefile.")
+            sys.exit()
+        print("Region settings are:")
+        print("  Shapefile:",shp_path)
+        print("  Column name:",col)
+        print("  Region name:",region_name)
+    elif coords is not None:    
+        use_region_mask = True
+        if isinstance(coords,str):
+            # Might be string when ingested from command line, convert to list
+            tmp=coords.replace("[","").replace("]","").split(",")
+            coords=[[float(tmp[n]),float(tmp[n+1])] for n in range(0,len(tmp)-1,2)]
+        if region_name is None:
+            print("No region name provided. Using 'custom'.")
+            region_name = "custom"
+        print("Region settings are:")
+        print("  Coordinates:",coords)
+        print("  Region name:",region_name)
+    else:
+        region_name = default
+    
+    return use_region_mask,region_name
+
+def verify_output_path(metrics_output_path,case_id):
+    if metrics_output_path is None:
+        metrics_output_path = datetime.datetime.now().strftime("v%Y%m%d")
+    if case_id is not None:
+        metrics_output_path = metrics_output_path.replace('%(case_id)', case_id)
+    if not os.path.exists(metrics_output_path):
+        print("\nMetrics output path not found.")
+        print("Creating metrics output directory",metrics_output_path)
+        try:
+            os.makedirs(metrics_output_path)
+        except Error as e:
+            print("\nError: Could not create metrics output path",metrics_output_path)
+            print(e)
+            print("Exiting.")
+            sys.exit()
+    return metrics_output_path
+
+def set_up_realizations(realization):
+    find_all_realizations = False
+    if realization is None:
+        realization = ""
+        realizations = [realization]
+    elif isinstance(realization, str):
+        if realization.lower() in ["all", "*"]:
+            find_all_realizations = True
+        else:
+            realizations = [realization]
+    elif isinstance(realization,list):
+        realizations = realization
+    
+    return find_all_realizations,realizations
+
 ##########
 # Set up
 ##########
@@ -119,44 +200,29 @@ annual_strict = parameter.annual_strict
 exclude_leap = parameter.exclude_leap
 dec_mode = parameter.dec_mode
 drop_incomplete_djf = parameter.drop_incomplete_djf
+# Region masking
+shp_path = parameter.shp_path
+col = parameter.column
+region_name = parameter.region_name
+coords = parameter.coords
+
+# Check the region masking parameters, if provided
+use_region_mask,region_name = check_region_params(shp_path,coords,region_name,col,"land")
 
 # Verifying output directory
-if metrics_output_path is None:
-    metrics_output_path = datetime.datetime.now().strftime("v%Y%m%d")
-if case_id is not None:
-    metrics_output_path = parameter.metrics_output_path.replace('%(case_id)', case_id)
-if not os.path.exists(metrics_output_path):
-    print("\nMetrics output path not found.")
-    print("Creating metrics output directory",metrics_output_path)
-    try:
-        os.makedirs(metrics_output_path)
-    except Error as e:
-        print("\nError: Could not create metrics output path",metrics_output_path)
-        print(e)
-        print("Exiting.")
-        sys.exit()
+metrics_output_path = verify_output_path(metrics_output_path,case_id)
 
 obs = {}
 # TODO: Obs will likely need to be converted to model grid
 
 # Setting up model realization list
-find_all_realizations = False
-if realization is None:
-    realization = ""
-    realizations = [realization]
-elif isinstance(realization, str):
-    if realization.lower() in ["all", "*"]:
-        find_all_realizations = True
-    else:
-        realizations = [realization]
-elif isinstance(realization,list):
-    realizations = realization
+find_all_realizations,realizations = set_up_realizations(realization)
 
 # Initialize output JSON structures
 # FYI: if new metrics are added to this analysis, 
 # the index object in the compute_metrics.init_metrics_dict
 # code must be updated manually.
-metrics_dict = compute_metrics.init_metrics_dict(dec_mode,drop_incomplete_djf,annual_strict)
+metrics_dict = compute_metrics.init_metrics_dict(dec_mode,drop_incomplete_djf,annual_strict,region_name)
 
 # Only include reference data in loop if it exists
 if reference_data_path is not None:
@@ -206,6 +272,8 @@ for model in model_loop_list:
         # Stats calculation is expecting sfltf scaled from 0-100
         if sftlf["sftlf"].max() <= 20:
             sftlf["sftlf"] = sftlf["sftlf"] * 100.
+        if use_region_mask:
+            sftlf = mask_region(sftlf,region_name,coords=coords,shp_path=shp_path,column=col)
         
         metrics_dict["RESULTS"][model][run] = {}
         
@@ -237,6 +305,9 @@ for model in model_loop_list:
             else:
                 ds = xcdat.open_dataset(test_data_full_path[0])
 
+            if use_region_mask:
+                ds = mask_region(ds,region_name,coords=coords,shp_path=shp_path,column=col)
+
             if start_year is not None and end_year is not None:
                 start_time = cftime.datetime(start_year,1,1) - datetime.timedelta(days=0)
                 end_time = cftime.datetime(end_year+1,1,1) - datetime.timedelta(days=1)
@@ -259,9 +330,9 @@ for model in model_loop_list:
 
                 if nc_out:
                     print("Writing results to netCDF.")
-                    filepath = os.path.join(metrics_output_path,"TXx_{0}.nc".format("_".join([model,run])))
+                    filepath = os.path.join(metrics_output_path,"TXx_{0}.nc".format("_".join([model,run,region_name])))
                     write_to_nc(filepath,TXx)
-                    filepath = os.path.join(metrics_output_path,"TXn_{0}.nc".format("_".join([model,run])))
+                    filepath = os.path.join(metrics_output_path,"TXn_{0}.nc".format("_".join([model,run,region_name])))
                     write_to_nc(filepath,TXn)
    
             if varname == "tasmin":
@@ -275,9 +346,9 @@ for model in model_loop_list:
 
                 if nc_out:
                     print("Writing results to netCDF.")
-                    filepath = os.path.join(metrics_output_path,"TNx_{0}.nc".format("_".join([model,run])))
+                    filepath = os.path.join(metrics_output_path,"TNx_{0}.nc".format("_".join([model,run,region_name])))
                     write_to_nc(filepath,TNx)
-                    filepath = os.path.join(metrics_output_path,"TNn_{0}.nc".format("_".join([model,run])))
+                    filepath = os.path.join(metrics_output_path,"TNn_{0}.nc".format("_".join([model,run,region_name])))
                     write_to_nc(filepath,TNx)
 
             if varname in ["pr","PRECT","precip"]:
@@ -294,14 +365,14 @@ for model in model_loop_list:
 
                 if nc_out:
                     print("Writing results to netCDF.")
-                    filepath = os.path.join(metrics_output_path,"Rx1day_{0}.nc".format("_".join([model,run])))
+                    filepath = os.path.join(metrics_output_path,"Rx1day_{0}.nc".format("_".join([model,run,region_name])))
                     write_to_nc(filepath,Rx1day)
-                    filepath = os.path.join(metrics_output_path,"Rx5day_{0}.nc".format("_".join([model,run])))
+                    filepath = os.path.join(metrics_output_path,"Rx5day_{0}.nc".format("_".join([model,run,region_name])))
                     write_to_nc(filepath,Rx5day)
             
             # Get stats and update metrics dictionary
-            print("Generating metrics")
-            result_dict = compute_metrics.metrics_json(stats_dict,sftlf,obs_dict=obs)
+            print("Generating metrics.")
+            result_dict = compute_metrics.metrics_json(stats_dict,sftlf,obs_dict=obs,region=region_name)
             metrics_dict["RESULTS"][model][run].update(result_dict)
             if run not in metrics_dict["DIMENSIONS"]["realization"]:
                 metrics_dict["DIMENSIONS"]["realization"].append(run)
