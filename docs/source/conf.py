d3163bea2f7b05a6c003421a49d0ee15601b2efb
Jiwoo Lee <lee1043@llnl.gov>
2023-06-20 14:46:19 -0700

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_autosummary_accessors
 
 # -- Project information -----------------------------------------------------
 
@@ -30,8 +31,7 @@ author = 'PCMDI'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -59,10 +59,32 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+html_theme = "sphinx_book_theme"
+
+# Theme options are theme-specific and customize the look and feel of a
+# theme further.  For a list of options available for each theme, see the
+# documentation.
 #
+# sphinx_book_theme configurations
+# https://sphinx-book-theme.readthedocs.io/en/latest/configure.html
+html_logo = "_static/PCMDILogo_300x98px_72dpi.png"
+html_title = "PMP Documentation"
+html_theme_options = {
+    "repository_url": "https://github.com/PCMDI/pcmdi_metrics",
+    "repository_branch": "main",
+    "path_to_docs": "docs",
+    "use_edit_page_button": True,
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_download_button": True,
+    "use_fullscreen_button": True,
+}
+
+"""
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+"""
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
