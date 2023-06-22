3b4b892cbdc088b6a23d715bf7a6139ec061bdc5
Jiwoo Lee <lee1043@llnl.gov>
2023-06-22 11:39:39 -0700

    to avoid github action docs (push) build error

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_autosummary_accessors
+#import sphinx_autosummary_accessors
 
 # -- Project information -----------------------------------------------------
 
