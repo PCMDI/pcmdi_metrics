f19a24749f05e026751e8f5d4b79a4afe1c6fcf8
Ana Ordonez <anaordonez@comcast.net>
2023-07-18 11:27:03 -0700

    changes

--- pcmdi_metrics/extremes/param/daily_model_data.py
+++ pcmdi_metrics/extremes/param/daily_model_data.py
@@ -2,31 +2,38 @@
 
 vars = ["pr"]
 
-test_data_set = ["MRI-ESM2-0"]
+#test_data_set = ["MRI-ESM2-0"]
 #test_data_set = ["MIROC6"]
-realization = ["r1i1p1f1"]
-test_data_path = "/p/css03/esgf_publish/CMIP6/CMIP/*/%(model)/1pctCO2/%(realization)/day/%(variable)/*/*/"
-filename_template = "%(variable)_day_%(model)_1pctCO2_%(realization)_gn_19500101-19991231.nc"
-sftlf_filename_template = "/p/css03/esgf_publish/CMIP6/CMIP/*/%(model)/historical/r1i1p1f1/fx/sftlf/*/*/sftlf_fx_%(model)_historical_r1i1p1f1_*.nc"
+#realization = ["r1i1p1f1"]
+#test_data_path = "/p/css03/esgf_publish/CMIP6/CMIP/*/%(model)/1pctCO2/%(realization)/day/%(variable)/*/*/"
+#filename_template = "%(variable)_day_%(model)_1pctCO2_%(realization)_gn_19500101-19991231.nc"
+#sftlf_filename_template = "/p/css03/esgf_publish/CMIP6/CMIP/*/%(model)/historical/r1i1p1f1/fx/sftlf/*/*/sftlf_fx_%(model)_historical_r1i1p1f1_*.nc"
 
-metrics_output_path = "debug_nc/"
+metrics_output_path = "giss-pr-nonst/"
 
 #test_data_set = ["MIROC6","GISS-E2-1-G"]
-#test_data_set = ["GISS-E2-1-G"]
+test_data_set = ["GISS-E2-2-G"]
 #test_data_set = ["CESM2"]
-#realization = ["r1i1p1f1"]
-#test_data_path = "/p/user_pub/xclim/CMIP6/CMIP/historical/atmos/day/%(variable)"
-#filename_template = "CMIP6.CMIP.historical.*.%(model).%(realization).day.%(variable).atmos.*.v????????.0000000.0.xml"
-#sftlf_filename_template = "/p/css03/esgf_publish/CMIP6/CMIP/*/%(model)/piControl/r1i1p1f1/fx/sftlf/gn/v????????/sftlf_fx_%(model)_piControl_r1i1p1f1_gn.nc"
+realization = ["r1i1p1f1","r1i1p3f1"]
+test_data_path = "/p/user_pub/xclim/CMIP6/CMIP/historical/atmos/day/%(variable)"
+filename_template = "CMIP6.CMIP.historical.*.%(model).%(realization).day.%(variable).atmos.*.v????????.0000000.0.xml"
+sftlf_filename_template = "/p/css03/esgf_publish/CMIP6/CMIP/*/%(model)/piControl/r1i1p1f1/fx/sftlf/gn/v????????/sftlf_fx_%(model)_piControl_r1i1p1f1_gn.nc"
 
 #case_id = ver
 dec_mode="DJF"
 annual_strict = False
 drop_incomplete_djf = True
-nc_out=True
 regrid=False
-plots=False
+plots=True
 generate_sftlf = False
+msyear = 1970
+meyear = 1999
+return_period = 20
+
+ModUnitsAdjust=(True,"multiply",86400,"mm/day")
+
+covariate = "mole_fraction_of_carbon_dioxide_in_air"
+covariate_path = "/home/ordonez4/git/pcmdi_metrics/pcmdi_metrics/extremes/co2_annual_1950-1999.nc"
 
 #year_range = [1900,1930]
 #coords="[[0,100],[100,100],[100,0],[0,0]]"
