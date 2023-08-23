da35fe9a810fddaae78a6a44fba512bdf50605c4
Ana Ordonez <ordonez4@llnl.gov>
2023-08-23 13:42:51 -0700

    address xesmf zeros issue

--- pcmdi_metrics/precip_variability/lib/lib_variability_across_timescales.py
+++ pcmdi_metrics/precip_variability/lib/lib_variability_across_timescales.py
@@ -120,6 +120,15 @@ def RegridHoriz(d, var, res):
 
     tgrid = grid.create_uniform_grid(start_lat,end_lat,res,start_lon,end_lon,res)
     drg = d.regridder.horizontal(var, tgrid, tool="xesmf", method="conservative_normed",periodic=True)[var]
+
+    # Workaround for regional grid cases where areas outside
+    # original grid might be populated with 0's due to xesmf.
+    drg = drg.where(
+        (drg.lat >= d.lat.min()) &
+        (drg.lat <= d.lat.max()) &
+        (drg.lon >= d.lon.min()) &
+        (drg.lon <= d.lon.max())
+    )
     
     print("Complete regridding from", d[var].shape, "to", drg.shape)
     return drg
