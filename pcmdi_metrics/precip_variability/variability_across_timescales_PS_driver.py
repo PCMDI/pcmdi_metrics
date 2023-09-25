5f44c39e603a53a5cffbf7ef5a9245ce25d915f1
Ana Ordonez <ordonez4@llnl.gov>
2023-09-25 13:53:02 -0700

    Remove string constructor

--- pcmdi_metrics/precip_variability/variability_across_timescales_PS_driver.py
+++ pcmdi_metrics/precip_variability/variability_across_timescales_PS_driver.py
@@ -33,9 +33,6 @@ cmec = param.cmec
 # Create output directory
 case_id = param.case_id
 outdir_template = param.process_templated_argument("results_dir")
-outdir = StringConstructor(
-    str(outdir_template(output_type="%(output_type)", mip=mip, case_id=case_id))
-)
 outdir_template = outdir_template.replace("%(mip)",mip).replace("%(case_id)",case_id)
 for output_type in ["graphics", "diagnostic_results", "metrics_results"]:
     outdir = outdir_template.replace("%(output_type)",output_type)
