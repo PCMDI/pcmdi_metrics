3ce145399c45910ef1353104d38ebb370307dbbf
Ana Ordonez <ordonez4@llnl.gov>
2023-10-20 10:50:51 -0700

    Update driver name

--- cmec/extremes/pmp_extremes_driver.sh
+++ cmec/extremes/pmp_extremes_driver.sh
@@ -10,7 +10,7 @@ tmp_param=$CMEC_WK_DIR/extremes_param.py
 python $CMEC_CODE_DIR/../scripts/pmp_param_generator.py $CMEC_CONFIG_DIR/cmec.json $tmp_param "extremes"
 
 if [[ $? = 0 ]]; then
-    pmp_extremeds_driver.py -p $tmp_param
+    extremes_driver.py -p $tmp_param
 
 else
     echo "Failure in PMP/extremes parameter file generation"
