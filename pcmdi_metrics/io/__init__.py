20be92e26763ca5f1ac8c69764580c326b928c04
Jiwoo Lee <lee1043@llnl.gov>
2024-01-04 11:21:00 -0800

    update

--- pcmdi_metrics/io/__init__.py
+++ pcmdi_metrics/io/__init__.py
@@ -4,4 +4,19 @@ from . import base  # noqa
 from .base import MV2Json  # noqa
 from .default_regions_define import load_regions_specs  # noqa
 from .default_regions_define import region_subset  # noqa
-from .select_subset import select_subset  # noqa
+from .xcdat_xarray_dataset_io import (  # noqa
+    get_axis_list,
+    get_lat_bounds_key,
+    get_lat_key,
+    get_latitude,
+    get_latitude_bounds,
+    get_lon_bounds_key,
+    get_lon_key,
+    get_longitude,
+    get_longitude_bounds,
+    get_time,
+    get_time_bound,
+    get_time_bounds_key,
+    get_time_key,
+    select_subset,
+)
