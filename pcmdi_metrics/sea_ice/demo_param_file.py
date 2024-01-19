c215ad9dd714ae06503fbbe0a73b70ad3ecebd4d
Ana Ordonez <anaordonez@comcast.net>
2024-01-19 15:27:35 -0800

    add obs

--- pcmdi_metrics/sea_ice/demo_param_file.py
+++ pcmdi_metrics/sea_ice/demo_param_file.py
@@ -2,7 +2,7 @@
 
 # List of models to include in analysis
 test_data_set = [
-    "E3SM-1-0"
+    "E3SM-1-0",
 ]
 
 # realization can be a single realization, a list of realizations, or "*" for all realizations
@@ -37,3 +37,16 @@ AreaUnitsAdjust = (True, "multiply", 1e-6)
 # Directory for writing outputs
 case_id = "ex1"
 metrics_output_path = "sea_ice_demo/%(case_id)/"
+
+# Settings for the observational data
+reference_data_path_nh = "/work/ordonez4/ice_conc_nh_ease2-250_cdr-v3p0_198801-202012.nc"
+reference_data_path_sh = "/work/ordonez4/ice_conc_sh_ease2-250_cdr-v3p0_198801-202012.nc"
+ObsUnitsAdjust=(True,"multiply",1e-2)
+reference_data_set="OSI-SAF"
+osyear=1988
+oeyear=2020
+obs_var="ice_conc"
+ObsAreaUnitsAdjust = (False, 0, 0)
+obs_area_template = None
+obs_area_var = None
+obs_cell_area = 625 #km 2
