05e3216049da34cb01268c401caa89ac04be2023
Ana Ordonez <anaordonez@comcast.net>
2024-01-25 15:53:43 -0800

    comments

--- pcmdi_metrics/sea_ice/param/parameter_file.py
+++ pcmdi_metrics/sea_ice/param/parameter_file.py
@@ -1,21 +1,3 @@
-# CMIP5
-# =========
-# Model settings
-# test_data_set=["ACCESS1-3","GISS-E2-H","NorESM1-M"]
-# test_data_set=["ACCESS1-3"]
-# realization=["*"]
-# test_data_path = "/p/user_pub/pmp/pmp_results/pmp_v1.1.2/additional_xmls/latest/v20231104/cmip5/historical/seaIce/mon/sic/"
-# filename_template= "cmip5.historical.%(model).%(realization).mon.sic.xml"
-# var="sic"
-# msyear=1981
-# meyear=2010
-# ModUnitsAdjust=(True,"multiply",1e-2)
-
-# Model area file
-# area_template="/p/user_pub/hoang1-backups/ARCHIVE/ivanova2/IceMetrics/CMIP5/AREACELLO/areacello_fx_%(model)_historical_r0i0p0.nc"
-# area_var = "areacello"
-# AreaUnitsAdjust = (True, "multiply", 1e-6)
-
 # CMIP6
 # =======
 case_id = "cmip6_osi-saf"
@@ -44,7 +26,7 @@ AreaUnitsAdjust = (True, "multiply", 1e-6)
 metrics_output_path = "demo/%(case_id)/"
 
 
-# Reference is hard coded currently so this is a placeholder
+# OSI-SAF data
 reference_data_path_nh = (
     "/p/user_pub/PCMDIobs/obs4MIPs_input/EUMETSAT/OSI-SAF-450-a-3-0/v20231201/*nh*"
 )
