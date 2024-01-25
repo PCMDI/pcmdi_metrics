60c0e41348a4e622f0ed59d1394d8cb6c889c401
Ana Ordonez <anaordonez@comcast.net>
2024-01-25 13:03:15 -0800

    clean up

--- pcmdi_metrics/sea_ice/param/parameter_file.py
+++ pcmdi_metrics/sea_ice/param/parameter_file.py
@@ -45,14 +45,18 @@ metrics_output_path = "demo/%(case_id)/"
 
 
 # Reference is hard coded currently so this is a placeholder
-reference_data_path_nh = "/p/user_pub/PCMDIobs/obs4MIPs_input/EUMETSAT/OSI-SAF-450-a-3-0/v20231201/*nh*"
-reference_data_path_sh = "/p/user_pub/PCMDIobs/obs4MIPs_input/EUMETSAT/OSI-SAF-450-a-3-0/v20231201/*sh*"
-ObsUnitsAdjust=(True,"multiply",1e-2)
-reference_data_set="OSI-SAF"
-osyear=1981
-oeyear=2010
-obs_var="ice_conc"
+reference_data_path_nh = (
+    "/p/user_pub/PCMDIobs/obs4MIPs_input/EUMETSAT/OSI-SAF-450-a-3-0/v20231201/*nh*"
+)
+reference_data_path_sh = (
+    "/p/user_pub/PCMDIobs/obs4MIPs_input/EUMETSAT/OSI-SAF-450-a-3-0/v20231201/*sh*"
+)
+ObsUnitsAdjust = (True, "multiply", 1e-2)
+reference_data_set = "OSI-SAF"
+osyear = 1981
+oeyear = 2010
+obs_var = "ice_conc"
 ObsAreaUnitsAdjust = (False, 0, 0)
-obs_area_template = None #km2
+obs_area_template = None  # km2
 obs_area_var = None
 obs_cell_area = 625
