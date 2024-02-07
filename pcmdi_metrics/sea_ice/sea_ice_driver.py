a4581ffa36056f0aa9c426618c0d79ad1e97f7d5
Ana Ordonez <anaordonez@comcast.net>
2024-02-07 10:11:47 -0800

    add plot

--- pcmdi_metrics/sea_ice/sea_ice_driver.py
+++ pcmdi_metrics/sea_ice/sea_ice_driver.py
@@ -435,6 +435,7 @@ if __name__ == "__main__":
     meyear = parameter.meyear
     osyear = parameter.osyear
     oeyear = parameter.oeyear
+    plot = parameter.plot
 
     print(model_list)
     model_list.sort()
@@ -812,129 +813,134 @@ if __name__ == "__main__":
     # ----------------
     # Make figure
     # ----------------
-    sector_list = [
-        "Central Arctic Sector",
-        "North Atlantic Sector",
-        "North Pacific Sector",
-        "Indian Ocean Sector",
-        "South Atlantic Sector",
-        "South Pacific Sector",
-    ]
-    sector_short = ["ca", "na", "np", "io", "sa", "sp"]
-    fig7, ax7 = plt.subplots(6, 1, figsize=(5, 9))
-    mlabels = model_list
-    ind = np.arange(len(mlabels))  # the x locations for the groups
-    width = 0.3
-    n = len(ind)
-    for inds, sector in enumerate(sector_list):
-        # Assemble data
-        mse_clim = []
-        mse_ext = []
-        clim_range = []
-        ext_range = []
-        clim_err_x = []
-        clim_err_y = []
-        ext_err_y = []
-        rgn = sector_short[inds]
-        for nmod, model in enumerate(model_list):
-            mse_clim.append(
-                float(
-                    metrics["RESULTS"][model][rgn]["model_mean"][reference_data_set][
-                        "monthly_clim"
-                    ]["mse"]
+    if plot:
+        sector_list = [
+            "Central Arctic Sector",
+            "North Atlantic Sector",
+            "North Pacific Sector",
+            "Indian Ocean Sector",
+            "South Atlantic Sector",
+            "South Pacific Sector",
+        ]
+        sector_short = ["ca", "na", "np", "io", "sa", "sp"]
+        fig7, ax7 = plt.subplots(6, 1, figsize=(5, 9))
+        mlabels = model_list
+        ind = np.arange(len(mlabels))  # the x locations for the groups
+        width = 0.3
+        n = len(ind)
+        for inds, sector in enumerate(sector_list):
+            # Assemble data
+            mse_clim = []
+            mse_ext = []
+            clim_range = []
+            ext_range = []
+            clim_err_x = []
+            clim_err_y = []
+            ext_err_y = []
+            rgn = sector_short[inds]
+            for nmod, model in enumerate(model_list):
+                mse_clim.append(
+                    float(
+                        metrics["RESULTS"][model][rgn]["model_mean"][
+                            reference_data_set
+                        ]["monthly_clim"]["mse"]
+                    )
                 )
-            )
-            mse_ext.append(
-                float(
-                    metrics["RESULTS"][model][rgn]["model_mean"][reference_data_set][
-                        "total_extent"
-                    ]["mse"]
+                mse_ext.append(
+                    float(
+                        metrics["RESULTS"][model][rgn]["model_mean"][
+                            reference_data_set
+                        ]["total_extent"]["mse"]
+                    )
                 )
-            )
-            # Get spread, only if there are multiple realizations
-            if len(metrics["RESULTS"][model][rgn].keys()) > 2:
-                for r in metrics["RESULTS"][model][rgn]:
-                    if r != "model_mean":
-                        clim_err_x.append(ind[nmod])
-                        clim_err_y.append(
-                            float(
-                                metrics["RESULTS"][model][rgn][r][reference_data_set][
-                                    "monthly_clim"
-                                ]["mse"]
+                # Get spread, only if there are multiple realizations
+                if len(metrics["RESULTS"][model][rgn].keys()) > 2:
+                    for r in metrics["RESULTS"][model][rgn]:
+                        if r != "model_mean":
+                            clim_err_x.append(ind[nmod])
+                            clim_err_y.append(
+                                float(
+                                    metrics["RESULTS"][model][rgn][r][
+                                        reference_data_set
+                                    ]["monthly_clim"]["mse"]
+                                )
                             )
-                        )
-                        ext_err_y.append(
-                            float(
-                                metrics["RESULTS"][model][rgn][r][reference_data_set][
-                                    "total_extent"
-                                ]["mse"]
+                            ext_err_y.append(
+                                float(
+                                    metrics["RESULTS"][model][rgn][r][
+                                        reference_data_set
+                                    ]["total_extent"]["mse"]
+                                )
                             )
-                        )
 
-        # plot data
-        if len(model_list) < 4:
-            mark_size = 9
-        elif len(model_list) < 12:
-            mark_size = 3
-        else:
-            mark_size = 1
-        ax7[inds].bar(ind - width / 2.0, mse_clim, width, color="b", label="Ann. Cycle")
-        ax7[inds].bar(ind, mse_ext, width, color="r", label="Ann. Mean")
-        if len(clim_err_x) > 0:
-            ax7[inds].scatter(
-                [x - width / 2.0 for x in clim_err_x],
-                clim_err_y,
-                marker="D",
-                s=mark_size,
-                color="k",
+            # plot data
+            if len(model_list) < 4:
+                mark_size = 9
+            elif len(model_list) < 12:
+                mark_size = 3
+            else:
+                mark_size = 1
+            ax7[inds].bar(
+                ind - width / 2.0, mse_clim, width, color="b", label="Ann. Cycle"
             )
-            ax7[inds].scatter(clim_err_x, ext_err_y, marker="D", s=mark_size, color="k")
-        # xticks
-        if inds == len(sector_list) - 1:
-            ax7[inds].set_xticks(ind + width / 2.0, mlabels, rotation=90, size=7)
-        else:
-            ax7[inds].set_xticks(ind + width / 2.0, labels="")
-        # yticks
-        if len(clim_err_y) > 0:
-            datamax = np.max(np.array(clim_err_y))
-        else:
-            datamax = np.max(np.array(mse_clim))
-        ymax = (datamax) * 1.3
-        ax7[inds].set_ylim(0.0, ymax)
-        if ymax < 0.1:
-            ticks = np.linspace(0, 0.1, 6)
-            labels = [str(round(x, 3)) for x in ticks]
-        elif ymax < 1:
-            ticks = np.linspace(0, 1, 5)
-            labels = [str(round(x, 1)) for x in ticks]
-        elif ymax < 4:
-            ticks = np.linspace(0, round(ymax), num=round(ymax / 2) * 2 + 1)
-            labels = [str(round(x, 1)) for x in ticks]
-        elif ymax > 10:
-            ticks = range(0, round(ymax), 5)
-            labels = [str(round(x, 0)) for x in ticks]
-        else:
-            ticks = range(0, round(ymax))
-            labels = [str(round(x, 0)) for x in ticks]
+            ax7[inds].bar(ind, mse_ext, width, color="r", label="Ann. Mean")
+            if len(clim_err_x) > 0:
+                ax7[inds].scatter(
+                    [x - width / 2.0 for x in clim_err_x],
+                    clim_err_y,
+                    marker="D",
+                    s=mark_size,
+                    color="k",
+                )
+                ax7[inds].scatter(
+                    clim_err_x, ext_err_y, marker="D", s=mark_size, color="k"
+                )
+            # xticks
+            if inds == len(sector_list) - 1:
+                ax7[inds].set_xticks(ind + width / 2.0, mlabels, rotation=90, size=7)
+            else:
+                ax7[inds].set_xticks(ind + width / 2.0, labels="")
+            # yticks
+            if len(clim_err_y) > 0:
+                datamax = np.max(np.array(clim_err_y))
+            else:
+                datamax = np.max(np.array(mse_clim))
+            ymax = (datamax) * 1.3
+            ax7[inds].set_ylim(0.0, ymax)
+            if ymax < 0.1:
+                ticks = np.linspace(0, 0.1, 6)
+                labels = [str(round(x, 3)) for x in ticks]
+            elif ymax < 1:
+                ticks = np.linspace(0, 1, 5)
+                labels = [str(round(x, 1)) for x in ticks]
+            elif ymax < 4:
+                ticks = np.linspace(0, round(ymax), num=round(ymax / 2) * 2 + 1)
+                labels = [str(round(x, 1)) for x in ticks]
+            elif ymax > 10:
+                ticks = range(0, round(ymax), 5)
+                labels = [str(round(x, 0)) for x in ticks]
+            else:
+                ticks = range(0, round(ymax))
+                labels = [str(round(x, 0)) for x in ticks]
 
-        ax7[inds].set_yticks(ticks, labels, fontsize=8)
-        # labels etc
-        ax7[inds].set_ylabel("10${^1}{^2}$km${^4}$", size=8)
-        ax7[inds].grid(True, linestyle=":")
-        ax7[inds].annotate(
-            sector,
-            (0.35, 0.8),
-            xycoords="axes fraction",
-            size=9,
+            ax7[inds].set_yticks(ticks, labels, fontsize=8)
+            # labels etc
+            ax7[inds].set_ylabel("10${^1}{^2}$km${^4}$", size=8)
+            ax7[inds].grid(True, linestyle=":")
+            ax7[inds].annotate(
+                sector,
+                (0.35, 0.8),
+                xycoords="axes fraction",
+                size=9,
+            )
+        # Add legend, save figure
+        ax7[0].legend(loc="upper right", fontsize=6)
+        plt.suptitle("Mean Square Error relative to " + reference_data_set, y=0.91)
+        figfile = os.path.join(metrics_output_path, "MSE_bar_chart.png")
+        plt.savefig(figfile)
+        meta.update_plots(
+            "bar_chart", figfile, "regional_bar_chart", "Bar chart of regional MSE"
         )
-    # Add legend, save figure
-    ax7[0].legend(loc="upper right", fontsize=6)
-    plt.suptitle("Mean Square Error relative to " + reference_data_set, y=0.91)
-    figfile = os.path.join(metrics_output_path, "MSE_bar_chart.png")
-    plt.savefig(figfile)
-    meta.update_plots(
-        "bar_chart", figfile, "regional_bar_chart", "Bar chart of regional MSE"
-    )
 
     # Update and write metadata file
     try:
