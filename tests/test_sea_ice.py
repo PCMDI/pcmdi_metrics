35ce04d4ecee496b50cbd35a623b66d88a1c4fe9
Ana Ordonez <ordonez4@llnl.gov>
2024-02-21 16:47:36 -0800

    add tests

--- tests/test_sea_ice.py
+++ tests/test_sea_ice.py
@@ -1,9 +1,13 @@
+import xarray as xr
+import numpy as np
+
+
 def create_fake_sea_ice_ds():
-    years = [1980, 1981]
+    years = [1980, 1999]
     times = xr.cftime_range(
         start="{0}-01-01".format(years[0]),
         end="{0}-12-31".format(years[1]),
-        freq="D",
+        freq="M",
         calendar="noleap",
         name="time",
     )
@@ -11,7 +15,7 @@ def create_fake_sea_ice_ds():
     lond = 2
 
     values = np.ones((len(times), latd, lond)) * 100.
-    lat = np.arange(0, latd)
+    lat = np.arange(80, 80+latd)
     lon = np.arange(0, lond)
     fake_ds = xr.Dataset(
         {
@@ -24,7 +28,7 @@ def create_fake_sea_ice_ds():
         }
     )
 
-    area = np.ones((latd,lond)) * 25
+    area = np.ones((latd,lond)) * 600
     fake_area = xr.Dataset(
       {
         "areacello": xr.DataArray(
@@ -39,14 +43,26 @@ def create_fake_sea_ice_ds():
     return fake_ds, fake_area
 
 def test_get_total_extent():
-    pass
+    siconc,area=create_fake_sea_ice_ds()
+    total_extent,te_mean=get_total_extent(siconc,area)
+
+    total_ext_true = 600.*len(area.lat)*len(area.lon)
+
+    assert te_mean == total_ext_true
 
 def test_mse_t():
-    pass
+    siconc,area=create_fake_sea_ice_ds()
+    mse = mse_t(siconc, siconc, weights=None):
+    assert mse == 0.
 
 def test_mse_model():
-    pass
+    siconc,area=create_fake_sea_ice_ds()    
+    mse = mse_model(siconc, siconc, var=None)
+    assert mse == 0.
 
 def test_adjust_units():
-    pass
+    ds,_=create_fake_sea_ice_ds()
+    adjust_tuple=(False, 0, 0)
+    dsnew = adjust_units(ds, adjust_tuple)
+    assert dsnew.equals(ds)
 
