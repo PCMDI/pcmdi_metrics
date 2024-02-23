e54e460df8640bd521edb91d058e36d866cf0310
Ana Ordonez <ordonez4@llnl.gov>
2024-02-23 11:35:54 -0800

    get tests working

--- tests/test_sea_ice.py
+++ tests/test_sea_ice.py
@@ -1,13 +1,15 @@
 import numpy as np
 import xarray as xr
 
+from pcmdi_metrics.sea_ice.lib import sea_ice_lib as lib
+
 
 def create_fake_sea_ice_ds():
     years = [1980, 1999]
     times = xr.cftime_range(
         start="{0}-01-01".format(years[0]),
         end="{0}-12-31".format(years[1]),
-        freq="M",
+        freq="ME",
         calendar="noleap",
         name="time",
     )
@@ -45,27 +47,69 @@ def create_fake_sea_ice_ds():
 
 def test_get_total_extent():
     siconc, area = create_fake_sea_ice_ds()
-    total_extent, te_mean = get_total_extent(siconc, area)
+    total_extent, te_mean = lib.get_total_extent(siconc.siconc / 100, area.areacello)
 
     total_ext_true = 600.0 * len(area.lat) * len(area.lon)
 
     assert te_mean == total_ext_true
 
 
-def test_mse_t():
+def test_mse_t_identical():
     siconc, area = create_fake_sea_ice_ds()
-    mse = mse_t(siconc, siconc, weights=None)
+    ts = (siconc.siconc / 100 * area.areacello).sum(("lat", "lon"))
+    mse = lib.mse_t(ts, ts, weights=None)
     assert mse == 0.0
 
 
-def test_mse_model():
+def test_mse_twos():
     siconc, area = create_fake_sea_ice_ds()
-    mse = mse_model(siconc, siconc, var=None)
-    assert mse == 0.0
+    ts = (siconc.siconc / 100 * area.areacello).sum(("lat", "lon"))
+    ts2 = ts.copy()
+    ts2 = ts2 + 2.0
+    mse = lib.mse_t(ts, ts2, weights=None)
+    assert mse == 4.0
 
 
-def test_adjust_units():
+def test_mse_model_identical():
+    siconc, area = create_fake_sea_ice_ds()
+    ts = (siconc.siconc / 100 * area.areacello).sum(("lat", "lon"))
+    mse = lib.mse_model(ts, ts, var=None)
+    mse_true = np.zeros(np.shape(ts))
+    assert np.array_equal(mse, mse_true)
+
+
+def test_mse_model_twos():
+    siconc, area = create_fake_sea_ice_ds()
+    ts = (siconc.siconc / 100 * area.areacello).sum(("lat", "lon"))
+    ts2 = ts.copy()
+    ts2 = ts2 + 2.0
+    mse = lib.mse_model(ts, ts2, var=None)
+    mse_true = np.ones(np.shape(ts)) * 4.0
+    assert np.array_equal(mse, mse_true)
+
+
+def test_adjust_units_False():
     ds, _ = create_fake_sea_ice_ds()
     adjust_tuple = (False, 0, 0)
-    dsnew = adjust_units(ds, adjust_tuple)
+    dsnew = lib.adjust_units(ds, adjust_tuple)
     assert dsnew.equals(ds)
+
+
+def test_adjust_units_True():
+    ds, _ = create_fake_sea_ice_ds()
+    adjust_tuple = (True, "multiply", 1e-2)
+    ds_new = lib.adjust_units(ds, adjust_tuple)
+    ds_true = ds
+    ds_true["siconc"] = ds_true.siconc * 1e-2
+    assert ds_new.equals(ds_true)
+
+
+def get_clim_time_bounds():
+    siconc, area = create_fake_sea_ice_ds()
+    ts = (siconc.siconc / 100 * area.areacello).sum(("lat", "lon"))
+    clim1 = lib.get_clim(ts, siconc, "siconc")
+    ts2 = ts.copy()
+    # Change time bounds name so bounds won't be found
+    ts2 = ts2.rename("time_bnds", "time_bounds")
+    clim2 = lib.get_clim(ts2, siconc, "siconc")
+    assert clim1.equals(clim2)
