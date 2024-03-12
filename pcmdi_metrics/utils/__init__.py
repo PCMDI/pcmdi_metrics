8ec3dac3946ce84981054035b21ae4b57149e56f
lee1043 <lee1043@llnl.gov>
2024-03-11 21:26:32 -0700

    clean up

--- pcmdi_metrics/utils/__init__.py
+++ pcmdi_metrics/utils/__init__.py
@@ -1,10 +1,10 @@
-from .land_sea_mask import apply_landmask, apply_oceanmask, create_land_sea_mask
 from .grid import (
     calculate_area_weights,
     calculate_grid_area,
     create_target_grid,
     regrid,
 )
+from .land_sea_mask import apply_landmask, apply_oceanmask, create_land_sea_mask
 from .sort_human import sort_human
 from .string_constructor import StringConstructor, fill_template
 from .tree_dict import tree
