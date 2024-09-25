d99cf173d734cfb3fb3ada4e34e5191d2cc773f5
Jiwoo Lee <lee1043@llnl.gov>
2024-09-25 12:34:38 -0700

    rewrite docstring to scipy style

--- pcmdi_metrics/io/xcdat_dataset_io.py
+++ pcmdi_metrics/io/xcdat_dataset_io.py
@@ -333,33 +333,47 @@ def select_subset(
     ds: xr.Dataset, lat: tuple = None, lon: tuple = None, time: tuple = None
 ) -> xr.Dataset:
     """
-    Selects a subset of the given xarray dataset based on specified latitude, longitude, and time ranges.
+    Select a subset of the given xarray dataset based on specified latitude, longitude, and time ranges.
 
-    Parameters:
-    - ds (xr.Dataset): The input xarray dataset.
-    - lat (tuple, optional): Latitude range in the form of (min, max).
-    - lon (tuple, optional): Longitude range in the form of (min, max).
-    - time (tuple, optional): Time range. If time is specified, it should be in the form of (start_time, end_time),
-      where start_time and end_time can be integers, floats, string, or cftime.datetime objects.
+    Parameters
+    ----------
+    ds : xr.Dataset
+        The input xarray dataset.
+    lat : tuple, optional
+        Latitude range in the form of (min, max).
+    lon : tuple, optional
+        Longitude range in the form of (min, max).
+    time : tuple, optional
+        Time range in the form of (start_time, end_time). start_time and end_time
+        can be integers, floats, strings, or cftime.datetime objects.
 
-    Returns:
-    - xr.Dataset: Subset of the input dataset based on the specified latitude, longitude, and time ranges.
+    Returns
+    -------
+    xr.Dataset
+        Subset of the input dataset based on the specified latitude, longitude, and time ranges.
 
-    Example Usage:
-    ```
-    import cftime
+    Notes
+    -----
+    This function allows for flexible subsetting of xarray datasets based on
+    geographical coordinates and time ranges.
 
-    # Define latitude, longitude, and time ranges
-    lat_tuple = (30, 50)  # Latitude range
-    lon_tuple = (110, 130)  # Longitude range
-    time_tuple = ("1850-01-01 00:00:00", "1851-12-31 23:59:59") # Time range
-
-    # Load your xarray dataset (ds) here
-
-    # Select subset based on specified ranges
-    ds_subset = select_subset(ds, lat=lat_tuple, lon=lon_tuple, time=time_tuple)
-    ```
+    Examples
+    --------
+    >>> import xarray as xr
+    >>> import cftime
+    >>>
+    >>> # Load your xarray dataset (ds) here
+    >>> ds = xr.open_dataset('path/to/your/dataset.nc')
+    >>>
+    >>> # Define latitude, longitude, and time ranges
+    >>> lat_tuple = (30, 50)  # Latitude range
+    >>> lon_tuple = (110, 130)  # Longitude range
+    >>> time_tuple = ("1850-01-01 00:00:00", "1851-12-31 23:59:59")  # Time range
+    >>>
+    >>> # Select subset based on specified ranges
+    >>> ds_subset = select_subset(ds, lat=lat_tuple, lon=lon_tuple, time=time_tuple)
     """
+
     sel_keys = {}
     if lat is not None:
         lat_key = get_latitude_key(ds)
