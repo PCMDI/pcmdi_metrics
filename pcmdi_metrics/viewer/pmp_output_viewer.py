c4971b495016de79bd47ef27e79bcd2d0562f92a
Kristin Chang <chang61@llnl.gov>
2024-12-13 15:06:09 -0800

    Adding docstrings

--- pcmdi_metrics/viewer/pmp_output_viewer.py
+++ pcmdi_metrics/viewer/pmp_output_viewer.py
@@ -1,4 +1,4 @@
-# - Generate an html file of compiled PMP results using Bokeh.
+# - Generate an html file of compiled PMP graphics using Bokeh.
 # - Author: Kristin Chang (2024.12)
 # - Last Update: 2024.12
 
@@ -16,25 +16,53 @@ from pcmdi_metrics.graphics import download_archived_results
 from pcmdi_metrics.graphics import Metrics
 
 def view_pmp_output(
-        dir_path: Optional[str] = None,
-        season_map: Optional[Dict[str, Dict[str, Union[int, float, str]]]] = None,
+        graphics_path: Optional[str] = None,
+        version: str = "v20241101",
+        season_map: Dict[str, Dict[str, Union[int, float, str]]] = {'AC':'ANN'},
         compare_cmip6: bool = False
 ):
     """
-    Generates an html file of compiled PMP results using Bokeh. 
+    Generates an html file of compiled PMP graphics using Bokeh. 
 
     Parameters
     ----------
-    dir_path (str, optional): 
-    season_map (dict, option): 
-    compare_cmip6 (bool): 
-    summaries (list): A list of options specifying which output results to include.
-        Valid options include:
-        - 'mean_climate' 
-    """
+    graphics_path : str, optional
+        A string for the directory path where graphics are saved. Default is None.
+    version : str
+        A string for the version number of the images. # NEED TO CONFIRM
+    season_map : dict
+        A dictionary to map different season abbreviations. Default maps pmp acronym for Annual Climatology to E3SM acronym: {'AC':'ANN'}. 
+    compare_cmip6 : bool, optional
+        If True, the viewer will include cmip6 results. Default is False.
+
+    Returns
+    ----------
+    webpage : html files
+        Multiple html files with pmp_output_viewer.html as the home page. One html file is generated per plot subfolder in graphics_path with valid png images.
+        Example directory path: graphics > mean_climate > e3sm > amip > v20241101 >portrait_plot
+            Example return: Two html files; pmp_output_viewer.html and mean_climate_portrait_plot_results.html
     
-    mean_clim_dive_down(dir_path=dir_path, season_map=season_map, compare_cmip6=compare_cmip6)
-        
+    Example
+    ----------
+    >>>from pmp_output_viewer import view_pmp_output
+
+    Notes
+    ----------
+    - Function(s) support limited variations of names for each summary:
+        - Mean Climate : ['mean_climate', 'mean_clim', 'climate_mean', 'clim_mean']
+        - Modes of Variability: ['modes_of_variability', 'variability_modes', 'modes_of_var', 'var_modes', 'MoV', 'MOV']
+    
+    """
+
+    # ----------
+    # Check if directory path provided is a valid directory
+    # ----------
+    if not os.path.isdir(graphics_path):
+        raise ValueError(f"The provided path '{graphics_path}' is not a valid directory.")    
+    
+    # ----------
+    # Create home page
+    # ----------
     home_content = """
     <!DOCTYPE html>
     <html>
@@ -45,22 +73,152 @@ def view_pmp_output(
         <h1>Welcome to the POV PROTOTYPE page</h1>
         <p>Goal: Generate a PMP output viewer that offers a HTML page showing PMP output image files in an organized way.<p>
         <br><br><br>
-        <h3>Mean Climate:</h3>
-        <a href="./mean_clim_results.html" target="_blank">Dive Down Plots</a><br>
+    """
+    # Use folder names as headers
+    summaries, plot_paths = navigate_graphics_dir(graphics_path=graphics_path, version=version)
+
+    # Run corresponding functions to generate individual plot pages
+    mean_climate = ['mean_climate', 'mean_clim', 'climate_mean', 'clim_mean']
+    for mc in mean_climate:
+        if mc in summaries:
+            summary_name = mc
+            for plot_path in plot_paths:
+                if check_png_files(plot_path):
+                    if any(mc in plot_path for mc in mean_climate):
+                        plot_name = os.path.basename(plot_path)
+                        layout = create_mean_climate_page(plot_path=plot_path, season_map=season_map, compare_cmip6=compare_cmip6)
+                        output_file(f"./{summary_name}_{plot_name}_results.html")
+                        save(layout)
+    
+    """ if 'variability_modes' in summaries:
+        full_paths = []
+        for plot_path in plot_paths:
+            if 'variability_modes' in plot_path:
+                full_path = os.path.join(plot_path, 'ERA5')
+                full_paths.append(full_path)
+        for fp in full_paths:
+            if check_png_files(fp):
+                #    create_mov_page() """ 
+    
+    # Add local links for individual pages by plot type
+    for summary_name in summaries:
+        home_content += f"<h3>{summary_name}</h3>\n"
+        
+        for plot_path in plot_paths:
+            if summary_name in plot_path:
+                if check_png_files(plot_path):
+                    plot_name = os.path.basename(plot_path)
+                    home_content += f'<li><a href="{summary_name}_{plot_name}_results.html">{plot_name}</a></li>\n'
+            
+    # close html tags
+    home_content += """
     </body>
     </html>
     """
+    
+    # save home page as html
     with open("pmp_output_viewer.html", "w") as pov_file:
         pov_file.write(home_content)
 
     cwd = os.getcwd()
     return print(f"POV created in {cwd}")
 
-def mean_clim_dive_down(dir_path, season_map=None, compare_cmip6=False):
-    
+# ----------
+# Support functions
+# ----------
+
+def navigate_graphics_dir(graphics_path, version):
+    """
+    Navigates summary folder to png files.
+
+    Parameters
+    ----------
+    graphics_path : str, optional
+        A string for the directory path where graphics are saved. Default is None.
+    version : str
+        A string for the version number of the images. # NEED TO CONFIRM
+
+    Returns
+    ----------
+    Two arrays
+        One contains the names of summary folders. One contains the full paths to each PNG file.
+    """
+    # summary > e3sm > amip > version > plot type > png files
+    summaries = [name for name in os.listdir(graphics_path) if os.path.isdir(os.path.join(graphics_path, name))]
+    summary_paths = []
+    e3sm_paths = []
+    amip_paths = []
+    version_paths = []
+    plot_paths = []
+
+    for summary in summaries:
+        summary_path = os.path.join(graphics_path, summary)
+        summary_paths.append(summary_path)
+
+    for summary_path in summary_paths:
+        e3sm_path = os.path.join(summary_path, 'e3sm')
+        e3sm_paths.append(e3sm_path)
+
+    for e3sm_path in e3sm_paths:
+        amip_path = os.path.join(e3sm_path, 'amip')
+        amip_paths.append(amip_path)
+
+    for amip_path in amip_paths:
+        version_path = os.path.join(amip_path, version)
+        version_paths.append(version_path)
+
+    for version_path in version_paths:
+        plot_dirs = [os.path.join(version_path, plot_folder)
+        for plot_folder in os.listdir(version_path)
+        if os.path.isdir(os.path.join(version_path, plot_folder))]
+        plot_paths.extend(plot_dirs)
+
+    return summaries, plot_paths
+
+# checks if png files are available
+def check_png_files(path):
+    # check if the folder contains png files
+    for filename in os.listdir(path):
+        if filename.lower().endswith('.png'):
+            return True
+    return False
+
+def create_mean_climate_page(plot_path, season_map={'AC':'ANN'}, compare_cmip6=False):
+
+    """
+    Creates mean climate results page.
+
+    Parameters
+    ----------
+    plot_path : str, optional
+        A string for the directory path where graphics are saved. Default is None.
+    season_map : dict
+        A dictionary to map different season abbreviations. Default maps pmp acronym for Annual Climatology to E3SM acronym: {'AC':'ANN'}. 
+    compare_cmip6 : bool, optional
+        If True, the viewer will include cmip6 results. Default is False.
+
+    Returns
+    ----------
+    webpage : html file
+        Interactive bokeh webpage with filters and data table.
+    """
     def create_layout():
-    
+        """
+        Compiles bokeh components into single layout.
+
+        Returns
+        ----------
+        bokeh layout object
+        """
         def add_var_long_name(df):
+            """
+            Uses 'long_name' column in the CMIP6 Amon table to add a variable description to the table.
+
+            Parameters
+            ----------
+            df : pandas dataframe
+                dataframe to add the description field to.
+            """
             with open('./CMIP6_Amon.json', 'r') as file:
                 cmor_table = json.load(file)
 
@@ -70,24 +228,49 @@ def mean_clim_dive_down(dir_path, season_map=None, compare_cmip6=False):
             }
 
             def extract_base_var(var_name):
+                """
+                Extracts the base name of the variable.
+
+                Example
+                ----------
+                For variables with different layers, use the base name to match to a name in the AMON table. (e.g., For ua-200 use ua)
+
+                Returns
+                ----------
+                Pandas dataframe with a new column 'Description'.
+                """
                 return var_name.split('-')[0]
 
             df['Description'] = df['Variable'].apply(lambda var: var_to_long_name.get(extract_base_var(var), 'NaN'))
             return df
-        if dir_path:
-            def create_user_df(dir_path, season_map=None):
+        
+        if plot_path:
+            def create_user_df(plot_path, season_map={'AC':'ANN'}):
+                """
+                Create a dataframe from the provided directory path. (User model)
+
+                Parameters
+                ----------
+                plot_path : str, optional
+                    A string for the directory path where graphics are saved. Default is None.
+                season_map : dict
+                    A dictionary to map different season abbreviations. Default maps pmp acronym for Annual Climatology to E3SM acronym: {'AC':'ANN'}.
+                
+                Returns
+                ----------
+                pandas dataframe
+                    A table with columns: mip, model, variable, description, region, ANN, DJF, JJA, MAM, SON
+                """
                 data = {}
                 season_map = season_map or {} # Default to empty dict if none
 
-                for var_folder, _, files in os.walk(dir_path):
-                    # extract variable name from subdirectory
-                    variable = os.path.basename(var_folder)
-
+                for this_path, this_dir, files in os.walk(plot_path):
                     for file in files:
-                        if file.endswith(('.png')):
+                        if file.endswith(('png')):
                             # extract file components based on underscores
                             parts = file.split('_')
                             if len(parts) >= 8:
+                                variable = parts[0]
                                 model = parts[1]
                                 region = parts[5]
                                 season = parts[6]
@@ -97,9 +280,9 @@ def mean_clim_dive_down(dir_path, season_map=None, compare_cmip6=False):
                                 season = season_map.get(season, season)
                             else:
                                 model, region, season = None, None, None
-                            
+
                             # construct image file path
-                            file_path = os.path.join(dir_path, var_folder, file)
+                            file_path = os.path.join(plot_path, file)
 
                             # use unique key to group data
                             key = (variable, model, region)
@@ -121,14 +304,21 @@ def mean_clim_dive_down(dir_path, season_map=None, compare_cmip6=False):
                 df = add_var_long_name(df)
 
                 return df
-    
+            
         if compare_cmip6:
-            season_map = {'AC':'ANN'}
-            if dir_path:
-                user_df = create_user_df(dir_path=dir_path, season_map=season_map).sort_values(['MIP', 'Model', 'Variable', 'Region'])
+            if plot_path:
+                user_df = create_user_df(plot_path=plot_path, season_map=season_map).sort_values(['MIP', 'Model', 'Variable', 'Region'])
             else:
                 user_df = pd.DataFrame()
             def create_cmip6_df():
+                """
+                Create results using cmip6 data - downloaded from pmp.
+
+                Returns
+                ----------
+                pandas dataframe
+                    A table with columns: mip, model, variable, description, region, ANN, DJF, JJA, MAM, SON
+                """
                 # download cmip6 data using pmp
                 cmip6_dir = './pov_cmip6_mean_clim_json_files'
                 os.makedirs(cmip6_dir, exist_ok=True)
@@ -176,17 +366,15 @@ def mean_clim_dive_down(dir_path, season_map=None, compare_cmip6=False):
                 cmip6_df = season_df
                 
                 return cmip6_df
-        
+            
             cmip6_df = create_cmip6_df().sort_values(['MIP', 'Model', 'Variable', 'Region'])
             cmip6_df = add_var_long_name(cmip6_df)
             df = pd.concat([user_df, cmip6_df])
         else:
-            season_map = {'AC':'ANN'}
-            user_df = create_user_df(dir_path, season_map=season_map).sort_values(['MIP', 'Model', 'Variable', 'Region'])
+            user_df = create_user_df(plot_path, season_map=season_map).sort_values(['MIP', 'Model', 'Variable', 'Region'])
             df = user_df
 
         source = ColumnDataSource(data=dict(df))
-
         filtered_data = df.loc[df['Region']=='global']
         filtered_source = ColumnDataSource(data=filtered_data.to_dict(orient="list"))
 
@@ -289,6 +477,4 @@ def mean_clim_dive_down(dir_path, season_map=None, compare_cmip6=False):
         return column(mip_dropdown, model_dropdown, var_dropdown, region_dropdown, dtable)
     
     layout = create_layout()
-
-    output_file("./mean_clim_results.html")
-    return save(layout)
+    return layout
