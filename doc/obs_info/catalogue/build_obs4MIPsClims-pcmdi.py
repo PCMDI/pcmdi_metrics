2942794a28f469398b1596277a1399b7735075bb
Peter Gleckler <gleckler1@gates.llnl.gov>
2025-01-31 15:18:15 -0800

    jsons added

--- doc/obs_info/catalogue/build_obs4MIPsClims-pcmdi.py
+++ doc/obs_info/catalogue/build_obs4MIPsClims-pcmdi.py
@@ -24,9 +24,9 @@ pathout = '/p/user_pub/PCMDIobs/catalogue/'
 if len(sys.argv) > 1:
     data_path = sys.argv[1]
 else:
-    data_path = '/p/user_pub/pmp/pmp_reference/obs4MIPs_clims/'
+    data_path = '/p/user_pub/pmp/pmp_reference/'
 
-full_path = data_path + '/' + vrs + '/gn/' + verin + '/*_' +sea + '.nc' 
+full_path = data_path + 'obs4MIPs_clims/' + vrs + '/gn/' + verin + '/*_' +sea + '.nc' 
 
 ##############################################################3
 by_source = {}
@@ -43,18 +43,26 @@ for l in lsttt:
    vn = path.split('/')[len(path.split('/'))-1]
    if source_id not in by_source.keys(): by_source[source_id] = {}
    if vr not in by_source[source_id].keys(): by_source[source_id][vr] = {}
+   by_source[source_id][vr]['PCMDI-path'] = path + '/'
+   by_source[source_id][vr]['path'] = path.split(data_path)[1] + '/' 
    by_source[source_id][vr]['filename'] = filename 
    by_source[source_id][vr]['version'] = vn
    by_source[source_id][vr]['period'] = per 
-
    if vr not in by_var.keys(): by_var[vr] = {}
    if source_id not in by_var[vr].keys(): by_var[vr][source_id] = {}
+   by_var[vr][source_id]['PCMDI-path'] = path + '/'
+   by_var[vr][source_id]['path'] = path.split(data_path)[1] + '/' 
    by_var[vr][source_id]['filename'] = filename
    by_var[vr][source_id]['version'] = vn
    by_var[vr][source_id]['period'] = per
+#  w = sys.stdin.readline()
 
-with open(pathout + "PMP-obs4MIPsClims-bySource-" + ver + ".json", "w") as outfile:
-    outfile.write(json.dumps(by_source, indent=4))
-with open(pathout + "PMP-obs4MIPsClims-byVar-" + ver + ".json", "w") as outfile:
-    outfile.write(json.dumps(by_var, indent=4))
+pths_out = [pathout,'./']
+for pth in pths_out:
+ with open(pth + "PMP-obs4MIPsClims-bySource-" + ver + ".json", "w") as outfile:
+     outfile.write(json.dumps(by_source, indent=4))
+ with open(pth + "PMP-obs4MIPsClims-byVar-" + ver + ".json", "w") as outfile:
+     outfile.write(json.dumps(by_var, indent=4))
+
+
 
