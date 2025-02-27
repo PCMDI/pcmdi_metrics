26d3176f5ce28736beaebbf250d6c236a4260cb5
Ana Ordonez <ordonez4@llnl.gov>
2025-02-27 11:23:21 -0800

    add nclim run scripts

---
+++ pcmdi_metrics/drcdm/param/nClimGrid-Daily-1-0/drcdm_nClimGrid_tasmin.py
@@ -0,0 +1,22 @@
+# Settings for extremes driver
+
+# These settings are required
+vars = ["tasmin"]  # Choices are 'pr','tasmax', 'tasmin'
+test_data_set = ["Reference"]
+realization = ["nClimGrid-Daily-1-0"]
+test_data_path = "/pscratch/sd/a/aordonez/nclim/tasmin/"
+filename_template = "tasmin_day_nClimGrid-Daily-1-0_PCMDI_5km_*.nc"
+
+metrics_output_path = "/pscratch/sd/a/aordonez/pmp_data/drcdm/obs/tasmin/nClimGrid-Daily-1-0/"
+
+
+ModUnitsAdjust=(True, 'KtoF', 0, 'F')
+dec_mode = "DJF"
+annual_strict = False
+drop_incomplete_djf = False
+regrid = False
+plot = True
+netcdf = True
+generate_sftlf = True
+msyear = 1985
+meyear = 2014
---
+++ pcmdi_metrics/drcdm/param/nClimGrid-Daily-1-0/run_CONUS_obs_tasmax.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -A m2637
+#SBATCH --job-name=Nclimtas
+#SBATCH --nodes=1
+#SBATCH --time=3:00:00
+#SBATCH --qos=regular
+#SBATCH --constraint=cpu
+
+
+source /global/homes/a/aordonez/miniconda3/etc/profile.d/conda.sh
+conda activate pmp_drcdm
+
+srun time drcdm_driver.py -p /global/homes/a/aordonez/pmp_param/drcdm/obs/nClimGrid-Daily-1-0/drcdm_nClimGrid_tasmax.py
+
