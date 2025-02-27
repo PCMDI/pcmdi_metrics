c7a3045eade7260738d4ecda444e8e7513c9aa80
Ana Ordonez <ordonez4@llnl.gov>
2025-02-27 11:22:03 -0800

    add PRISM run scripts

---
+++ pcmdi_metrics/drcdm/param/PRISM/run_CONUS_obs_tasmin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -A m2637
+#SBATCH --job-name=obstas
+#SBATCH --nodes=1
+#SBATCH --time=3:00:00
+#SBATCH --qos=regular
+#SBATCH --constraint=cpu
+
+
+source /global/homes/a/aordonez/miniconda3/etc/profile.d/conda.sh
+conda activate pmp_drcdm
+
+srun time drcdm_driver.py -p /global/homes/a/aordonez/pmp_param/drcdm/obs/PRISM/drcdm_PRISM_tasmin.py
+
