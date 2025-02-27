e454a8bd98f510795553561c0c633b043d8f2920
Ana Ordonez <ordonez4@llnl.gov>
2025-02-27 11:19:50 -0800

    add STAR run scripts

---
+++ pcmdi_metrics/drcdm/param/STAR-ESDM/drcdm_STAR-ESDM_template_tasmin.py
@@ -0,0 +1,18 @@
+# Settings for extremes driver
+
+# These settings are required
+vars = ["tasmin"]  # Choices are 'pr','tasmax', 'tasmin'
+test_data_path = "/global/cfs/projectdirs/m3522/project_downscale/CMIP6/NAM/TTU/STAR-ESDM-V1/%(model)/CMIP/historical/%(realization)/day/%(variable)/v20241130/"
+filename_template = "tasmin_NAM_CMIP6_%(model)_historical_%(realization)_TTU_STAR-ESDM-V1_day_*.nc"
+
+
+ModUnitsAdjust=(True, 'KtoF', 0, 'F')
+dec_mode = "DJF"
+annual_strict = False
+drop_incomplete_djf = False
+regrid = False
+plot = True
+netcdf = True
+generate_sftlf = False
+msyear = 1985
+meyear = 2014
