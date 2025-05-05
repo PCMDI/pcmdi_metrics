75ba56bacb6f079b73bf3c06e47bc5af46301f81
Jiwoo Lee <lee1043@llnl.gov>
2025-05-05 08:50:35 -0700

    remove deprecated tests

--- tests/deprecated/test_pmp_gensftlf_test.py
+++
@@ -1,7 +0,0 @@
-import basepmpdriver
-import os
-
-class PMPParamTest(basepmpdriver.PMPDriverTest):
-    def testParam(self):
-        param = os.path.join(self.path_parameter_files,"gensftlf_test.py")
-        self.runPMP(param)
