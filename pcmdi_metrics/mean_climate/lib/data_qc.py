0b6da6c2fe5ac1c0620c1d3a60fa0c996f83535b
Jiwoo Lee <lee1043@llnl.gov>
2025-07-01 14:14:02 -0700

    add docstring

--- pcmdi_metrics/mean_climate/lib/data_qc.py
+++ pcmdi_metrics/mean_climate/lib/data_qc.py
@@ -2,6 +2,35 @@ from pcmdi_metrics.stats import cor_xy
 
 
 def data_qc(test_data_name, ds_test, ds_ref, var, varname):
+    """
+    Perform data quality control checks and adjustments on the test dataset.
+    This function checks for common issues such as sign conventions in wind stress data.
+    
+    Parameters
+    ----------
+    test_data_name : str
+        Name of the test dataset.
+    ds_test : xarray.Dataset
+        Test dataset to be checked and potentially modified.
+    ds_ref : xarray.Dataset
+        Reference dataset for comparison.
+    var : str
+        Variable name (e.g., 'tauu' or 'tauv') to be checked.
+    varname : str
+        Actual variable name in the dataset.
+    
+    Returns
+    -------
+    ds_test : xarray.Dataset
+        The potentially modified test dataset after quality control checks.
+    
+    Notes
+    -----
+    This function currently checks for sign conventions in wind stress data ('tauu' and 'tauv').
+    If the pattern correlation between the test and reference datasets is negative, it suggests
+    an opposite sign convention, and the function will swap the sign of the test dataset variable.
+    In addition, for 'tauu', it checks the tropical mean to further confirm sign consistency.
+    """
     if var in ["tauu", "tauv"]:
         # Get time mean
         test_time_mean = ds_test[varname].mean(dim="time")
