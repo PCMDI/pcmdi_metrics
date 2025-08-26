d0aca068a6f25dd292aa066a673a838ae3bda11e
Kristin Chang <chang61@llnl.gov>
2025-08-26 16:01:58 -0700

    Update base.py
    Simplifying class Base and removing legacy cdat based libraries/functions.

--- pcmdi_metrics/io/__init__.py
+++ pcmdi_metrics/io/__init__.py
@@ -2,7 +2,6 @@
 from .xcdat_openxml import xcdat_open  # noqa  # isort:skip
 from .string_constructor import StringConstructor, fill_template  # noqa  # isort:skip
 from . import base  # noqa
-from .base import MV2Json  # noqa
 
 from .xcdat_dataset_io import (  # noqa  # isort:skip
     da_to_ds,
