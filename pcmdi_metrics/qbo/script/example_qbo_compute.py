41df1a66b3608cb0a3d486bbf5ca0f6a3e734b55
lee1043 <lee1043@llnl.gov>
2026-08-03 11:24:22 -0700

    use sample data in local

--- pcmdi_metrics/qbo/script/example_qbo_compute.py
+++ pcmdi_metrics/qbo/script/example_qbo_compute.py
@@ -10,13 +10,15 @@ def test_example_cesm2():
         "model": "CESM2",
         "exp": "historical",
         "member": "r1i1p1f1",
-        "input_file": "sample_data/ua_Amon_CESM2_historical_r1i1p1f1_gn_185001-201412.nc",
-        "input_file2": "sample_data/rlut_day_CESM2_historical_r1i1p1f1_gn_19800101-19891231.nc",
+        "input_file": "/home/lee1043/work/DATA/CMIP6/CESM2/historical/Amon/ua/ua_Amon_CESM2_historical_r1i1p1f1_gn_185001-201412.nc",
+        "input_file2": "/home/lee1043/work/DATA/CMIP6/CESM2/historical/day/rlut/rlut_day_CESM2_historical_r1i1p1f1_gn_????0101-????1231.nc",
         "varname": "ua",
         "level": 50,  # hPa (=mb)
         "varname2": "rlut",
-        "start": "1981-01",
-        "end": "1988-12",
+        #"start": "1981-01",
+        "start": "1979-01",
+        #"end": "1988-12",
+        "end": "2005-12",
         "regrid": False,
         "regrid_tool": "xesmf",
         "target_grid": "2x2",
@@ -35,8 +37,8 @@ def test_example_era5():
         "model": "ERA5",
         "exp": None,
         "member": None,
-        "input_file": "/work/lee1043/DATA/ERA5/ERA5_u50_monthly_1979-2021_rewrite.nc",
-        "input_file2": "/work/lee1043/DATA/ERA5/ERA5_olr_daily_40s40n_1979-2021_rewrite.nc",
+        "input_file": "/home/lee1043/work/DATA/ERA5/qbo_mjo_input/ERA5_u50_monthly_1979-2021_rewrite.nc",
+        "input_file2": "/home/lee1043/work/DATA/ERA5/qbo_mjo_input/ERA5_olr_daily_40s40n_1979-2021_rewrite.nc",
         "varname": "u50",
         "level": None,  # hPa (=mb)
         "varname2": "olr",
@@ -56,4 +58,7 @@ def test_example_era5():
 
 if __name__ == "__main__":
     # Run ERA5 example by default
-    test_example_era5()
+    # test_example_era5()
+    
+    # Run CESM2 example
+    test_example_cesm2()
